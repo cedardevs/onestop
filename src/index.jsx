459f7d0e8abd558d005b567a9a6a5dfdfae99ff4
Evan McQuinn <evan.mcquinn@noaa.gov>
2016-04-15 23:36:02 -0600

    Need to add this plugin to enable animations, e.g. collapsing

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,5 @@
 import 'babel-polyfill'
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import React from 'react';
 import {render} from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
@@ -7,6 +8,12 @@ import thunk from 'redux-thunk';
 import {Provider} from 'react-redux';
 import Root from './components/Root.jsx'
 import reducer from './reducers/main';
+
+// Needed for onTouchTap
+// Can go away when react 1.0 release
+// Check this repo:
+// https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
 
 let store = createStore(reducer, applyMiddleware(thunk));
 
