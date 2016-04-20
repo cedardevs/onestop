a089b3c3145ed91de911e9f5fa75403c1db01054
Evan McQuinn <evan.mcquinn@noaa.gov>
2016-04-20 12:05:45 -0600

    Clean up

--- src/components/ResultsList.jsx
+++ src/components/ResultsList.jsx
@@ -1,26 +1,19 @@
 import React, { PropTypes } from 'react'
-import GridList from 'material-ui/lib/grid-list/grid-list';
 import CircularProgress from 'material-ui/lib/circular-progress';
 import Result from './Result';
 
 const styles = {
-  root: {
+  spinner: {
     display: 'flex',
     flexWrap: 'wrap',
     justifyContent: 'space-around',
     paddingTop: 24
-  },
-  gridList: {
-    width: 800,
-    height: 600,
-    overflowY: 'auto',
-    marginBottom: 24
   }
 };
 
 const ResultsList = ({results, loading}) => {
   if (loading) {
-    return <div style={styles.root}><CircularProgress/></div>
+    return <div style={styles.spinner}><CircularProgress/></div>
   }
   else {
     return <div>{results.map(r => <Result key={r.id} record={r}/>)}</div>
