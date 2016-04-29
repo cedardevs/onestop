e46e0a8250beb1bfd893bb8c609e386f4ec54e5e
Evan McQuinn <evan.mcquinn@noaa.gov>
2016-04-29 14:55:18 -0600

    Fix the assertion to compare the result with the expected state

--- test/detailsReducerSpec.js
+++ test/detailsReducerSpec.js
@@ -17,12 +17,12 @@ describe('The details reducer', function () {
   it('handles completed details request', function () {
     it('for a new details request', function () {
       const id = 'a';
-      const detailContent = {id: 'a', summary: 'this is a super cool test result'};
-      const detailsAction = completeDetails(id, detailContent);
+      const expectedResult = {id: 'a', summary: 'this is a super cool test result'};
+      const detailsAction = completeDetails(id, expectedResult);
       const result = details(initialState, detailsAction);
 
       result.should.not.equal(initialState);
-      result.should.equal(Immutable.fromJS(details));
+      result.should.equal(Immutable.fromJS(expectedResult));
     });
   });
 
