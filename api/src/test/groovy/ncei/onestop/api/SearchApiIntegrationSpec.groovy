8a539a51afa8a7f289b01d0331b8beadf66392c1
manstett <mike.anstett@noaa.gov>
2016-05-31 09:16:14 -0600

    Fix initial integration test

--- api/src/test/groovy/ncei/onestop/api/SearchApiIntegrationSpec.groovy
+++ api/src/test/groovy/ncei/onestop/api/SearchApiIntegrationSpec.groovy
@@ -1,52 +1,53 @@
-package ngdc.auth
+package ncei.onestop.api
 
-import ncei.onestop.api.Application
-import ncei.onestop.api.TestResponseErrorHandler
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.SpringApplicationConfiguration
 import org.springframework.boot.test.WebIntegrationTest
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.RequestEntity
 import org.springframework.web.client.RestTemplate
 import spock.lang.Specification
 import spock.lang.Unroll
 
 @Unroll
-@WebIntegrationTest("server.port:0")
+@WebIntegrationTest("server.port:8097")
 //@WebIntegrationTest("server.port:8097")
 @SpringApplicationConfiguration(classes = Application)
 class SearchApiIntegrationSpec extends Specification {
-  @Value('${local.server.port}')
-  String testPort = "8097";
+    @Value('${local.server.port}')
+    String testPort = "8097";
+    MediaType contentType = MediaType.APPLICATION_JSON
 
-  String searchBaseUrl = "http://localhost:${testPort}/search"
+    String searchBaseUrl = "http://localhost:${testPort}/search"
 
-  private RestTemplate setupRestClient() {
-    RestTemplate restTemplate = new RestTemplate()
-    restTemplate.setErrorHandler(new TestResponseErrorHandler())
-    restTemplate
-  }
+    private static RestTemplate setupRestClient() {
+        RestTemplate restTemplate = new RestTemplate()
+        restTemplate.setErrorHandler(new TestResponseErrorHandler())
+        restTemplate
+    }
 
-  def searches = [
-    baresearch: [
-      body: '{"searchText": "temperature"}'
+    def searches = [
+            baresearch: [
+                    body: '{"searchText": "temperature"}'
+            ]
     ]
-  ]
 
-  // -------- Test Cases --------
-  def 'valid search returns ok and results'() {
-    println("searchBaseUrl:${searchBaseUrl}")
-    def restTemplate = setupRestClient()
-    def requestEntity = RequestEntity
-        .post(searchBaseUrl.toURI())
-        .body(searches.baresearch.body)
+    // -------- Test Cases --------
+    def 'valid search returns ok and results'() {
+        println("searchBaseUrl:${searchBaseUrl}")
+        def restTemplate = setupRestClient()
+        def requestEntity = RequestEntity
+                .post(searchBaseUrl.toURI())
+                .contentType(contentType)
+                .body(searches.baresearch.body)
 
-    when:
-    def result = restTemplate.exchange(requestEntity, Map)
+        when:
+        def result = restTemplate.exchange(requestEntity, Map)
 
-    then:
-    result.statusCode == HttpStatus.OK
-  }
+        then:
+        result.statusCode == HttpStatus.OK
+    }
 
 
 }
