e009ae789f31d32752a932c7ca0bc5bb7407b483
Aaron Caldwell <aaron.caldwell@noaa.gov>
2016-06-02 09:35:40 -0600

    Semicolons

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,14 +1,14 @@
 import 'babel-polyfill'
-import injectTapEventPlugin from 'react-tap-event-plugin';
-import React from 'react';
-import {render} from 'react-dom';
-import {Router, Route, hashHistory} from 'react-router';
-import {createStore, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
-import {Provider} from 'react-redux';
+import injectTapEventPlugin from 'react-tap-event-plugin'
+import React from 'react'
+import {render} from 'react-dom'
+import {Router, Route, hashHistory} from 'react-router'
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import {Provider} from 'react-redux'
 import Root from './components/Root.jsx'
-import reducer from './reducers/main';
-import '../style/style.css';
+import reducer from './reducers/main'
+import '../style/style.css'
 
 // Needed for onTouchTap
 // Can go away when react 1.0 release
