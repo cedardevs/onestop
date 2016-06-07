39a1996d26bbb78c56d75b4a1e31ce8a49910b00
Aaron Caldwell <aaron.caldwell@noaa.gov>
2016-06-07 14:59:47 -0600

    Use separate style file for root, add ids for debugging

--- client/src/components/Root.jsx
+++ client/src/components/Root.jsx
@@ -1,34 +1,33 @@
 const logoPath = require('../../img/BS_noaalogo1.jpg')
 
 import React from 'react'
-//import Paper from 'material-ui/lib/paper'
 import ResultsContainer from '../result/ResultContainer'
 import DetailContainer from '../detail/DetailContainer'
 import SearchContainer from '../search/SearchContainer'
 import Footer from './Footer.jsx'
 import Header from './Header.jsx'
 import CSSModules from 'react-css-modules'
-import styles from './root.css'
+import './root.css'
 
 
 const Root = () => (
     <div>
-      <div styleName='header'>
+      <div styleName='header' id='header'>
         <Header/>
       </div>
-      <div styleName='appbar' rounded={false}>
-        <img styleName='logo' src={logoPath} alt="NOAA Logo"/>
-        <div styleName='searchFacet'>
+      <div styleName='appbar' id='appbar' rounded={false}>
+        <img styleName='logo' id='logo' src={logoPath} alt="NOAA Logo"/>
+        <div styleName='search-facet' id='search-facet'>
           <SearchContainer/>
         </div>
       </div>
-      <div styleName='results'>
+      <div styleName='results' id='results'>
         <ResultsContainer/>
       </div>
-      <div styleName='details'>
+      <div styleName='details' id='details'>
         <DetailContainer/>
       </div>
-      <div styleName='footer'>
+      <div styleName='footer' id='footer'>
         <Footer/>
       </div>
     </div>
