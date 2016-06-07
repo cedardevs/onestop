23394631ad06fd84dadf12d753fa8a66a78f57c0
Aaron Caldwell <aaron.caldwell@noaa.gov>
2016-06-07 16:15:22 -0600

    Add loader for handling of multiple image types

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -28,9 +28,11 @@ module.exports = {
         'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
         'postcss']
     }, {
-      test: /\.jpg$/,
-      exclude: /node_modules/,
-      loader: 'file?name=[path][name]-[hash].[ext]'
+      test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+            'file?hash=sha512&digest=hex&name=[hash].[ext]',
+            'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
     }]
   },
   postcss: function(webpack){
