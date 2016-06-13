73d61cd51506ae470fd5cd8be057a59756bd1d46
Aaron Caldwell <aaron.caldwell@noaa.gov>
2016-06-13 10:55:32 -0600

    Pull detail components from external styles

--- client/src/detail/DetailComponent.jsx
+++ client/src/detail/DetailComponent.jsx
@@ -1,58 +1,20 @@
 import React, { PropTypes } from 'react'
-
-import FlipCard from '../../node_modules/react-flipcard/lib/main';
+import FlipCard from '../../node_modules/react-flipcard/lib/main'
+import styles from './detail.css'
 
 
 const Detail = (props) => {
 
-
-  const cardHight = "300px";
-  const cardWidth = "300px";
-
-  const styles = {
-    base: {
-      display: props.id ? 'block' : 'none',
-      margin: 20
-    },
-    reactFlipCard: {
-      margin: "25px",
-      textAlign: "center",
-      width: cardWidth,
-      height: cardHight
-    },
-    reactFlipCard__Front: {
-      boxSizing: "border-box",
-      width: cardWidth,
-      height: cardHight,
-      borderRadius: "5px",
-      border: "1px solid #ccc",
-      padding: "25px",
-      backgroundColor: "#eee",
-      color: "black"
-    },
-    reactFlipCard__Back: {
-      boxSizing: "border-box",
-      width: cardWidth,
-      height: cardHight,
-      borderRadius: "5px",
-      border: "1px solid #ccc",
-      padding: "25px",
-      backgroundColor: "#cef",
-      color: "black"
-    }
-  };
-
-
   return (
 
-        <FlipCard disabled={true} style={styles.reactFlipCard} flipped={props.flipped}>
+        <FlipCard disabled={true} className={styles.reactFlipCard} flipped={props.flipped}>
 
-          <div style={styles.reactFlipCard__Front} onClick={props.onClick}>
+          <div className={styles.reactFlipCard__Front} onClick={props.onClick}>
             <div>Front</div>
             <div>Title: {props.title} </div>
             <div></div>
           </div>
-          <div style={styles.reactFlipCard__Back} onClick={props.onClick}>
+          <div className={styles.reactFlipCard__Back} onClick={props.onClick}>
             <div>Summary: {props.summary}</div>
           </div>
         </FlipCard>
