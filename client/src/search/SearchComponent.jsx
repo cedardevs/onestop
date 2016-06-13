bee944bf7a255734fbdd4ddf2719673912dece51
Aaron Caldwell <aaron.caldwell@noaa.gov>
2016-06-13 11:09:58 -0600

    Remove purecss test code

--- client/src/search/SearchComponent.jsx
+++ client/src/search/SearchComponent.jsx
@@ -13,11 +13,6 @@ const SearchFacet = ({indexName, submit, handleIndexChange}) => {
       <div>
         <IndexDropDown indexName={indexName} onChange={handleIndexChange}/>
       </div>
-      <div className={styles['pure-g']}>
-        <div className={styles['pure-u-1-3']}><p>Thirds</p></div>
-        <div className={styles['pure-u-1-3']}><p>Thirds</p></div>
-        <div className={styles['pure-u-1-3']}><p>Thirds</p></div>
-      </div>
     </fieldset>
   </form>
 }
