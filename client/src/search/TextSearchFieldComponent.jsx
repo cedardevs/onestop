6fd478a2b1272f5e7151c7534385f64bd0b98d4c
Evan McQuinn <evan.mcquinn@noaa.gov>
2016-06-15 14:35:35 -0600

    Only call onEnterKeyDown when the enter key is pushed. Clean up some leftover material ui attributes.

--- client/src/search/TextSearchFieldComponent.jsx
+++ client/src/search/TextSearchFieldComponent.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 
 const TextSearchField = ({onEnterKeyDown}) => {
-  const handleSubmit = (e) => onEnterKeyDown(e.target.value);
+  const handleKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      e.preventDefault()
+      onEnterKeyDown(e.target.value)
+    }
+  }
 
   return <input
-      hintText="Enter Search Term"
-      fullWidth={true}
-      onKeyDown={handleSubmit}
+    placeholder="Enter Search Term"
+    onKeyDown={handleKeyDown}
   />
 }
 
