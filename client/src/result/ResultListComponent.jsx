8fb693939549d0b81675bd6c90eb39200a1b6a0f
Aaron Caldwell <aaron.caldwell@noaa.gov>
2016-06-19 22:19:17 -0600

    Flipcards in test grid with no content passed in

--- client/src/result/ResultListComponent.jsx
+++ client/src/result/ResultListComponent.jsx
@@ -1,10 +1,11 @@
 import React, { PropTypes } from 'react'
 import Result from './ResultComponent'
+import Detail from '../detail/DetailComponent'
 import styles from './result.css'
 
 const ResultsList = ({results, loading, onResultClick}) => {
-  return <div className={`pure-menu-list ${styles.list}`} zDepth={3} rounded={false}>
-    {results.map(r => <Result key={r.id} record={r} onClick={onResultClick}/>)}
+  return <div className={styles['pure-g']} zDepth={3} rounded={false}>
+    {results.map(r => <div className={`${styles["pure-u-sm-1-3"]} ${styles["pure-u-md-1-6"]} ${styles.grid}`}><Detail key={r.id} /></div>)}
   </div>
 }
 
