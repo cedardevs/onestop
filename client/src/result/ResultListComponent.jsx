f54a610cb964c850d0d5b17394c2cfc968cac3a9
Aaron Caldwell <aaron.caldwell@noaa.gov>
2016-06-21 08:50:53 -0600

    Stable point of grid resizing for flip cards

--- client/src/result/ResultListComponent.jsx
+++ client/src/result/ResultListComponent.jsx
@@ -4,8 +4,8 @@ import Detail from '../detail/DetailComponent'
 import styles from './result.css'
 
 const ResultsList = ({results, loading, onCardClick}) => {
-  return <div className={styles['pure-g']} zDepth={3} rounded={false}>
-    {results.map(r => <div className={`${styles["pure-u-1"]} ${styles["pure-u-1"]} ${styles["pure-u-md-1-2"]} ${styles["pure-u-lg-1-4"]} ${styles.grid}`}>
+  return <div className={`${styles['pure-g']} ${styles.gridContainer}`} zDepth={3} rounded={false}>
+    {results.map(r => <div className={`${styles["pure-u-1"]} ${styles["pure-u-md-1-2"]} ${styles["pure-u-lg-1-3"]} ${styles["pure-u-xl-1-4"]} ${styles.grid}`}>
       <Detail key={r.id}
         onClick={() => onCardClick(r.id)}
         title={r.title}
