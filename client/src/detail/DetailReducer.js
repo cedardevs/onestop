0c3f3e3dd86a1a0efb7c0bd745ca76aee15ea07a
Aaron Caldwell <aaron.caldwell@noaa.gov>
2016-06-22 09:57:21 -0600

    Semicolons

--- client/src/detail/DetailReducer.js
+++ client/src/detail/DetailReducer.js
@@ -1,24 +1,24 @@
-import Immutable from 'immutable';
-import {SEARCH, SEARCH_COMPLETE} from '../search/SearchActions';
-import {FETCH_DETAILS, RECEIVE_DETAILS, FLIP_CARD} from './DetailActions';
+import Immutable from 'immutable'
+import {SEARCH, SEARCH_COMPLETE} from '../search/SearchActions'
+import {FETCH_DETAILS, RECEIVE_DETAILS, FLIP_CARD} from './DetailActions'
 
 export const initialState = {
   id: null,
   details: null,
   flipped: false
-};
+}
 
 export const details = (state = initialState, action) => {
   switch (action.type) {
     case SEARCH:
     case SEARCH_COMPLETE:
-      return Immutable.Map();
+      return Immutable.Map()
 
     case FETCH_DETAILS:
-      return Immutable.fromJS({id: action.id, flipped: false});
+      return Immutable.fromJS({id: action.id, flipped: false})
 
     case RECEIVE_DETAILS:
-        console.log("Action Details is "+ action.details);
+        console.log("Action Details is "+ action.details)
       return state.merge({
         details: action.details
       })
@@ -31,8 +31,8 @@ export const details = (state = initialState, action) => {
       }
 
     default:
-      return state;
+      return state
   }
-};
+}
 
-export default details;
+export default details
