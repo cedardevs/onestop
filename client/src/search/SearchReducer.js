d88326860564a2eeec23740484ea42d920b0bfb4
Aaron Caldwell <aaron.caldwell@noaa.gov>
2016-06-24 13:01:27 -0600

    Assign results to map with ID as key

--- client/src/search/SearchReducer.js
+++ client/src/search/SearchReducer.js
@@ -1,34 +1,34 @@
-import Immutable from 'immutable';
-import {INDEX_CHANGE, SEARCH, SEARCH_COMPLETE} from './SearchActions';
+import Immutable from 'immutable'
+import {INDEX_CHANGE, SEARCH, SEARCH_COMPLETE} from './SearchActions'
 
 export const initialState = Immutable.Map({
   text: '',
   index: '',
   inFlight: false
-});
+})
 
 export const search = (state = initialState, action) => {
   switch (action.type) {
     case INDEX_CHANGE:
       return state.merge({
         index: action.indexText
-      });
+      })
 
     case SEARCH:
       return state.merge({
         text: action.searchText,
         inFlight: true
-      });
+      })
 
     case SEARCH_COMPLETE:
       return state.merge({
         text: action.searchText,
         inFlight: false
-      });
+      })
 
     default:
       return state
   }
-};
+}
 
-export default search;
+export default search
