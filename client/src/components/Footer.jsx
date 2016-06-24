e5d07a78d5290bcceff7cbc808328c432d1fed49
sghebrec <semere.ghebrechristos@noaa.gov>
2016-06-24 15:50:08 -0600

    add pure-link and pure menu

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,15 +5,14 @@ import 'purecss'
 
 let Footer = ({dispatch}) => {
   return (
-      <div  className={styles.container}>
-        <div className={`${styles['pure-g']}`}>
+      <div className={`${styles.container} ${styles['pure-menu']}`}>
+        <div className={`${styles['pure-g']} ${styles['pure-u-lg']}`}>
           <div className={`${styles['pure-u-1-2']}`}>
             <a href="http://www.noaa.gov/about-our-agency" className={styles.about}>About our agency</a>
             <a href="http://www.noaa.gov/news-features" className={styles.about}>News and features</a>
             <a href="http://www.noaa.gov/our-work" className={styles.about}>Our work</a>
           </div>
-
-          <div className= {`${styles.feedback} ${styles['pure-u-lg-1-2']} ${styles['pure-u-md-1-2']}`}>
+          <div className= {`${styles.feedback} ${styles['pure-u-1-2']} ${styles['pure-u-md-1-2']}`}>
             <div className={styles.icons}>
               Stay connected:
               <a href="https://twitter.com/NOAA?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" className={styles.twitterLogo}>
@@ -26,14 +25,13 @@ let Footer = ({dispatch}) => {
                 <span className={styles.hidden}>Share to Youtube</span></a>
             </div>
 
-            <form className={styles.feedbackButton} method="link" action="https://www8.nos.noaa.gov/survey">
+            <form className={`${styles.feedbackButton} ${styles['pure-form']}`} method="link" action="https://www8.nos.noaa.gov/survey">
               How are we doing?
-              <input  className={styles.button}  type="submit" value="Feedback"></input>
+              <button type="submit" className={`${styles['pure-button']} ${styles['pure-button-primary']}`}>Feedback</button>
             </form>
           </div>
         </div>
-
-        <div className={`${styles.item2} ${styles['pure-g']}`}>
+        <div className={`${styles.items} ${styles['pure-g']}`}>
           <div className={`${styles['pure-u-1-4']}`}>
             <a href="http://www.noaa.gov/" className={styles.noaaLogo}>
               <span className={styles.hidden}>NOAA Home</span>
@@ -41,25 +39,23 @@ let Footer = ({dispatch}) => {
             <span>Science. Service. Stewardship.</span>
           </div>
 
-          <div className={`${styles['pure-u-3-4']}`}>
-            <ul className={styles.menu}>
-              <li className={styles.menuLi}><a href="http://www.noaa.gov/protecting-your-privacy" className={styles.menuLiA}>Protecting your Privacy</a></li>
-              <li className={styles.menuLi}><a href="http://www.noaa.gov/foia-freedom-information-act" className={styles.menuLiA}>FOIA</a></li>
-              <li className={styles.menuLi}><a href="http://www.cio.noaa.gov/services_programs/info_quality.html" className={styles.menuLiA}>Information Quality</a></li>
-              <li className={styles.menuLi}><a href="http://www.noaa.gov/disclaimer" className={styles.menuLiA}>Disclaimer</a></li>
-              <li className={styles.menuLi}><a href="https://www.usa.gov/" className={styles.menuLiA}>USA.gov</a></li>
-              <li className={styles.menuLi}><a href="https://www.ready.gov/" className={styles.menuLiA}>Ready.gov</a></li>
-              <li className={styles.menuLi}><a href="http://www.eeo.noaa.gov/noaa/" className={styles.menuLiA}>EEO</a></li>
-              <li className={styles.menuLi}><a href="http://www.homelandsecurity.noaa.gov/" className={styles.menuLiA}>Employee Check-In</a></li>
-              <li className={styles.menuLi}><a href="http://www.noaa.gov/contact-us" className={styles.menuLiA}>Contact us</a></li>
-              <li className={styles.menuLi}><a href="https://nsd.rdc.noaa.gov/nsd" className={styles.menuLiA}>Staff Directory</a></li>
-              <li className={styles.menuLi}><a href="http://www.noaa.gov/need-help" className={styles.menuLiA}>Need help?</a></li>
+          <div className={`${styles['pure-u-3-4']} ${styles['pure-menu']}`}>
+            <ul className = {`${styles['pure-menu-list']}`}>
+              <li className={`${styles['pure-menu-item']}`}><a href="http://www.noaa.gov/protecting-your-privacy" className={`${styles['pure-menu-link']}`}>Protecting your Privacy</a></li>
+              <li className={`${styles['pure-menu-item']}`}><a href="http://www.noaa.gov/foia-freedom-information-act" className={`${styles['pure-menu-link']}`}>FOIA</a></li>
+              <li className={`${styles['pure-menu-item']}`}><a href="http://www.cio.noaa.gov/services_programs/info_quality.html" className={`${styles['pure-menu-link']}`}>Information Quality</a></li>
+              <li className={`${styles['pure-menu-item']}`}><a href="http://www.noaa.gov/disclaimer" className={`${styles['pure-menu-link']}`}>Disclaimer</a></li>
+              <li className={`${styles['pure-menu-item']}`}><a href="https://www.usa.gov/" className={`${styles['pure-menu-link']}`}>USA.gov</a></li>
+              <li className={`${styles['pure-menu-item']}`}><a href="https://www.ready.gov/" className={`${styles['pure-menu-link']}`}>Ready.gov</a></li>
+              <li className={`${styles['pure-menu-item']}`}><a href="http://www.eeo.noaa.gov/noaa/" className={`${styles['pure-menu-link']}`}>EEO</a></li>
+              <li className={`${styles['pure-menu-item']}`}><a href="http://www.homelandsecurity.noaa.gov/" className={`${styles['pure-menu-link']}`}>Employee Check-In</a></li>
+              <li className={`${styles['pure-menu-item']}`}><a href="http://www.noaa.gov/contact-us" className={`${styles['pure-menu-link']}`}>Contact us</a></li>
+              <li className={`${styles['pure-menu-item']}`}><a href="https://nsd.rdc.noaa.gov/nsd" className={`${styles['pure-menu-link']}`}>Staff Directory</a></li>
+              <li className={`${styles['pure-menu-item']}`}><a href="http://www.noaa.gov/need-help" className={`${styles['pure-menu-link']}`}>Need help?</a></li>
             </ul>
           </div>
         </div>
       </div>
-
-
   );
 };
 
