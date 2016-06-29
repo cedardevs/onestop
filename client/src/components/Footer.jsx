c5faa205e6f586c24e6eda6369474dfa924ed89c
Aaron Caldwell <aaron.caldwell@noaa.gov>
2016-06-29 09:02:23 -0600

    Improve stacking and spacing of footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,11 +4,52 @@ import styles from './footer.css'
 import 'purecss'
 
 let Footer = ({dispatch}) => {
+
+  var externalResources = [
+              {
+                href: "http://www.noaa.gov/protecting-your-privacy",
+                text: "Protecting your Privacy"
+              },{
+                href: "http://www.noaa.gov/foia-freedom-information-act",
+                text: "FOIA"
+              },{
+                href: "http://www.cio.noaa.gov/services_programs/info_quality.html",
+                text: "Information Quality"
+              },{
+                href: "http://www.noaa.gov/disclaimer",
+                text: "Disclaimer"
+              },{
+                href: "https://www.usa.gov/",
+                text: "USA.gov"
+              },{
+                href: "https://www.ready.gov/",
+                text: "Ready.gov"
+              },{
+                href: "http://www.eeo.noaa.gov/noaa/",
+                text: "EEO"
+              },{
+                href: "http://www.homelandsecurity.noaa.gov/",
+                text: "Employee Check-In"
+              },{
+                href: "http://www.noaa.gov/contact-us",
+                text: "Contact Us"
+              },{
+                href: "https://nsd.rdc.noaa.gov/nsd",
+                text: "Staff Directory"
+              },{
+                href: "http://www.noaa.gov/need-help",
+                text: "Need help?"
+              }
+            ]
+  externalResources = externalResources.map(function(el){
+    return <li className={`${styles.externalResource} ${styles.rowElement}`}><a href={`${el.href}`} className={`${styles.footerLink}`}>{`${el.text}`}</a></li>
+  })
+
   return (
       <div className={styles.container}>
       <div className={styles.panel}>
         <div className={`${styles['pure-g']} ${styles['pure-u-lg']}`}>
-          <div className={`${styles['pure-u-1-2']}`}>
+          <div className={`${styles['pure-u-1-2']} ${styles.noaaLinks}`}>
             <a href="http://www.noaa.gov/about-our-agency" className={styles.about}>About our agency</a>
             <a href="http://www.noaa.gov/news-features" className={styles.about}>News and features</a>
             <a href="http://www.noaa.gov/our-work" className={styles.about}>Our work</a>
@@ -26,33 +67,23 @@ let Footer = ({dispatch}) => {
                 <span className={styles.hidden}>Share to Youtube</span></a>
             </div>
 
-            <form className={`${styles.feedbackForm} ${styles['pure-form']}`} method="link" action="https://www8.nos.noaa.gov/survey">
-              How are we doing?
-              <button type="submit" className={`${styles['pure-button']} ${styles['pure-button-primary']}`}>Feedback</button>
+            <form className={`${styles.feedbackForm} ${styles['pure-form']}`} method="link" action="https://www.nos.noaa.gov/survey">
+              <div className={styles.rowElement}>How are we doing?</div>
+              <button type="submit" className={`${styles['pure-button']} ${styles['pure-button-primary']} ${styles.rowElement}`}>Feedback</button>
             </form>
           </div>
         </div>
         <div className={`${styles.items} ${styles['pure-g']}`}>
-          <div className={`${styles['pure-u-1-4']} ${styles.logoPanel}`}>
-            <a href="http://www.noaa.gov/" className={styles.noaaLogo}>
+          <div className={`${styles['pure-u-2-5']} ${styles.logoPanel}`}>
+            <a href="http://www.noaa.gov/" className={`${styles.noaaLogo} ${styles.rowElement}`}>
               <span className={styles.hidden}>NOAA Home</span>
             </a>
-            <span>Science. Service. Stewardship.</span>
+            <span className={`${styles.rowElement} ${styles.slogan}`}>Science. Service. Stewardship.</span>
           </div>
 
-          <div className={`${styles['pure-u-3-4']} ${styles['pure-menu']} ${styles.externalLinksPanel}`}>
-            <ul className = {`${styles['pure-menu-list']}`}>
-              <li className={`${styles['pure-menu-item']}`}><a href="http://www.noaa.gov/protecting-your-privacy" className={`${styles['pure-menu-link']}`}>Protecting your Privacy</a></li>
-              <li className={`${styles['pure-menu-item']}`}><a href="http://www.noaa.gov/foia-freedom-information-act" className={`${styles['pure-menu-link']}`}>FOIA</a></li>
-              <li className={`${styles['pure-menu-item']}`}><a href="http://www.cio.noaa.gov/services_programs/info_quality.html" className={`${styles['pure-menu-link']}`}>Information Quality</a></li>
-              <li className={`${styles['pure-menu-item']}`}><a href="http://www.noaa.gov/disclaimer" className={`${styles['pure-menu-link']}`}>Disclaimer</a></li>
-              <li className={`${styles['pure-menu-item']}`}><a href="https://www.usa.gov/" className={`${styles['pure-menu-link']}`}>USA.gov</a></li>
-              <li className={`${styles['pure-menu-item']}`}><a href="https://www.ready.gov/" className={`${styles['pure-menu-link']}`}>Ready.gov</a></li>
-              <li className={`${styles['pure-menu-item']}`}><a href="http://www.eeo.noaa.gov/noaa/" className={`${styles['pure-menu-link']}`}>EEO</a></li>
-              <li className={`${styles['pure-menu-item']}`}><a href="http://www.homelandsecurity.noaa.gov/" className={`${styles['pure-menu-link']}`}>Employee Check-In</a></li>
-              <li className={`${styles['pure-menu-item']}`}><a href="http://www.noaa.gov/contact-us" className={`${styles['pure-menu-link']}`}>Contact us</a></li>
-              <li className={`${styles['pure-menu-item']}`}><a href="https://nsd.rdc.noaa.gov/nsd" className={`${styles['pure-menu-link']}`}>Staff Directory</a></li>
-              <li className={`${styles['pure-menu-item']}`}><a href="http://www.noaa.gov/need-help" className={`${styles['pure-menu-link']}`}>Need help?</a></li>
+          <div className={`${styles['pure-u-3-5']} ${styles.externalLinksPanel}`}>
+            <ul className = {`${styles.resourceList}`}>
+            {externalResources}
             </ul>
           </div>
           </div>
