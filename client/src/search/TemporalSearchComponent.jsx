5e973b60010f5a830b9e07822f57bd25531e0c98
sghebrec <semere.ghebrechristos@noaa.gov>
2016-07-12 17:35:19 -0600

    add place holder for date dropdown and add react - date picker

--- client/src/search/TemporalSearchComponent.jsx
+++ client/src/search/TemporalSearchComponent.jsx
@@ -1,34 +1,53 @@
 import React from 'react'
-import {TransitionView, DateField, Calendar } from 'react-date-picker'
+import {TransitionView, DateField, Calendar} from 'react-date-picker'
 import styles from './search.css'
 
-const TemporalSearch = () => (
-        <div>
-          <div className={styles.startTimeField}>
-            <DateField
-                forceValidDate
-                defaultValue={"2016-05-30 15:23:34"}
-                dateFormat="YYYY-MM-DD HH:mm:ss">
-              <TransitionView>
-                <Calendar style={{padding: 10}}/>
-              </TransitionView>
-            </DateField>
-          </div>
-          <div className={styles.endTimeField}>
-          <DateField
-              forceValidDate
-              defaultValue={"2016-05-30 15:23:34"}
-              dateFormat="YYYY-MM-DD HH:mm:ss">
-            <TransitionView>
-              <Calendar style={{padding: 10}}/>
-            </TransitionView>
-          </DateField>
-          </div>
+
+const TemporalSearch = () => {
+
+  let startDateValue = Date.now();
+
+  var startDate = [
+    <DateField className = {styles['react-date-picker-theme-hackerone']}
+        forceValidDate
+        defaultValue={startDateValue}
+        dateFormat="YYYY-MM-DD HH:mm:ss">
+      <TransitionView>
+        <Calendar style={{size: 10}} />
+      </TransitionView>
+    </DateField>
+  ];
+
+  var endDate = [
+    <DateField
+        forceValidDate
+        defaultValue={startDateValue}
+        dateFormat="YYYY-MM-DD HH:mm:ss">
+      <TransitionView>
+        <Calendar  className={styles.hackerone}/>
+      </TransitionView>
+    </DateField>
+  ];
+
+  var changeDate = [
+    <div className={styles.dateTimeField}>
+      <button className={`${styles['pure-button']} ${styles.temporalDropdown}`}>
+        Temporal
+      </button>
+    </div>
+  ]
+
+  return ( <div>
+        <div className={styles.startTimeField}>
+          {startDate}
         </div>
-)
+        <div className={styles.endTimeField}>
+          {endDate}
+        </div>
 
-
+        { changeDate }
+      </div>
+  )
+}
 
 export default TemporalSearch
-
-
