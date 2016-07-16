4d07e161a783644fb1318a1ff1bfdb313561c920
Evan McQuinn <Evan.McQuinn@colorado.edu>
2016-07-15 23:29:47 -0600

    Fixing test so it looks more like real search results. Needs more work...

--- client/test/detailsReducerSpec.js
+++ client/test/detailsReducerSpec.js
@@ -4,16 +4,15 @@ import * as detail from '../src/detail/DetailReducer'
 describe('The details reducer',() => {
   it('handles search request',() => {
     const initialState = detail.initialState
+    const testItems = new Map()
+    testItems.set('a', {id: 'a'})
     const initalAction =  { type: 'search_complete',
                               searchText: 'test',
-                              items: [{id:'1'}]}
+                              items: testItems}
 
     const result = detail.details(initialState, initalAction).toJS()
     console.log(JSON.stringify(result))
 
-    for (let [k, v] of Object.entries(result)) {
-      var cardStatus = v.cardStatus
-      cardStatus.should.equal('SHOW_FRONT')
-    }
+    result['a'].cardStatus.should.equal('SHOW_FRONT')
   })
 })
