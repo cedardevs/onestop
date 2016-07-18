73af9388d0495856f47b8934a8e78125abe4b7f1
Aaron Caldwell <aaron.caldwell@colorado.edu>
2016-07-17 18:27:05 -0600

    Use redux-immutable's combinereducers for safe combination of ImmutableJS reducers states

--- client/src/reducers/main.js
+++ client/src/reducers/main.js
@@ -1,4 +1,5 @@
 import Immutable from 'immutable'
+import { combineReducers } from 'redux-immutable'
 import search from './../search/SearchReducer'
 import results from './../result/ResultReducer'
 import details from './../detail/DetailReducer'
@@ -6,25 +7,26 @@ import facets from './../facet/FacetReducer'
 import { LOCATION_CHANGE } from 'react-router-redux'
 
 // Routing reducer
-const initialState = Immutable.Map({})
+const initialState = Immutable.fromJS({
+    locationBeforeTransitions: null
+})
 const routing = (state = initialState, action) => {
   switch (action.type) {
     case LOCATION_CHANGE:
-      state.set("routing", {locationBeforeTransitions: action.payload})
-      return state
+    return state.merge({
+            locationBeforeTransitions: action.payload
+        });
     default:
       return state
   }
 }
 
-const reducer = (state = Immutable.Map(), action) => {
-  return state.merge({
-    search: search(state.get('search'), action),
-    facets: facets(state.get('facets'), action),
-    results: results(state.get('results'), action),
-    details: details(state.get('details'), action),
-    routing: routing(state.get('routing'), action)
+const reducer = combineReducers({
+    search: search,
+    facets: facets,
+    results: results,
+    details: details,
+    routing: routing
   })
-}
 
 export default reducer
