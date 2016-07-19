113c138c6117c9ca0988d116031f98fe57f77bb3
Aaron Caldwell <aaron.caldwell@noaa.gov>
2016-07-19 10:36:20 -0600

    Update collected date format to ISO, set default to empty string

--- client/src/search/temporal/TemporalSearchComponent.jsx
+++ client/src/search/temporal/TemporalSearchComponent.jsx
@@ -2,22 +2,19 @@ import React from 'react'
 import {TransitionView, DateField, Calendar} from 'react-date-picker'
 import { DateRange } from './TemporalActions'
 import styles from '../search.css'
-import 'moment'
+import moment from 'moment'
 
 const TemporalSearch = ({onChange, currentDate}) => {
 
-  let startDateValue = new Date();
-
   const formatAndEmit = (dateString, dateSelected) => {
-    onChange(dateString + 'T00:00:00Z', dateSelected)
+    onChange(moment(dateString).format(), dateSelected)
   }
 
   var startDate = [
     <DateField
-        forceValidDate
         updateOnOk
-        defaultValue={startDateValue}
-        dateFormat="YYYY-MM-DD"
+        dateFormat="YYYY-MM-DD HH:mm"
+        defaultValue=""
         onChange={(dateString)=>{formatAndEmit(dateString, DateRange.START_DATE)}}
     >
       <TransitionView>
@@ -25,14 +22,13 @@ const TemporalSearch = ({onChange, currentDate}) => {
         />
       </TransitionView>
     </DateField>
-  ];
+  ]
 
   var endDate = [
     <DateField
-        forceValidDate
         updateOnOk
-        defaultValue={startDateValue}
-        dateFormat="YYYY-MM-DD"
+        dateFormat="YYYY-MM-DD HH:mm"
+        defaultValue=""
         onChange={(dateString)=>{formatAndEmit(dateString, DateRange.END_DATE)}}
     >
       <TransitionView>
@@ -40,7 +36,7 @@ const TemporalSearch = ({onChange, currentDate}) => {
         />
       </TransitionView>
     </DateField>
-  ];
+  ]
 
   return (<div>
         <div className={styles.startTimeField} >
