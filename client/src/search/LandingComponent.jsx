b91082001d40e6bc74b66c199470a71f70ae41b8
Aaron Caldwell <aaron.caldwell@colorado.edu>
2016-07-20 11:47:16 -0600

    Modify map button to have background image

--- client/src/search/LandingComponent.jsx
+++ client/src/search/LandingComponent.jsx
@@ -56,7 +56,7 @@ class LandingComponent extends React.Component {
         <div className={styles.temporalBox}>
           <TemporalContainer />
         </div>
-        <button id="mapButton" className={`pure-button ${styles.landingButton}`} onClick={this.toggleMap}>Map</button>
+        <button id="mapButton" className={`${styles.mapButton}`} onClick={this.toggleMap}></button>
         <ToggleDisplay show={this.state.showMap}>
           <div className={styles.mapContainer}>
             <MapContainer updated={this.state.showMap} ref='mapComponent' />
