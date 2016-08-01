7d7d764e7395c6c68a6f1e149795a962bcd61e9d
Aaron Caldwell <aaron.caldwell@noaa.gov>
2016-08-01 15:43:40 -0600

    Add new calendar with range working

--- client/src/search/temporal/TemporalSearchComponent.jsx
+++ client/src/search/temporal/TemporalSearchComponent.jsx
@@ -1,49 +1,58 @@
 import React from 'react'
-import {DateField, Calendar} from 'react-date-picker'
 import { DateRange } from './TemporalActions'
+import DayPicker, { DateUtils } from 'react-day-picker'
 import styles from './temporal.css'
 import moment from 'moment'
 
-const TemporalSearch = ({onChange, currentDate}) => {
+//const TemporalSearch = ({onChange, currentDate}) => {
 
-  const formatAndEmit = (dateString, dateSelected) => {
-    const value = dateString ? moment(dateString).format() : null
-    onChange(value, dateSelected)
+class TemporalSearch extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleDayClick = this.handleDayClick.bind(this)
+    this.handleResetClick = this.handleResetClick.bind(this)
+    this.state = this.getInitialState()
   }
 
-  var startDate = [
-    <DateField
-        key='start'
-        updateOnOk
-        dateFormat="YYYY-MM-DD HH:mm"
-        defaultValue=""
-        onChange={(dateString)=>{formatAndEmit(dateString, DateRange.START_DATE)}}
-    >
-      <Calendar/>
-    </DateField>
-  ]
+  getInitialState() {
+    return {
+      from: null,
+      to: null,
+    }
+  }
 
-  var endDate = [
-    <DateField
-        key='end'
-        updateOnOk
-        dateFormat="YYYY-MM-DD HH:mm"
-        defaultValue=""
-        onChange={(dateString)=>{formatAndEmit(dateString, DateRange.END_DATE)}}
-    >
-      <Calendar/>
-    </DateField>
-  ]
-
-  return (<div>
-        <div className={styles.startTimeField}>
-          {startDate}
-        </div>
-        <div className={styles.endTimeField}>
-          {endDate}
-        </div>
+  handleDayClick(e, day) {
+    const range = DateUtils.addDayToRange(day, this.state)
+    this.setState(range)
+  }
+  handleResetClick(e) {
+    e.preventDefault()
+    this.setState({
+      from: null,
+      to: null,
+    });
+  }
+  render() {
+    const { from, to } = this.state;
+    return (
+      <div className="RangeExample">
+        { !from && !to && <p>Please select the <strong>first day</strong>.</p> }
+        { from && !to && <p>Please select the <strong>last day</strong>.</p> }
+        { from && to &&
+          <p>
+            You chose from { moment(from).format('L') } to { moment(to).format('L') }.
+            { ' ' }<a href="#" onClick={ this.handleResetClick }>Reset</a>
+          </p>
+        }
+        <DayPicker className={styles.dateComponent}
+          ref="daypicker"
+          numberOfMonths={ 2 }
+          selectedDays={ day => DateUtils.isDayInRange(day, { from, to }) }
+          onDayClick={ this.handleDayClick }
+        />
       </div>
-  )
+    )
+  }
 }
 
 export default TemporalSearch
