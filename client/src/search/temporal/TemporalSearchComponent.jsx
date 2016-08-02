528be3c42aa3c44e79c7395f89284a0d8534a580
Aaron Caldwell <aaron.caldwell@noaa.gov>
2016-08-02 12:58:35 -0600

    New calendar with month, year and range select in place

--- client/src/search/temporal/TemporalSearchComponent.jsx
+++ client/src/search/temporal/TemporalSearchComponent.jsx
@@ -6,11 +6,49 @@ import moment from 'moment'
 
 //const TemporalSearch = ({onChange, currentDate}) => {
 
+const currentYear = (new Date()).getFullYear()
+const fromMonth = new Date(currentYear - 100, 0, 1, 0, 0)
+const toMonth = new Date()
+// Component will receive date, locale
+function YearMonthForm({ date, onChange }) {
+  const months = moment.months()
+
+  const years = []
+  for (let i = fromMonth.getFullYear(); i <= toMonth.getFullYear(); i++) {
+    years.push(i)
+  }
+
+  const handleChange = function handleChange(e) {
+    let { year, month } = e.target.form
+    onChange(new Date(year.value, month.value))
+  }
+
+  return (
+    <form className="DayPicker-Caption">
+      <select name="month" onChange={ handleChange } value={ date.getMonth() }>
+        { months.map((month, i) =>
+          <option key={ i } value={ i }>
+            { month }
+          </option>)
+        }
+      </select>
+      <select name="year" onChange={ handleChange } value={ date.getFullYear() }>
+        { years.map((year, i) =>
+          <option key={ i } value={ year }>
+            { year }
+          </option>)
+        }
+      </select>
+    </form>
+  )
+}
+
 class TemporalSearch extends React.Component {
   constructor(props) {
     super(props)
     this.handleDayClick = this.handleDayClick.bind(this)
     this.handleResetClick = this.handleResetClick.bind(this)
+    this.render = this.render.bind(this)
     this.state = this.getInitialState()
   }
 
@@ -18,37 +56,38 @@ class TemporalSearch extends React.Component {
     return {
       from: null,
       to: null,
+      initialMonth: toMonth
     }
   }
 
   handleDayClick(e, day) {
     const range = DateUtils.addDayToRange(day, this.state)
     this.setState(range)
+    console.log("Current range: " + JSON.stringify(range))
   }
+
   handleResetClick(e) {
     e.preventDefault()
     this.setState({
       from: null,
       to: null,
-    });
+    })
   }
+
   render() {
-    const { from, to } = this.state;
+    const { from, to } = this.state
     return (
       <div className="RangeExample">
-        { !from && !to && <p>Please select the <strong>first day</strong>.</p> }
-        { from && !to && <p>Please select the <strong>last day</strong>.</p> }
-        { from && to &&
-          <p>
-            You chose from { moment(from).format('L') } to { moment(to).format('L') }.
-            { ' ' }<a href="#" onClick={ this.handleResetClick }>Reset</a>
-          </p>
-        }
         <DayPicker className={styles.dateComponent}
           ref="daypicker"
-          numberOfMonths={ 2 }
-          selectedDays={ day => DateUtils.isDayInRange(day, { from, to }) }
           onDayClick={ this.handleDayClick }
+          initialMonth={ this.state.initialMonth }
+          fromMonth={ fromMonth }
+          toMonth={ toMonth }
+          selectedDays={ day => DateUtils.isDayInRange(day, { from, to }) }
+          captionElement={
+            <YearMonthForm onChange={ initialMonth => this.setState({ initialMonth }) } />
+          }
         />
       </div>
     )
