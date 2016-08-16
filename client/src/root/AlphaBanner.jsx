83d34b124223ad12f6934237d8b00cf17f440140
sghebrec <semere.ghebrechristos@noaa.gov>
2016-08-16 11:53:23 -0600

    update banner background ( to red ) and text

--- client/src/root/AlphaBanner.jsx
+++ client/src/root/AlphaBanner.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import styles from './alphaBanner.css'
 
 const AlphaBanner = () => {
-    return <div className={styles.banner}>Demonstration site for OneStop development</div>
+    return <div className={styles.banner}>Not an official US Government website - OneStop demonstration site to allow quick feedback from our stakeholders.
+                <a href="https://github.com/cires-ncei" target="_blank" ><strong> Application Source</strong></a>
+            </div>
 }
 
 export default AlphaBanner
