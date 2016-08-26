25cb505aa8d9b52874f33ee2bb721f57829a8e53
Aaron Caldwell <aaron.caldwell@colorado.edu>
2016-08-26 16:10:59 -0600

    Pass facet name, value and checked status from input

--- client/src/facet/FacetListComponent.jsx
+++ client/src/facet/FacetListComponent.jsx
@@ -6,31 +6,33 @@ import Collapse, { Panel } from 'rc-collapse'
 class FacetList extends React.Component {
   constructor(props) {
     super(props)
-    console.log(props.categories)
     this.categories = props.categories
   }
 
-  search(query) {
-    this.updateQuery(query)
-    this.submit(query)
+  updateAndSubmitSearch(e) {
+    const {name, value} = e.target.dataset
+    const selected = e.target.checked
+    // Update query
+    // Submit query
   }
 
   render() {
     let facets = []
+    let self = this
     let i = 0, j = 0
     _.forOwn(this.categories, function(v,k){
       facets.push(
         <Panel header={`${k}`} key={`${i++}`}>
           {v.map((obj)=> {
             return(<div>
-              <input className={styles.checkFacet} id={`${obj.term}`} type="checkbox"/><span className={styles.facetLabel}>{`${obj.term}`}</span>
+              <input className={styles.checkFacet} data-name={`${k}`} data-value={`${obj.term}`} id={`${k}-${obj.term}`} type="checkbox"
+               onChange={self.updateAndSubmitSearch}/><span className={styles.facetLabel}>{`${obj.term}`}</span>
               <div className={`${styles.count} ${styles.numberCircle}`}>{`(${obj.count})`}</div>
             </div>)
           })}
         </Panel>
       )
     })
-    console.log(facets)
 
     return <div>
       <div className={`${styles.facetContainer}`}>
