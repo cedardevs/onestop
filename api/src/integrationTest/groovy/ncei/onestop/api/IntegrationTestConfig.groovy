ad9bb0d726fa9d5a18a8c19d49107fdb6a29c355
Arianna Jakositz <arianna.jakositz@noaa.gov>
2016-08-30 19:08:14 +0000

    Modifications to integration tests to incorporate expression script module in embedded elasticsearch. Fixed broken integration tests

--- api/src/integrationTest/groovy/ncei/onestop/api/IntegrationTestConfig.groovy
+++ api/src/integrationTest/groovy/ncei/onestop/api/IntegrationTestConfig.groovy
@@ -1,12 +1,16 @@
 package ncei.onestop.api
 
+import org.elasticsearch.Version
 import org.elasticsearch.cluster.metadata.IndexMetaData
 import org.elasticsearch.common.settings.Settings
 import org.elasticsearch.common.util.concurrent.EsExecutors
 import org.elasticsearch.node.Node
 import org.elasticsearch.client.Client
 import org.elasticsearch.node.NodeBuilder
-import org.springframework.beans.factory.annotation.Value
+import org.elasticsearch.env.Environment
+import org.elasticsearch.node.internal.InternalSettingsPreparer
+import org.elasticsearch.plugins.Plugin
+import org.elasticsearch.script.expression.ExpressionPlugin
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.DependsOn
@@ -17,49 +21,46 @@ import org.springframework.context.annotation.Profile
 @Configuration
 class IntegrationTestConfig {
 
-    // Config constants:
-    static final String CLUSTER_NAME = 'integrationTest'
+  // Config constants:
+  static final String CLUSTER_NAME = 'integrationTest'
 
-    @Value('${elasticsearch.index.search.name}')
-    private String INDEX
+  ////////////////////////////////////////////
 
-    @Value('${elasticsearch.index.search.type}')
-    private String TYPE
+  // Bean definitions:
 
+  @Bean(name = "dataDirectory")
+  public File dataDirectory() {
+    def tmpDir = File.createTempDir()
+    return tmpDir
+  }
 
-    ////////////////////////////////////////////
+  @Bean(name = "node", destroyMethod = "close")
+  @DependsOn("dataDirectory")
+  public Node node() {
+    Settings settings = Settings.builder()
+        .put("path.home", dataDirectory().toString())
+        .put("cluster.name", CLUSTER_NAME)
+        .put("discovery.zen.ping.multicast", false)
+        .put("node.local", true)
+        .put(IndexMetaData.SETTING_NUMBER_OF_SHARDS, 1)
+        .put(IndexMetaData.SETTING_NUMBER_OF_REPLICAS, 0)
+        .put(EsExecutors.PROCESSORS, 1)
+        .build()
+    Collection<Class<? extends Plugin>> classpathPlugins = Collections.singletonList(ExpressionPlugin.class)
 
-    // Bean definitions:
-
-    @Bean(name = "dataDirectory")
-    public File dataDirectory() {
-        def tmpDir = File.createTempDir()
-        return tmpDir
-    }
-
-    @Bean(name = "node", destroyMethod = "close")
-    @DependsOn("dataDirectory")
-    public Node node() {
-        Settings settings = Settings.builder()
-                .put("path.home", dataDirectory().toString())
-                .put("cluster.name", CLUSTER_NAME)
-                .put(IndexMetaData.SETTING_NUMBER_OF_SHARDS, 1)
-                .put(IndexMetaData.SETTING_NUMBER_OF_REPLICAS, 0)
-                .put(EsExecutors.PROCESSORS, 1)
-                .build()
-
-        def node = NodeBuilder.nodeBuilder().local(true).settings(settings).build()
-        return node
-    }
+    def node = new IntegrationTestNode(InternalSettingsPreparer.prepareEnvironment(settings, null),
+        Version.CURRENT, classpathPlugins)
+    return node
+  }
 
 
-    @Bean(destroyMethod = "close")
-    @DependsOn("node")
-    public Client client() {
-        def node = node()
-        def client = node.client()
-        node.start()
-        return client
-    }
+  @Bean(destroyMethod = "close")
+  @DependsOn("node")
+  public Client client() {
+    def node = node()
+    def client = node.client()
+    node.start()
+    return client
+  }
 
 }
