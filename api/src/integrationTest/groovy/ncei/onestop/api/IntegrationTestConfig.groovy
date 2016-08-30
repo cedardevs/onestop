189a7a10fae19824e5ac008efda0c43193bc2d9b
Evan McQuinn <Evan.McQuinn@colorado.edu>
2016-08-30 10:00:49 -0600

    formatting

--- api/src/integrationTest/groovy/ncei/onestop/api/IntegrationTestConfig.groovy
+++ api/src/integrationTest/groovy/ncei/onestop/api/IntegrationTestConfig.groovy
@@ -17,49 +17,46 @@ import org.springframework.context.annotation.Profile
 @Configuration
 class IntegrationTestConfig {
 
-    // Config constants:
-    static final String CLUSTER_NAME = 'integrationTest'
+  // Config constants:
+  static final String CLUSTER_NAME = 'integrationTest'
 
-    @Value('${elasticsearch.index.search.name}')
-    private String INDEX
+  @Value('${elasticsearch.index.search.name}')
+  private String INDEX
 
-    @Value('${elasticsearch.index.search.type}')
-    private String TYPE
+  @Value('${elasticsearch.index.search.type}')
+  private String TYPE
 
+  ////////////////////////////////////////////
+  // Bean definitions:
 
-    ////////////////////////////////////////////
+  @Bean(name = "dataDirectory")
+  public File dataDirectory() {
+    def tmpDir = File.createTempDir()
+    return tmpDir
+  }
 
-    // Bean definitions:
+  @Bean(name = "node", destroyMethod = "close")
+  @DependsOn("dataDirectory")
+  public Node node() {
+    Settings settings = Settings.builder()
+        .put("path.home", dataDirectory().toString())
+        .put("cluster.name", CLUSTER_NAME)
+        .put(IndexMetaData.SETTING_NUMBER_OF_SHARDS, 1)
+        .put(IndexMetaData.SETTING_NUMBER_OF_REPLICAS, 0)
+        .put(EsExecutors.PROCESSORS, 1)
+        .build()
 
-    @Bean(name = "dataDirectory")
-    public File dataDirectory() {
-        def tmpDir = File.createTempDir()
-        return tmpDir
-    }
+    def node = NodeBuilder.nodeBuilder().local(true).settings(settings).build()
+    return node
+  }
 
-    @Bean(name = "node", destroyMethod = "close")
-    @DependsOn("dataDirectory")
-    public Node node() {
-        Settings settings = Settings.builder()
-                .put("path.home", dataDirectory().toString())
-                .put("cluster.name", CLUSTER_NAME)
-                .put(IndexMetaData.SETTING_NUMBER_OF_SHARDS, 1)
-                .put(IndexMetaData.SETTING_NUMBER_OF_REPLICAS, 0)
-                .put(EsExecutors.PROCESSORS, 1)
-                .build()
-
-        def node = NodeBuilder.nodeBuilder().local(true).settings(settings).build()
-        return node
-    }
-
-
-    @Bean(destroyMethod = "close")
-    @DependsOn("node")
-    public Client client() {
-        def node = node()
-        def client = node.client()
-        node.start()
-        return client
-    }
+  @Bean(destroyMethod = "close")
+  @DependsOn("node")
+  public Client client() {
+    def node = node()
+    def client = node.client()
+    node.start()
+    return client
+  }
 
 }
