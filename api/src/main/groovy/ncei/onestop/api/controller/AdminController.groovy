feb145c6aabfc38c00c688bb8a54cb0b45130815
Evan McQuinn <Evan.McQuinn@colorado.edu>
2016-09-12 14:20:40 -0600

    Move the reindex endpoint from /search/reindex to /admin/reindex, and move the implementation to a new AdminController

---
+++ api/src/main/groovy/ncei/onestop/api/controller/AdminController.groovy
@@ -0,0 +1,29 @@
+package ncei.onestop.api.controller
+
+import groovy.util.logging.Slf4j
+import ncei.onestop.api.service.ETLService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET
+import static org.springframework.web.bind.annotation.RequestMethod.PUT
+
+@Slf4j
+@RestController
+class AdminController {
+
+  private ETLService etlService
+
+  @Autowired
+  AdminController(ETLService etlService) {
+    this.etlService = etlService
+  }
+
+  @RequestMapping(path = '/admin/reindex', method = [GET, PUT], produces = 'application/json')
+  Map reindex() {
+    etlService.reindexAsync()
+    return [acknowledged: true]
+  }
+
+}
