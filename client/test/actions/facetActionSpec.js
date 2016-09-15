e4ca03c01de13a3aa055c21cac6f38f3a439bca1
Aaron Caldwell <aaron.caldwell@noaa.gov>
2016-09-15 17:13:41 -0600

    Fix tests broken mostly by name changes and new function params

--- client/test/actions/facetActionSpec.js
+++ client/test/actions/facetActionSpec.js
@@ -8,25 +8,25 @@ describe('The facet action', function () {
 
   const metadata = {took: 2, total: 1, facets: {science: [{term: "Land Surface", count: 2}, {term: "Land Surface > Topography", count: 2}]}}
 
-  it('process Metadata  ', function () {
-    const facetAction = actions.processMetadata(metadata)
-    const expectedAction =  { type: 'METADATA_RECEIVED', metadata: metadata}
+  it('process new facets  ', function () {
+    const facetAction = actions.facetsReceived(metadata)
+    const expectedAction =  { type: 'FACETS_RECEIVED', metadata: metadata, processFacets: false}
 
     facetAction.should.deep.equal(expectedAction)
   })
 
-  it('update Facets Selected ', function () {
+  it('update facets selected ', function () {
     const facet = {name: "science", value: "Atmosphere", selected: true}
     const facetAction = actions.updateFacetsSelected(facet)
-    const expectedAction =   { type: 'UPDATE_FACETS', facet: facet }
+    const expectedAction =   { type: 'UPDATE_FACETS', facetsSelected: facet }
 
     facetAction.should.deep.equal(expectedAction)
   })
 
-  it('clear Facets ', function () {
+  it('clear facets ', function () {
     const facet = {name: "science", value: "Atmosphere", selected: true}
     const facetAction = actions.updateFacetsSelected(facet)
-    const expectedAction = {type: 'UPDATE_FACETS', facet: facet}
+    const expectedAction = {type: 'UPDATE_FACETS', facetsSelected: facet}
 
     facetAction.should.deep.equal(expectedAction)
 
