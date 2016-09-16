2487d75b9b116073c1f5145934aef9401cc74d67
Aaron Caldwell <aaronjcaldwell@gmail.com>
2016-09-16 15:02:11 -0600

    Reload facet checks on page refresh

--- client/src/query.js
+++ client/src/query.js
@@ -1,6 +1,9 @@
 import queryString from 'query-string'
+import _ from 'lodash'
+import Immutable from 'immutable'
 import store from './store'
 import { triggerSearch } from './search/SearchActions'
+import { modifySelectedFacets } from './search/facet/FacetActions'
 
 const loadQuery = () => {
   const urlString = document.location.hash
@@ -12,11 +15,27 @@ const loadQuery = () => {
       delete(queryParams[key])
     }
   }
+  
   let params = JSON.stringify(queryParams)
   if(params !== '{}') {
-    store.dispatch(triggerSearch(params))
+    store.dispatch(triggerSearch(params, null, loadFacets(queryParams.filters)))
   }
+}
 
+const loadFacets = filters => {
+  const facets = _.filter(filters, (o) => {
+    return o.type === 'facet'
+  })
+  if (facets) {
+    let selectedFacets = Immutable.Map()
+    for (let facet of facets) {
+      for (let value of facet.values) {
+        selectedFacets = selectedFacets.setIn([facet.name, value, 'selected'], true)
+      }
+    }
+    store.dispatch(modifySelectedFacets(selectedFacets))
+  }
+  return !!facets
 }
 
 export default loadQuery
