16593b3dd696d9af143d1e749aa230882f7f2ec1
Arianna Jakositz <arianna.jakositz@noaa.gov>
2016-10-21 21:56:32 +0000

    ansible vault & elasticsearch shield

--- api/src/main/groovy/ncei/onestop/api/ApplicationConfig.groovy
+++ api/src/main/groovy/ncei/onestop/api/ApplicationConfig.groovy
@@ -1,5 +1,7 @@
 package ncei.onestop.api
 
+import org.elasticsearch.common.settings.Settings
+import org.elasticsearch.shield.ShieldPlugin
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -17,9 +19,21 @@ class ApplicationConfig {
   @Value('${elasticsearch.host}')
   String elasticHost
 
+  @Value('${shield.user_ro}')
+  String user
+
+  @Value('${shield.pass_ro}')
+  String password
+
   @Bean(destroyMethod = 'close')
-  public Client client() {
-    TransportClient.builder().addPlugin(DeleteByQueryPlugin.class).build()
+  public Client searchClient() {
+    TransportClient.builder()
+        .addPlugin(DeleteByQueryPlugin)
+        .addPlugin(ShieldPlugin)
+        .settings(Settings.builder()
+          .put('shield.user', "${user}:${password}"))
+          //.put('shield.transport.ssl', 'true')
+        .build()
         .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(elasticHost), elasticPort))
   }
 
