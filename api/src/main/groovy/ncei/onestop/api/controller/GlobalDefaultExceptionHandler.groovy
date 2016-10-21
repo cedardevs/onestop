e7fd00d4368a1f8827028e49fef84fe2b387d1dc
Evan McQuinn <Evan.McQuinn@colorado.edu>
2016-10-21 13:14:24 -0600

    Extend ResponseEntityExceptionHandler and override the formatting of all error messages to follow JSON-API.

--- api/src/main/groovy/ncei/onestop/api/controller/GlobalDefaultExceptionHandler.groovy
+++ api/src/main/groovy/ncei/onestop/api/controller/GlobalDefaultExceptionHandler.groovy
@@ -2,37 +2,59 @@ package ncei.onestop.api.controller
 
 import groovy.util.logging.Slf4j
 import org.springframework.core.annotation.AnnotationUtils
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.context.request.WebRequest
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
 import javax.servlet.http.HttpServletRequest
 
 @Slf4j
 @ControllerAdvice
-class GlobalDefaultExceptionHandler {
+class GlobalDefaultExceptionHandler extends ResponseEntityExceptionHandler {
 
-  @ExceptionHandler(value = Exception)
-  @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
-  @ResponseBody Map defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
-    // If annotated with @ResponseStatus rethrow it so the framework can handle it
-    if (AnnotationUtils.findAnnotation(e.class, ResponseStatus)) {
-      throw e
-    }
+  @Override
+  protected ResponseEntity<Object> handleExceptionInternal(
+      Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
 
-    log.error("An exception occurred while handling a request to [${req.requestURL}]:", e)
-
-    return [
+    def result = [
         errors: [
             [
-                status: "500",
-                title : "Sorry, something has gone wrong.",
-                detail: "Looks like something isn't working on our end, please try again later."
+                status: status.value() as String,
+                title : buildTitle(status),
+                detail: buildDetail(status, ex)
             ]
+        ],
+        meta: [
+            timestamp: System.currentTimeMillis(),
+            request: request?.getDescription(false),
+            parameters: request?.parameterMap
         ]
     ]
+
+    return super.handleExceptionInternal(ex, result, headers, status, request)
+  }
+
+  private static buildTitle(HttpStatus status) {
+    status.is5xxServerError() ?
+        "Sorry, something has gone wrong" :
+        status.reasonPhrase
+  }
+
+  private static buildDetail(HttpStatus status, Exception e = null) {
+    status.is5xxServerError() ?
+        "Looks like something isn't working on our end, please try again later" :
+        sanitizeExceptionMessage(e)
+  }
+
+  private static sanitizeExceptionMessage(Exception e) {
+    def message = e?.message ?: 'Bad Request'
+    return message.tokenize(':').first()
   }
 
 }
