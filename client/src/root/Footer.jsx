e1fc0ba04cdbc9c03edcac53e57bb98b847c8b3a
Aaron Caldwell <aaronjcaldwell@gmail.com>
2016-10-24 14:22:40 -0600

    Remove old footer layout info. It can be retrieved via git if needed

--- client/src/root/Footer.jsx
+++ client/src/root/Footer.jsx
@@ -51,56 +51,13 @@ let Footer = ({dispatch}) => {
     return (
       <div className={styles.container}>
           <div className={styles.panel}>
-              {/*<div className={`pure-g pure-u-lg`}>*/}
-                  {/*<div className={`pure-u-1-2 ${styles.noaaLinks}`}>*/}
-                      {/*<a href="//www.noaa.gov/about-our-agency" className={styles.about}>About our agency</a>*/}
-                      {/*<a href="//www.noaa.gov/news-features" className={styles.about}>News and features</a>*/}
-                      {/*<a href="//www.noaa.gov/our-work" className={styles.about}>Our work</a>*/}
-                  {/*</div>*/}
-                  {/*<div className={`${styles.feedback} pure-u-1-2 pure-u-md-1-2`}>*/}
-                      {/*<div className={styles.socialMedia}>*/}
-                          {/*Stay connected:*/}
-                          {/*<a href="https://twitter.com/NOAA"*/}
-                             {/*className={`${styles.socialMediaLogo} ${styles.twitterLogo}`}>*/}
-                              {/*<span className={styles.hidden}>Share to Twitter</span></a>*/}
-                          {/*<a href="https://www.facebook.com/NOAA"*/}
-                             {/*className={`${styles.socialMediaLogo} ${styles.facebookLogo}`}>*/}
-                              {/*<span className={styles.hidden}>Share to Facebook</span></a>*/}
-                          {/*<a href="https://www.instagram.com/noaa"*/}
-                             {/*className={`${styles.socialMediaLogo} ${styles.instagramLogo}`}>*/}
-                              {/*<span className={styles.hidden}>Share to Instagramm</span></a>*/}
-                          {/*<a href="https://www.youtube.com/user/noaa"*/}
-                             {/*className={`${styles.socialMediaLogo} ${styles.youtubeLogo}`}>*/}
-                              {/*<span className={styles.hidden}>Share to Youtube</span></a>*/}
-                      {/*</div>*/}
-
-                      {/*<form className={`${styles.feedbackForm} pure-form`} method="link"*/}
-                            {/*action="https://www.nos.noaa.gov/survey">*/}
-                          {/*<div className={styles.rowElement}>How are we doing?</div>*/}
-                          {/*<button type="submit"*/}
-                                  {/*className={`pure-button pure-button-primary ${styles.rowElement}`}>*/}
-                              {/*Feedback*/}
-                          {/*</button>*/}
-                      {/*</form>*/}
-                  {/*</div>*/}
-              {/*</div>*/}
               <div className={'pure-g'}>
                   <div className={`pure-u-2-5 ${styles.logoPanel}`}>
-                      {/*   <a href="https://sciapps.colorado.edu" className={`${styles.ciresLogo} ${styles.rowElement}`}>
-                          <span className={styles.hidden}>OneStop Home</span>
-                      </a>*/}
-                      {/* <span className={`${styles.rowElement} ${styles.slogan}`}>CIRES-NCEI OneStop Demonstration Site</span>*/}
                      <a href="//www.noaa.gov/" className={`${styles.noaaLogo} ${styles.rowElement}`}>
                           <span className={styles.hidden}>NOAA Home</span>
                       </a>
                      <span className={`${styles.rowElement} ${styles.slogan}`}>Science. Service. Stewardship.</span>
                   </div>
-
-                  {/*<div className={`pure-u-3-5 ${styles.externalLinksPanel}`}>*/}
-                      {/*<ul className={`${styles.resourceList}`}>*/}
-                          {/*{externalResources}*/}
-                      {/*</ul>*/}
-                  {/*</div>*/}
               </div>
           </div>
       </div>
