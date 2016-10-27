a4e46be0845bc04ab607bb1f210b3a81c7c5a738
Evan McQuinn <Evan.McQuinn@colorado.edu>
2016-10-27 12:05:32 -0600

    Add a simple module to load a config file if available and use it to show the disclaimer.

--- client/src/root/AlphaBanner.jsx
+++ client/src/root/AlphaBanner.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import styles from './alphaBanner.css'
+import config from '../config'
 
-const AlphaBanner = () => {
-    return <div className={styles.banner}>Not an official US Government website - OneStop demonstration site to allow quick feedback from our stakeholders.
-            </div>
+class AlphaBanner extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.message = null
+  }
+
+  componentDidMount() {
+    config.then((config => {
+      if (config && config.disclaimer) {
+        this.message = 'Not an official US Government website - OneStop demonstration site to allow quick feedback from our stakeholders.'
+      }
+    }))
+  }
+
+  render() {
+    return this.message ? <div className={styles.banner}>{this.message}</div> : null
+  }
+
 }
 
 export default AlphaBanner
