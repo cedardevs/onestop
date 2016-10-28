effadcef0b1c4eb35037a38fffba66a951248d7d
Evan McQuinn <Evan.McQuinn@colorado.edu>
2016-10-28 13:09:52 -0600

    Add a script to help with releasing builds to bintray

---
+++ releaseBuild.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [[ $# != 4 ]]; then
+  echo "Usage: $0 username apiKey buildNumber releaseVersion"
+  exit 1
+fi
+
+curl -X POST -u "$1:$2" "http://oss.jfrog.org/api/plugins/build/promote/snapshotsToBintray/onestop/$3?params=releaseVersion=$4"s
