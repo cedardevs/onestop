bf90a0fad137a53514a631c6ccf4cfd804596f88
Arianna Jakositz <arianna.jakositz@noaa.gov>
2016-10-31 20:26:50 +0000

    Removing a debug line

--- client/src/search/SearchActions.js
+++ client/src/search/SearchActions.js
@@ -78,7 +78,6 @@ export const triggerSearch = (testing) => {
         })
         .then(response => response.json())
         .then(json => {
-          console.log("JSON: " + json)
           dispatch(facetsReceived(json.meta))
           dispatch(completeSearch(assignResourcesToMap(json.data)))
           dispatch(hideLoading())
