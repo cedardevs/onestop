16df1ec16dbe1f2866b161252985f917ba8c674c
Evan McQuinn <Evan.McQuinn@colorado.edu>
2016-11-08 18:05:18 -0700

    Add a fetchGranules action which retrieves granules from the api

--- client/src/granules/GranulesActions.js
+++ client/src/granules/GranulesActions.js
@@ -1,3 +1,7 @@
+import fetch from 'isomorphic-fetch'
+import { showLoading, hideLoading } from '../loading/LoadingActions'
+import { showErrors } from '../error/ErrorActions'
+
 export const TOGGLE_GRANULE_FOCUS = 'toggle_granule_focus'
 export const TOGGLE_COLLECTION_SELECTION = 'toggle_collection_selection'
 export const FETCHING_GRANULES = 'fetching_granules'
@@ -35,4 +39,64 @@ export const fetchedGranules = (granuleList) => {
     type: FETCHED_GRANULES,
     granules: granuleList
   }
+}
+
+export const fetchGranules = () => {
+  return (dispatch, getState) => {
+    if (getState().getIn(['granules', 'inFlight'])) {
+      return Promise.resolve() // let the calling code know there's nothing to wait for
+    }
+
+    let selectedCollections = getState().getIn(['granules', 'selectedCollections']).toJS()
+    if (!selectedCollections) {
+      return Promise.resolve()
+    }
+    let searchBody = JSON.parse(getState().getIn(['search', 'requestBody']) || '{}')
+    searchBody.filters = searchBody.filters || []
+    searchBody.filters.push({"type": "collection", "values": selectedCollections})
+    searchBody.facets = false
+    searchBody = JSON.stringify(searchBody)
+
+    const apiHost = getState().get('apiHost') || ''
+    const apiRoot = apiHost + '/onestop/api/search'
+    const fetchParams = {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: searchBody
+    }
+
+    dispatch(showLoading())
+    dispatch(fetchingGranules())
+
+    return fetch(apiRoot, fetchParams)
+        .then(response => {
+          if (response.status < 200 || response.status >= 400) {
+            var error = new Error(response.statusText)
+            error.response = response
+            throw error
+          } else {
+            return response
+          }
+        })
+        .then(response => response.json())
+        .then(json => {
+          dispatch(fetchedGranules(json.data))
+          dispatch(hideLoading())
+        })
+        .catch(ajaxError => {
+          ajaxError.response.json().then(errorJson => handleErrors(dispatch, errorJson))
+        })
+        .catch(jsError => {
+          handleErrors(dispatch, jsError)
+        })
+  }
+}
+
+const handleErrors = (dispatch, e) => {
+  dispatch(hideLoading())
+  dispatch(showErrors(e.errors || e))
+  dispatch(fetchedGranules([]))
 }
