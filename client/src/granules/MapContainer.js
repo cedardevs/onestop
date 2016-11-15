571bbf2f3190fcf4cc5b4d25d8692ca420140794
Aaron Caldwell <aaron.caldwell@noaa.gov>
2016-11-15 08:41:51 -0700

    Add new granule map container w/ placeholder geoJson converter function

---
+++ client/src/granules/MapContainer.js
@@ -0,0 +1,36 @@
+import { connect } from 'react-redux'
+import MapComponent from './search/map/MapComponent'
+import { toggleGranuleFocus } from './GranulesActions'
+
+const mapStateToProps = (state) => {
+  let granules = state.getIn(['granules', 'granules'])
+  if (!granules.isEmpty()){
+    granules.forEach((data, id)=> {
+      granules.update(id, convertToGeoJson(data))
+    })
+  }
+  return {
+    geoJsonFeatures: granules.toJS()
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    toggleGeometryFocus: id => toggleGranuleFocus(id)
+  }
+}
+
+const MapContainer = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(MapComponent)
+
+const convertToGeoJson = recordData => {
+  return {
+    type: "Feature",
+    properties: {},
+    geometry: recordData.spatialBounding.coordinates
+  }
+}
+
+export default MapContainer
