d15963793d9e300dce19d7cf7d62376f1ab23a61
Arianna Jakositz <arianna.jakositz@noaa.gov>
2016-11-17 16:19:59 -0700

    Facet menu exists only for non-granules views, and can be collapsed with a button

--- client/src/result/ResultLayout.jsx
+++ client/src/result/ResultLayout.jsx
@@ -6,14 +6,73 @@ import FacetContainer from '../search/facet/FacetContainer'
 class ResultLayout extends React.Component {
   constructor(props) {
     super(props)
+
+    this.toggleFacetMenu = this.toggleFacetMenu.bind(this)
+    this.renderFacetMenu = this.renderFacetMenu.bind(this)
+    this.renderFacetButton = this.renderFacetButton.bind(this)
+    this.facetButtonImage = this.facetButtonImage.bind(this)
+    this.renderResultsContainer = this.renderResultsContainer.bind(this)
+
+    this.location = props.location.pathname
+    this.collapseFacetMenu = false
+  }
+
+  componentWillUpdate(nextProps) {
+    this.location = nextProps.location.pathname
+  }
+
+  toggleFacetMenu() {
+    this.collapseFacetMenu = !this.collapseFacetMenu
+    this.forceUpdate()
+  }
+
+  renderFacetMenu() {
+    if(this.location.includes("granules") || this.collapseFacetMenu) {
+      return <div display="none"></div>
+    }
+    else {
+      return <div className={`pure-u-10-24 pure-u-md-7-24 pure-u-lg-5-24 ${styles.facetSideBar}`}>
+        <FacetContainer/>
+      </div>
+    }
+  }
+
+  renderFacetButton() {
+    if(this.location.includes("granules")) {
+      return <div display="none"></div>
+    }
+    else {
+      return <div className={"pure-u-1-24"}>
+        <button id="facetButton" className={`pure-button ${styles.facetButton}`} onClick={this.toggleFacetMenu}>
+          <i className={`${this.facetButtonImage()}`}></i>
+        </button>
+      </div>
+    }
+  }
+
+  facetButtonImage() {
+    if(this.collapseFacetMenu) {
+      return "fa fa-arrow-right"
+    }
+    else {
+      return "fa fa-arrow-left"
+    }
+  }
+
+  renderResultsContainer() {
+    if(this.location.includes("granules") || this.collapseFacetMenu) {
+      return "pure-u-23-24"
+    }
+    else {
+      return "pure-u-13-24 pure-u-md-16-24 pure-u-lg-18-24"
+    }
   }
 
   render() {
-    return <div id="layout" className={styles.mainWindow}>
-      <div className={styles.facetSideBar}>
-        <FacetContainer/>
-      </div>
-      <div className={styles.resultsContainer}>
+    return <div id="layout" className={`pure-g ${styles.mainWindow}`}>
+      {this.renderFacetMenu()}
+      {this.renderFacetButton()}
+      <div className={`${this.renderResultsContainer()} ${styles.resultsContainer}`}>
         <div className={styles.breadCrumbs}>
           <Breadcrumbs routes={this.props.routes} params={this.props.params}/>
         </div>
