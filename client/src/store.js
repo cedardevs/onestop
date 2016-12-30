818bf11f0021a62d90c413ec5141e5f7265f86f1
Aaron Caldwell <AaronJCaldwell@gmail.com>
2016-12-29 23:55:35 -0700

    Update store and banner for seamless

--- client/src/store.js
+++ client/src/store.js
@@ -1,11 +1,11 @@
 import {createStore, applyMiddleware} from 'redux'
 import { routerMiddleware } from 'react-router-redux'
 import { hashHistory } from 'react-router'
-import Immutable from 'immutable'
+import Immutable from 'seamless-immutable'
 import thunk from 'redux-thunk'
 import reducer from './reducer'
 
-const initialState = Immutable.Map()
+const initialState = Immutable({})
 const store = createStore(reducer, initialState,
     applyMiddleware(
         thunk,
