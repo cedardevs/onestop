e93945f58018daf6466960787c6b80ef006c5ace
Aaron Caldwell <AaronJCaldwell@gmail.com>
2017-01-24 08:12:05 -0700

    Clean up store, remove initial state logic

--- client/src/store.js
+++ client/src/store.js
@@ -4,6 +4,7 @@ import { hashHistory } from 'react-router'
 import Immutable from 'seamless-immutable'
 import thunk from 'redux-thunk'
 import reducer from './reducers/reducer'
+import { initialState } from './utils/refreshUtils'
 import _ from 'lodash'
 
 const store = createStore(reducer, initialState(),
@@ -11,28 +12,5 @@ const store = createStore(reducer, initialState(),
         thunk,
         routerMiddleware(hashHistory)
     ))
-
-function initialState() {
-  const initState = searchAndFacetState()
-  if (!_.isEmpty(initState)){
-    return Immutable({searchAndFacets: initState})
-  } else {
-    return Immutable({})
-  }
-}
-
-function searchAndFacetState() {
-  if(typeof document !== "undefined"){
-    const urlString = decodeURIComponent(document.location.hash)
-    if (urlString.includes('?')){
-      const urlArray = urlString.split('?')
-      return JSON.parse(urlArray[1])
-    } else {
-      return {}
-    }
-  } else {
-    return {}
-  }
-}
 
 export default store
