1b36e1c99c14bb8f789eb3d9ad761d7e1192aa6d
Evan McQuinn <evan.mcquinn@colorado.edu>
2017-01-24 16:01:58 -0700

    Restore empty request checking

--- client/src/search/SearchActions.js
+++ client/src/search/SearchActions.js
@@ -60,7 +60,7 @@ export const triggerSearch = (testing) => {
 
     const searchBody = assembleSearchRequestString(state)
     // To avoid returning all results when hitting search w/empty fields
-    if (_.isEmpty(JSON.parse(searchBody).queries)) {
+    if (!searchBody) {
       return Promise.resolve()
     }
     dispatch(showLoading())
