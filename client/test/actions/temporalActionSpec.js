62db8e67b583feaf5ebd64847117e398e2c75eac
Aaron Caldwell <AaronJCaldwell@gmail.com>
2017-01-25 09:54:02 -0700

    Update temporal action spec to reflect consolidated temporal action

--- client/test/actions/temporalActionSpec.js
+++ client/test/actions/temporalActionSpec.js
@@ -5,18 +5,19 @@ describe('The datetime action', function () {
 
   const datetime = '2016-07-25T15:45:00-06:00'
 
-  it('set start date time ', function () {
-    const temporalAction = actions.startDate(datetime);
-    const expectedAction =  { type: 'START_DATE', datetime: '2016-07-25T15:45:00-06:00'}
+  it('sets start date time ', function () {
+    const temporalAction = actions.updateDateRange(datetime, '');
+    const expectedAction =  { type: 'UPDATE_DATE_RANGE',
+      startDate: '2016-07-25T15:45:00-06:00', endDate: ''}
 
     temporalAction.should.deep.equal(expectedAction)
   })
 
-  it('set end date time ', function () {
-    const temporalAction = actions.endDate(datetime);
-    const expectedAction =  { type: 'END_DATE', datetime: '2016-07-25T15:45:00-06:00'}
+  it('sets end date time ', function () {
+    const temporalAction = actions.updateDateRange('', datetime);
+    const expectedAction =  { type: 'UPDATE_DATE_RANGE',
+      startDate: '', endDate: '2016-07-25T15:45:00-06:00' }
 
     temporalAction.should.deep.equal(expectedAction)
   })
 })
-
