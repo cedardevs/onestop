300e57d27ac794c61756942d164b31d98b109984
Evan McQuinn <evan.mcquinn@colorado.edu>
2017-01-25 17:25:59 -0700

    Let's simplify this a bit...

--- client/src/history.js
+++ client/src/history.js
@@ -2,20 +2,8 @@ import { syncHistoryWithStore } from 'react-router-redux'
 import { hashHistory } from 'react-router'
 import store from './store.js'
 
-// Create enhanced history object for router
-const createSelectLocationState = () => {
-  let prevRoutingState, prevRoutingStateJS
-  return (state) => {
-    const routingState = state.routing
-    if (typeof prevRoutingState === 'undefined' || prevRoutingState !== routingState) {
-      prevRoutingStateJS = routingState
-    }
-    return prevRoutingStateJS
-  }
-}
-
 const history = syncHistoryWithStore(hashHistory, store, {
-  selectLocationState: createSelectLocationState()
+  selectLocationState: (state) => state.routing
 })
 
 export default history
