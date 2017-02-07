16de7648e4e081cf7e4b0230db1e6a297fbaae80
Evan McQuinn <evan.mcquinn@colorado.edu>
2017-02-06 17:42:14 -0700

    First pass at adding links to footer

--- client/src/root/Footer.jsx
+++ client/src/root/Footer.jsx
@@ -1,65 +1,53 @@
 import React from 'react'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import styles from './footer.css'
 import 'purecss'
 
-let Footer = ({dispatch}) => {
+let Footer = () => {
 
-    var externalResources = [
-        {
-            href: "//www.noaa.gov/protecting-your-privacy",
-            text: "Protecting your Privacy"
-        }, {
-            href: "//www.noaa.gov/foia-freedom-information-act",
-            text: "FOIA"
-        }, {
-            href: "//www.cio.noaa.gov/services_programs/info_quality.html",
-            text: "Information Quality"
-        }, {
-            href: "//www.noaa.gov/disclaimer",
-            text: "Disclaimer"
-        }, {
-            href: "https://www.usa.gov/",
-            text: "USA.gov"
-        }, {
-            href: "https://www.ready.gov/",
-            text: "Ready.gov"
-        }, {
-            href: "//www.eeo.noaa.gov/noaa/",
-            text: "EEO"
-        }, {
-            href: "//www.homelandsecurity.noaa.gov/",
-            text: "Employee Check-In"
-        }, {
-            href: "//www.noaa.gov/contact-us",
-            text: "Contact Us"
-        }, {
-            href: "https://nsd.rdc.noaa.gov/nsd",
-            text: "Staff Directory"
-        }, {
-            href: "//www.noaa.gov/need-help",
-            text: "Need help?"
-        }
-    ]
-    externalResources = externalResources.map(function (el, idx) {
-        return <li key={idx} className={`${styles.externalResource}
-        ${styles.rowElement}`}><a href={`${el.href}`}
-        className={`${styles.footerLink}`}>{`${el.text}`}</a>
-        </li>
-    })
+  const links = [
+    {
+      href: "//www.ncdc.noaa.gov/about-ncdc/privacy",
+      text: "Privacy Policy"
+    }, {
+      href: "//www.rdc.noaa.gov/~foia/",
+      text: "Freedom of Information Act"
+    }, {
+      href: "//www.cio.noaa.gov/services_programs/info_quality.html",
+      text: "Information Quality"
+    }, {
+      href: "//www.noaa.gov/disclaimer.html",
+      text: "Disclaimer"
+    }, {
+      href: "//www.ncdc.noaa.gov/survey",
+      text: "Take Our Survey"
+    }, {
+      href: "//www.commerce.gov/",
+      text: "Department of Commerce"
+    }, {
+      href: "//www.noaa.gov/",
+      text: "NOAA"
+    }, {
+      href: "//www.nesdis.noaa.gov/",
+      text: "NESDIS"
+    }
+  ]
 
-    return (
-      <div className={styles.container}>
-          <div className={'pure-g'}>
-              <div className={`pure-u-2-5 ${styles.logoPanel}`}>
-                 <a href="//www.noaa.gov/" className={`${styles.noaaLogo} ${styles.rowElement}`}>
-                      <span className={styles.hidden}>NOAA Home</span>
-                  </a>
-                 <span className={`${styles.rowElement} ${styles.slogan}`}>Science. Service. Stewardship.</span>
-              </div>
+  return (
+      <div className={styles.footer}>
+        <div className={'pure-g'}>
+          <div className={`pure-u-1-3 ${styles.logoPanel}`}>
+            <a href="//www.noaa.gov/" title="NOAA Home" className={`${styles.noaaLogo}`}/>
+            <span className={`${styles.slogan}`}>Science. Service. Stewardship.</span>
           </div>
+          <div className={`pure-u-2-3`}>
+            <ul className={`${styles.footerLinks}`}>
+              {links.map((link, i) => <li key={i}><a href={link.href} title={link.text}>{link.text}</a></li>)}
+            </ul>
+          </div>
+        </div>
       </div>
-    )
+  )
 }
 
 Footer = connect()(Footer)
