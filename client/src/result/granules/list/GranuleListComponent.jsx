a37725cde3501e53a963c3bcb571986dd6ba7f6c
Aaron Caldwell <AaronJCaldwell@gmail.com>
2017-02-08 15:33:03 -0700

    First pass on granule download badges and legend

--- client/src/result/granules/list/GranuleListComponent.jsx
+++ client/src/result/granules/list/GranuleListComponent.jsx
@@ -4,26 +4,87 @@ import MapContainer from '../MapContainer'
 import styles from './list.css'
 
 class GranuleList extends React.Component {
+
   constructor(props) {
     super(props)
+    this.loadLinks = this.loadLinks.bind(this)
+    this.identifyFileSource = this.identifyFileSource.bind(this)
+    this.updateState = this.updateState.bind(this)
+    this.state = {
+      records: [],
+      dataSource: [
+        { id: 'OPeNDAP', letter: 'O', color: 'green', legend: false,
+          names: ['dap', 'opendap']},
+        { id: 'Download', letter: 'D', color: 'blue', legend: false,
+          names: ['download']},
+        { id: 'FTP', letter: 'F', color: 'red', legend: false,
+          names: ['ftp']},
+        { id: 'Web', letter: 'W', color: 'purple', legend: false,
+          names: ['http', 'https']},
+        { id: 'THREDDS', letter: 'T', color: 'grey', legend: false,
+          names: ['thredds']}]}
   }
 
-  render() {
-    const rows = []
-    const dataFormats = 'Data Formats TBD'
-    const tags = 'Tags TBD'
-    // FIXME Which granule fields are being displayed in table?
+  componentWillReceiveProps(nextProps) {
+    this.updateState(nextProps)
+  }
 
-    _.forEach(this.props.results, (value, key) => {
+  updateState(props) {
+    const rows = []
+    _.forEach(props.results, (value, key) => {
       rows.push(
         <tr key={key} onMouseOver={() => this.props.onMouseOver(key)} onMouseLeave={() => this.props.onMouseOver(key)}>
           <td>{value.title}</td>
-          {/*<td>{value.modifiedDate}</td>*/}
-          {/*<td>{dataFormats}</td>*/}
-          {/*<td>{tags}</td>*/}
+          <td>{this.loadLinks(value.links)}</td>
         </tr>
       )
     })
+    this.setState({records: rows})
+  }
+
+  loadLinks(links) {
+    if (!links || !links.length) { return <div></div> }
+    return <div className={'pure-g'}>{links.map((link) => this.linkBadge(link))}</div>
+  }
+
+  linkBadge({linkName, linkUrl}) {
+    const protocol = linkName ? _.toLower(linkName) : ''
+    const {letter, color} = this.identifyFileSource(protocol)
+    return <a href={linkUrl}
+              className={`pure-u-1 pure-u-md-1-6 pure-u-lg-1-12
+                ${styles.letterCircle}`}
+                style={{background: color}}>
+              {letter}
+            </a>
+  }
+
+  identifyFileSource(protocol) {
+    const { dataSource } = this.state
+    let finalSource
+    _.forEach(dataSource, (source, idx)  => {
+      if (_.includes(source.names, protocol)) {
+        finalSource = source
+        dataSource[idx].legend = true
+        this.setState(dataSource)
+      }
+    })
+    return finalSource || {letter: '?', color: 'maroon'}
+  }
+
+  render() {
+    const { records, dataSource } = this.state
+    console.log(dataSource)
+    const legendRows = _.filter(dataSource, source => source.legend )
+    console.log(legendRows)
+    const legend = <div className={styles.legend}>{legendRows.map( row => {
+      return <div className={`pure-u-sm-1-3 pure-u-md-1-6`}>
+        <div className={`${styles.letterCircle} ${styles.legendRow}`}
+          style={{background: row.color}}>
+          {row.letter}
+        </div>
+        <div className={styles.legendRow}>{row.id}</div>
+      </div>
+    })}</div>
 
     return (
       <div>
@@ -40,16 +101,15 @@ class GranuleList extends React.Component {
               <div className={`${styles.description}`}>
                 {this.props.selectedCollection.description}
               </div>
+              <div className='pure-g'>{legend}</div>
               <table className={`pure-table ${styles.table}`}>
                 <thead>
                 <tr>
                   <th>Title</th>
-                  {/*<th>Date Modified</th>*/}
-                  {/*<th>Data Formats</th>*/}
-                  {/*<th>Tags</th>*/}
+                  <th>Data Access</th>
                 </tr>
                 </thead>
-                <tbody>{rows}</tbody>
+                <tbody>{records}</tbody>
               </table>
             </div>
           </div>
