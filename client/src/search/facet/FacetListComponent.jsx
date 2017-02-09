4584b4050bd711f50eca43ccfb80c3ac005fc979
Aaron Caldwell <AaronJCaldwell@gmail.com>
2017-02-09 13:55:32 -0700

    Some facet component reorg

--- client/src/search/facet/FacetListComponent.jsx
+++ client/src/search/facet/FacetListComponent.jsx
@@ -9,6 +9,8 @@ class FacetList extends React.Component {
     super(props)
     this.updateStoreAndSubmitSearch = this.updateStoreAndSubmitSearch.bind(this)
     this.facetMap = props.facetMap
+    this.populateFacetComponent = this.populateFacetComponent.bind(this)
+    this.populateSubPanel = this.populateSubPanel.bind(this)
     this.selectedFacets = props.selectedFacets
     this.toggleFacet = props.toggleFacet
     this.submit = props.submit
@@ -36,61 +38,65 @@ class FacetList extends React.Component {
     this.submit()
   }
 
-  toTitleCase(str){
-    return _.startCase(_.toLower((str.split(/(?=[A-Z])/).join(" "))))
-  }
-
-  subFacetLabel(str) {
-    return str.split('>').pop().trim()
-  }
-
   isSelected(category, facet) {
     return this.selectedFacets[category]
     && this.selectedFacets[category].includes(facet)
     || false
   }
 
-  render() {
+  populateFacetComponent() {
+    const self = this
+    const toTitleCase = str => _.startCase(_.toLower((str.split(/(?=[A-Z])/).join(" "))))
     let facets = []
-    let self = this
     _.forOwn(this.facetMap, (terms,category) => {
       if (!_.isEmpty(terms)) { // Don't load categories that have no results
         facets.push(
           <Panel header={`${this.state.terms[category.toLowerCase()] ||
-            self.toTitleCase(category)}`} key={`${category}`}>
-            {Object.keys(terms).sort( (a, b) => {
-              const aSub = self.subFacetLabel(a)
-              const bSub = self.subFacetLabel(b)
-              if(aSub < bSub) { return -1 }
-              if(aSub > bSub) { return 1 }
-              return 0
-            } ).map( term => {
-              let input = {
-                className: styles.checkFacet,
-                'data-name': category,
-                'data-value': term,
-                id: `${category}-${term}`,
-                type: 'checkbox',
-                onChange: self.updateStoreAndSubmitSearch,
-                checked: self.isSelected(category, term)
-              }
-              return(<div key={`${category}-${term}`}>
-                <input {...input}/>
-                 <span className={styles.facetLabel}>{self.subFacetLabel(`${term}`)}</span>
-                <div className={`${styles.count} ${styles.numberCircle}`}>{`(${terms[term].count})`}</div>
-              </div>)
-            })}
+            toTitleCase(category)}`} key={`${category}`}>
+            {self.populateSubPanel(category, terms)}
           </Panel>
         )
       }
     })
+    return facets
+  }
 
+  populateSubPanel(category, subCategories) {
+    const self = this
+    const subFacetLabel = str => str.split('>').pop().trim()
+
+    return Object.keys(subCategories).sort((a, b) => {
+      const aSub = subFacetLabel(a)
+      const bSub = subFacetLabel(b)
+      if(aSub < bSub) { return -1 }
+      if(aSub > bSub) { return 1 }
+      return 0
+    }).map( subCategory => {
+      let input = {
+        className: styles.checkFacet,
+        'data-name': category,
+        'data-value': subCategory,
+        id: `${category}-${subCategory}`,
+        type: 'checkbox',
+        onChange: self.updateStoreAndSubmitSearch,
+        checked: self.isSelected(category, subCategory)
+      }
+      return(<div key={`${category}-${subCategory}`}>
+        <input {...input}/>
+        <span className={styles.facetLabel}>{subFacetLabel(`${subCategory}`)}</span>
+        <div className={`${styles.count} ${styles.numberCircle}`}>
+          {`(${subCategories[subCategory].count})`}</div>
+      </div>)
+    })
+  }
+
+  render() {
     return <div>
       <div className={`${styles.facetContainer}`}>
         <form className={`pure-form ${styles.formStyle}`}>
           <span className={'pure-menu-heading'}>Categories</span>
           <Collapse defaultActiveKey="0">
-            {facets}
+            {this.populateFacetComponent()}
           </Collapse>
         </form>
       </div>
