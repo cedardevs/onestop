2f197908780ebc0bac3d0833dc124eb0c0ca5193
Aaron Caldwell <AaronJCaldwell@gmail.com>
2017-02-16 15:39:01 -0700

    Bind required starting params in 508 landing container. Pull coordinate string out of geoJSON

--- client/src/landing/Section508LandingContainer.js
+++ client/src/landing/Section508LandingContainer.js
@@ -3,22 +3,46 @@ import Section508LandingComponent from './Section508LandingComponent'
 import { triggerSearch, clearFacets } from '../actions/SearchRequestActions'
 import { updateQuery } from '../actions/SearchParamActions'
 import { showCollections } from '../actions/FlowActions'
+import { newGeometry, removeGeometry, updateDateRange } from '../actions/SearchParamActions'
 
 const mapStateToProps = (state) => {
+  const { startDateTime, endDateTime } = state.behavior.search
+  const coordinates = pullOutCoordinates(state.behavior.search)
   return {
-    queryString: state.behavior.search.queryText.text
+    startDateTime: startDateTime,
+    endDateTime: endDateTime,
+    geoJsonSelection: coordinates,
+    queryString: state.behavior.search.queryText
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
+    // Update geometry
+    handleNewGeometry: geoJSON => {console.log(geoJSON);dispatch(newGeometry(geoJSON))},
+    removeGeometry: () => dispatch(removeGeometry()),
+    // Update date range
+    updateOnChange: (startDate, endDate) => {
+      dispatch(updateDateRange(startDate, endDate))
+    },
+    // Update search text
+    updateQuery: text => dispatch(updateQuery(text)),
+    // Submit search
     submit: () => {
       dispatch(clearFacets())
       dispatch(triggerSearch())
       dispatch(showCollections())
-    },
-    updateQuery: (text) => dispatch(updateQuery(text))
+    }
   }
+}
+
+const pullOutCoordinates = ({geoJSON}) => {
+  let coordinates = geoJSON && geoJSON.geometry && geoJSON.geometry.coordinates
+  return coordinates
+    ? coordinates[0]
+      .filter((el,idx)=>[0,2].includes(idx))
+      .toString()
+    : ''
 }
 
 const Section508LandingContainer = connect(
