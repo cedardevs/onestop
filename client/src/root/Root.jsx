3938c06f84abecfefa7c79df6602a8e41234a730
Arianna <arianna.jakositz@colorado.edu>
2017-02-16 15:30:22 -0700

    For #27, added NCEI header links and started work on burger menu when screen width gets small

--- client/src/root/Root.jsx
+++ client/src/root/Root.jsx
@@ -13,9 +13,7 @@ class RootComponent extends React.Component {
   constructor(props) {
     super(props)
 
-    this.renderSearchBarInHeader = this.renderSearchBarInHeader.bind(this)
-    this.renderOrgBoxStyle = this.renderOrgBoxStyle.bind(this)
-    this.renderLogoTextStyle = this.renderLogoTextStyle.bind(this)
+    this.renderHeader = this.renderHeader.bind(this)
 
     this.location = props.location.pathname
   }
@@ -24,52 +22,75 @@ class RootComponent extends React.Component {
     this.location = nextProps.location.pathname
   }
 
-  renderSearchBarInHeader() {
+  renderHeader() {
     if(this.location !== '/') {
       return (
-        <div className={`pure-u-1 pure-u-sm-3-4 ${styles.landingComponents}`}>
-          <SearchFieldsContainer/>
+        <div className={'pure-g'}>
+          <div className={`pure-u-5-24 ${styles.orgBox}`}>
+            <div className={styles.logoLinks}>
+              <a href="//www.noaa.gov"><img className={styles.noaaLogo} id='logo' src={logoPath} alt="NOAA Home"/></a>
+              <Link to='/' activeClassName="active" onlyActiveOnIndex={true}>
+                <span className={styles.oneStopText}><i className={`fa fa-stop-circle-o fa-md ${styles.oneStopText}`}></i>neStop</span>
+              </Link>
+            </div>
+          </div>
+          <div className={`pure-u-1 pure-u-sm-3-4 ${styles.landingComponents}`}>
+            <SearchFieldsContainer/>
+          </div>
+        </div>
+      )
+    }
+
+    else {
+      return (
+        <div className={`pure-g`}>
+          <div className={`pure-u-7-24`}>
+            <a href="//www.ncei.noaa.gov/">
+              <img className={styles.nceiLogo} src="https://www.ncei.noaa.gov/sites/default/files/ncei_dark_test_75.png" alt="NCEI Home"/>
+            </a>
+          </div>
+          <div className={`pure-u-2-3 ${styles.nceiMenu}`}>
+            <div className={`pure-menu pure-menu-horizontal`}>
+              <ul className={`pure-menu-list`}>
+                <li className={`pure-menu-item`}><a href="https://www.ncei.noaa.gov/" className={styles.nceiMenuItem}>Home</a></li>
+                <li className={`pure-menu-item`}><a href="https://www.ncei.noaa.gov/about" className={styles.nceiMenuItem}>About</a></li>
+                <li className={`pure-menu-item`}><a href="https://www.ncei.noaa.gov/news" className={styles.nceiMenuItem}>News</a></li>
+                <li className={`pure-menu-item`}><a href="https://www.ncei.noaa.gov/access" className={styles.nceiMenuItem}>Access</a></li>
+                <li className={`pure-menu-item`}><a href="https://www.ncei.noaa.gov/archive" className={styles.nceiMenuItem}>Archive</a></li>
+                <li className={`pure-menu-item`}><a href="https://www.ncei.noaa.gov/contact" className={styles.nceiMenuItem}>Contact</a></li>
+                <li className={`pure-menu-item`}><a href="#" className={styles.selected}>Search</a></li>
+              </ul>
+            </div>
+          </div>
+          <div className={`pure-u-2-3 ${styles.nceiBurgerMenuLanding}`}>
+            <div className={styles.nceiMenuItem}>
+              <a href="#" className={styles.nceiMenuItem}><i className={`fa fa-bars fa-3x`}></i></a>
+              <ul>
+                <li><a href="https://www.ncei.noaa.gov/" className={styles.nceiMenuItem}>Home</a></li>
+                <li><a href="https://www.ncei.noaa.gov/about" className={styles.nceiMenuItem}>About</a></li>
+                <li><a href="https://www.ncei.noaa.gov/news" className={styles.nceiMenuItem}>News</a></li>
+                <li><a href="https://www.ncei.noaa.gov/access" className={styles.nceiMenuItem}>Access</a></li>
+                <li><a href="https://www.ncei.noaa.gov/archive" className={styles.nceiMenuItem}>Archive</a></li>
+                <li><a href="https://www.ncei.noaa.gov/contact" className={styles.nceiMenuItem}>Contact</a></li>
+                <li><a href="#" className={styles.selected}>Search</a></li>
+              </ul>
+            </div>
+          </div>
         </div>
       )
     }
   }
 
-  renderOrgBoxStyle() {
-    if(this.location === '/') {
-      return `pure-u-1 ${styles.orgBoxLanding}`
-    }
-    else {
-      return `pure-u-5-24 ${styles.orgBox}`
-    }
-  }
-
-  renderLogoTextStyle() {
-    if(this.location === '/') {
-      return `${styles.oneStopTextLanding}`
-    }
-    else {
-      return `${styles.oneStopText}`
-    }
-  }
+  // renderBurgerMenu() {
+  //   if(this.location)
+  // }
 
   render() {
     return <div className={styles.rootContainer}>
       <div className={styles.mainContent}>
         <BannerContainer/>
         <DetailContainer/>
-        <div id="header" className={styles.headerArea}>
-          <div className={'pure-g'}>
-            <div className={`${this.renderOrgBoxStyle()}`}>
-              <div className={styles.logoLinks}>
-                <a href="http://www.noaa.gov"><img className={styles.logo} id='logo' src={logoPath} alt="NOAA Logo"/></a>
-                <Link to='/' activeClassName="active" onlyActiveOnIndex={true}>
-                  <span className={`${this.renderLogoTextStyle()}`}><i className={`fa fa-stop-circle-o fa-md ${this.renderLogoTextStyle()}`}></i>neStop</span>
-                </Link>
-              </div>
-            </div>
-            {this.renderSearchBarInHeader()}
-          </div>
-        </div>
+        <div id="header" className={styles.headerArea}>{this.renderHeader()}</div>
         <div className={styles.main}>
           <LoadingContainer/>
           {this.props.children}
