f508809bb7728314b1bbfd960fd171a47fade902
Aaron Caldwell <AaronJCaldwell@gmail.com>
2017-02-16 15:39:43 -0700

    508 landing inputs in place with basic content checks

--- client/src/landing/Section508LandingComponent.jsx
+++ client/src/landing/Section508LandingComponent.jsx
@@ -1,49 +1,116 @@
 import React from 'react'
 import styles from './section508.css'
+import GeoValidate from 'geojson-validation'
+import moment from 'moment'
 
 class Section508LandingComponent extends React.Component {
   constructor(props) {
     super(props)
     this.submit = props.submit
-    this.updateQuery = props.updateQuery
-    this.state = {
+    this.updateQuery = this.updateQuery.bind(this)
+    this.handleKeyDown = this.handleKeyDown.bind(this)
+    this.generateForm = this.generateForm.bind(this)
+    this.state = this.initializeState()
+  }
+
+  initializeState() {
+    return {
       formFields: [
-        { label: 'Search Text', name: 'search-text', placeholder: 'e.g. ocean', action: {}},
-        { label: 'Start Date', name: 'start-date', placeholder: 'e.g. 1940-02-01T00:00:00Z', action: {}},
-        { label: 'End Date', name: 'end-date', placeholder: 'e.g. 2017-02-13T00:00:00Z', action: {}},
-        { label: 'Bounding Box', name: 'geometry', placeholder: `e.g. -180.00,-90.00,180.00,90.00 (W,S,E,N)`, action: {}}
+        { label: 'Search Text', name: 'search-text', placeholder: 'e.g. ocean', action: {}, value: 'queryString'},
+        { label: 'Start Date', name: 'start-date', placeholder: 'e.g. 1940-02-01T00:00:00Z', action: {}, value: 'startDateTime'},
+        { label: 'End Date', name: 'end-date', placeholder: 'e.g. 2017-02-13T00:00:00Z', action: {}, value: 'endDateTime'},
+        { label: 'Bounding Box', name: 'geometry', placeholder: `e.g. -180.00,-90.00,180.00,90.00 (W,S,E,N)`, action: {}, value: 'geoJsonSelection' }
       ]
     }
   }
 
-  search(query) {
-    this.updateQuery(query);
-    this.submit(query);
+  submitSearch() {
   }
 
-  render() {
+  updateQuery(e) {
+    let { name, value } = e.target
+    value = value.trim()
+    switch (name) {
+      case 'search-text':
+        this.props.updateQuery(value)
+        break
+      case 'start-date':
+        if (moment(value).isValid()) {
+          this.props.updateOnChange(value, this.props.endDateTime || '')
+        }
+        break
+      case 'end-date':
+        if (moment(value).isValid()) {
+          this.props.updateOnChange(this.props.startDateTime || '', value)
+        }
+        break
+      case 'geometry':
+        this.validateAndSubmitGeoJson(value)
+        break
+      default:
+        // No action
+    }
+    console.log(e.target.name, e.target.value)
+  }
+
+  // Checks for bounding box only at this point
+  validateAndSubmitGeoJson(coordString) {
+    const coordArray = coordString.split(',').map(x=> parseFloat(x))
+    const sw = [coordArray[0], coordArray[1]]
+    const nw = [coordArray[0], coordArray[3]]
+    const ne = [coordArray[2], coordArray[3]]
+    const se = [coordArray[2], coordArray[1]]
+    const coordinates = [[sw, nw, ne, se, sw]]
+    let geoJSON = {
+      type: 'Feature',
+      properties: {},
+      geometry: {
+        coordinates,
+        type: 'Polygon'
+      }
+    }
+    if(coordArray.length >= 5 && GeoValidate.isPolygonCoor(coordinates)){
+      this.props.handleNewGeometry(geoJSON)
+    }
+  }
+
+  handleKeyDown(e) {
+    if (e.keyCode === 13) {
+      e.preventDefault()
+      this.submitSearch()
+    }
+  }
+
+  generateForm() {
     const { formFields } = this.state
     let form = formFields.map(field => {
-      return <div className={styles.formRow}>
+      return <div className={styles.formRow} key={field.name}>
         <label htmlFor={field.name} className={styles.formLabel}>{field.label}</label>
-        <input type="text" className={styles.formInput} name={field.name} id={field.name} placeholder={field.placeholder}/>
+        <input type="text" className={styles.formInput} name={field.name}
+          id={field.name} placeholder={field.placeholder} onKeyDown={this.handleKeyDown}
+          onChange={this.updateQuery} defaultValue={_.get(this.props, field.value)}/>
       </div>
     })
     const searchButton = <button className={`${styles.button} pure-button`}
-      onclick={this.submit}>Search</button>
+      onClick={this.submit}>Search</button>
     const clearButton = <button className={`${styles.button} pure-button`}>Clear</button>
 
-    return( <div className={`${styles.form} pure-form`}>
-      {form}
-      {searchButton}
-      {clearButton}
-    </div> )
+    return(
+      <form onSubmit={this.submitSearch} className={`${styles.form} pure-form`}>
+        {form}
+        {searchButton}
+        {clearButton}
+      </form>)
+  }
+
+  render() {
+    return this.generateForm()
   }
 
   componentDidMount() {
     setTimeout(() => {
-      window.dispatchEvent(new Event('resize'));
-    }, 0);
+      window.dispatchEvent(new Event('resize'))
+    }, 0)
   }
 }
 
