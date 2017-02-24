43d8770be4113fc023a14b6b2ee348e3ec34e520
Evan McQuinn <evan.mcquinn@colorado.edu>
2017-02-23 18:23:48 -0700

    Add recreate endpoints. Resolves #41

--- api/src/main/groovy/ncei/onestop/api/controller/AdminController.groovy
+++ api/src/main/groovy/ncei/onestop/api/controller/AdminController.groovy
@@ -2,8 +2,11 @@ package ncei.onestop.api.controller
 
 import groovy.util.logging.Slf4j
 import ncei.onestop.api.service.ETLService
+import ncei.onestop.api.service.MetadataIndexService
+import ncei.onestop.api.service.SearchIndexService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET
@@ -14,10 +17,14 @@ import static org.springframework.web.bind.annotation.RequestMethod.PUT
 class AdminController {
 
   private ETLService etlService
+  private SearchIndexService searchIndexService
+  private MetadataIndexService metadataIndexService
 
   @Autowired
-  AdminController(ETLService etlService) {
+  AdminController(ETLService etlService, SearchIndexService searchIndexService, MetadataIndexService metadataIndexService) {
     this.etlService = etlService
+    this.searchIndexService = searchIndexService
+    this.metadataIndexService = metadataIndexService
   }
 
   @RequestMapping(path = '/admin/index/search/rebuild', method = [GET, PUT], produces = 'application/json')
@@ -30,5 +37,21 @@ class AdminController {
   Map updateSearchIndex() {
     etlService.updateSearchIndexAsync()
     return [acknowledged: true]
+  }
+
+  @RequestMapping(path = '/admin/index/search/recreate', method = [GET, PUT], produces = 'application/json')
+  Map recreateSearchIndex(@RequestParam Boolean sure) {
+    if (sure != null) {
+      searchIndexService.recreate()
+      return [acknowledged: true]
+    }
+  }
+
+  @RequestMapping(path = '/admin/index/metadata/recreate', method = [GET, PUT], produces = 'application/json')
+  Map recreateMetadataIndex(@RequestParam Boolean sure) {
+    if (sure != null) {
+      metadataIndexService.recreate()
+      return [acknowledged: true]
+    }
   }
 }
