c26a12478a15d09fbe240b3a447d35d2a079485e
Evan McQuinn <evan.mcquinn@colorado.edu>
2017-02-24 16:14:38 -0700

    Fix tests. #32

--- client/test/reducers/ui/togglesReducerSpec.js
+++ client/test/reducers/ui/togglesReducerSpec.js
@@ -4,40 +4,40 @@ import { toggleAbout, toggleHelp } from '../../../src/actions/FlowActions'
 
 describe('The ui toggles reducer',() => {
   it('toggles about',() => {
-    initialState.showAbout.should.equal(false)
+    initialState.about.should.equal(false)
 
     const once = info(initialState, toggleAbout())
-    once.showAbout.should.equal(true)
+    once.about.should.equal(true)
 
     const twice = info(once, toggleAbout())
-    twice.showAbout.should.equal(false)
+    twice.about.should.equal(false)
   })
 
   it('toggles help',() => {
-    initialState.showHelp.should.equal(false)
+    initialState.help.should.equal(false)
 
     const once = info(initialState, toggleHelp())
-    once.showHelp.should.equal(true)
+    once.help.should.equal(true)
 
     const twice = info(once, toggleHelp())
-    twice.showHelp.should.equal(false)
+    twice.help.should.equal(false)
   })
 
   it('only allows one toggles to be toggled at once', function () {
     const aboutOnce = info(initialState, toggleAbout())
-    aboutOnce.showAbout.should.equal(true)
-    aboutOnce.showHelp.should.equal(false)
+    aboutOnce.about.should.equal(true)
+    aboutOnce.help.should.equal(false)
 
     const helpOnce = info(aboutOnce, toggleHelp())
-    helpOnce.showAbout.should.equal(false)
-    helpOnce.showHelp.should.equal(true)
+    helpOnce.about.should.equal(false)
+    helpOnce.help.should.equal(true)
 
     const aboutTwice = info(helpOnce, toggleAbout())
-    aboutTwice.showAbout.should.equal(true)
-    aboutTwice.showHelp.should.equal(false)
+    aboutTwice.about.should.equal(true)
+    aboutTwice.help.should.equal(false)
 
     const aboutThrice = info(aboutTwice, toggleAbout())
-    aboutThrice.showAbout.should.equal(false)
-    aboutThrice.showHelp.should.equal(false)
+    aboutThrice.about.should.equal(false)
+    aboutThrice.help.should.equal(false)
   })
 })
