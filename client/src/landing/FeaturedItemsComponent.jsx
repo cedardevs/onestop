076249ed7a9b20b6375eab8661e5dbcc3c2f35a9
Evan McQuinn <evan.mcquinn@colorado.edu>
2017-03-01 12:43:10 -0700

    Round edges and only launch a search when you click an image. Issue #34

--- client/src/landing/FeaturedItemsComponent.jsx
+++ client/src/landing/FeaturedItemsComponent.jsx
@@ -17,13 +17,14 @@ class FeaturedItemsComponent extends React.Component {
   render() {
     return <div className={`pure-g ${styles.featured}`}>
       <div className={`pure-u-1-4 ${styles.titles}`}>
-        {this.featured.map((f, i) =>
-            <div key={i} className={`${styles.title} ${this.selectedClass(i)}`}
-                 onMouseEnter={() => this.setState({current: i})}
-                 onClick={() => this.props.doSearch(f.term)}>
-              {f.title}
-            </div>
-        )}
+        <ul className={`${styles.titles}`}>
+          {this.featured.map((f, i) =>
+              <li key={i} className={`${this.selectedClass(i)}`}
+                  onMouseEnter={() => this.setState({current: i})}>
+                {f.title}
+              </li>
+          )}
+        </ul>
       </div>
       <div className={`pure-u-3-4 ${styles.images}`}>
         {this.featured.map((f, i) =>
