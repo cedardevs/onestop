fc5a214515f5759879a8958072584dec878e9086
Aaron Caldwell <AaronJCaldwell@gmail.com>
2017-03-13 15:28:22 -0600

    Add postcss config

---
+++ client/src/postcss.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  plugins: [
+    require('postcss-import')({ /* ...options */ }),
+    require('precss')({ /* ...options */ }),
+    require('autoprefixer')({ /* ...options */ })
+  ]
+}
