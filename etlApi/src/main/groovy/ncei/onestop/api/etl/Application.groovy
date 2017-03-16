68fd1d417b382742b1395bfc7a81cab3346b7e95
Arianna <arianna.jakositz@colorado.edu>
2017-03-16 10:25:24 -0600

    API is mostly broken into separate services, but integration tests are broken because of repeated spring beans across modules (dependencies between APIs for tests only). This is the ETL API.  -- #47

---
+++ etlApi/src/main/groovy/ncei/onestop/api/etl/Application.groovy
@@ -0,0 +1,25 @@
+package ncei.onestop.api.etl
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.support.SpringBootServletInitializer
+import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.scheduling.annotation.EnableScheduling
+
+@EnableAsync
+@EnableScheduling
+@SpringBootApplication
+class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application)
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args)
+    }
+
+}
+
---
+++ etlApi/src/main/groovy/ncei/onestop/api/etl/controller/AdminController.groovy
@@ -0,0 +1,61 @@
+package ncei.onestop.api.etl.controller
+
+import groovy.util.logging.Slf4j
+import ncei.onestop.api.etl.service.ETLService
+import ncei.onestop.api.etl.service.IndexAdminService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET
+import static org.springframework.web.bind.annotation.RequestMethod.PUT
+
+@Slf4j
+@RestController
+class AdminController {
+
+  @Value('${elasticsearch.index.prefix:}${elasticsearch.index.staging.name}')
+  String STAGING_INDEX
+
+  @Value('${elasticsearch.index.prefix:}${elasticsearch.index.search.name}')
+  String SEARCH_INDEX
+
+  private ETLService etlService
+  private IndexAdminService adminService
+
+  @Autowired
+  AdminController(ETLService etlService, IndexAdminService adminService) {
+    this.etlService = etlService
+    this.adminService = adminService
+  }
+
+  @RequestMapping(path = '/admin/index/search/rebuild', method = [GET, PUT], produces = 'application/json')
+  Map rebuildSearchIndex() {
+    etlService.rebuildSearchIndexAsync()
+    return [acknowledged: true]
+  }
+
+  @RequestMapping(path = '/admin/index/search/update', method = [GET, PUT], produces = 'application/json')
+  Map updateSearchIndex() {
+    etlService.updateSearchIndexAsync()
+    return [acknowledged: true]
+  }
+
+  @RequestMapping(path = '/admin/index/search/recreate', method = [GET, PUT], produces = 'application/json')
+  Map recreateSearchIndex(@RequestParam Boolean sure) {
+    if (sure != null) {
+      adminService.recreate(SEARCH_INDEX)
+      return [acknowledged: true]
+    }
+  }
+
+  @RequestMapping(path = '/admin/index/metadata/recreate', method = [GET, PUT], produces = 'application/json')
+  Map recreateMetadataIndex(@RequestParam Boolean sure) {
+    if (sure != null) {
+      adminService.recreate(STAGING_INDEX)
+      return [acknowledged: true]
+    }
+  }
+}
