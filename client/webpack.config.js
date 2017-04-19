14e8f892eea49ed9d98913b0561c0cc79fb9c719
Aaron Caldwell <AaronJCaldwell@gmail.com>
2017-04-19 09:18:29 -0600

    Put previous webpack package.json and config in place

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,86 +1,35 @@
-var webpack = require('webpack')
-var HtmlWebpackPlugin = require('html-webpack-plugin')
-var precss = require('precss')
-var autoprefixer = require('autoprefixer')
-var postcssAssets = require('postcss-assets')
-var path = require('path')
-
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const precss = require('precss')
+const autoprefixer = require('autoprefixer')
+const postcssAssets = require('postcss-assets')
+const path = require('path')
 
 module.exports = {
-  entry: {
-    app: './src/index.jsx',
-    vendor: ['purecss', 'react', 'react-dom', 'react-router', 'redux', 'redux-thunk',
-      'leaflet']
-  },
-  module: {
-    preLoaders: [{
-      test: /\.js$/,
-      loader: "eslint",
-      exclude: /node_modules/
-    }],
-    loaders: [{
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      loader: 'babel'
-    }, {
-      test: /\.css$/,
-      include: /node_modules/,
-      loaders: [
-        'style?sourceMap',
-        'css'
-      ]
-    }, {
-      test: /\.css$/,
-      exclude: /node_modules/,
-      loaders: [
-        'style?sourceMap',
-        'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
-        'postcss'
-      ]
-    }, {
-      test: /\.(jpe?g|png|gif)$/,
-      loaders: [
-        'file?hash=sha512&digest=hex&name=[hash].[ext]',
-        'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
-      ],
-    }, {
-      test: /\.(svg)(\?v=\d+\.\d+\.\d+)?$/,
-      loader: 'url?limit=65000&mimetype=image/svg+xml&name=public/fonts/[name].[ext]'
-    }, {
-        test: /\.(woff)(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=65000&mimetype=application/font-woff&name=public/fonts/[name].[ext]'
-    }, {
-        test: /\.(woff2)(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=65000&mimetype=application/font-woff2&name=public/fonts/[name].[ext]'
-    }, {
-        test: /\.([ot]tf)(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=65000&mimetype=application/octet-stream&name=public/fonts/[name].[ext]'
-    }, {
-        test: /\.(eot)(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=65000&mimetype=application/vnd.ms-fontobject&name=public/fonts/[name].[ext]'
-    }]
-  },
-  postcss: function(){
-    return [postcssAssets({
-        loadPaths: ['**']
-      }), precss, autoprefixer]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx'],
-    root: [
-      path.resolve('./node_modules/leaflet/dist')
-    ]
-  },
+  entry: [
+    'react-hot-loader/patch',
+    // activate HMR for React
+
+    'webpack-dev-server/client?http://localhost:8080',
+    // bundle the client for webpack-dev-server
+    // and connect to the provided endpoint
+
+    'webpack/hot/only-dev-server',
+    // bundle the client for hot reloading
+    // hot reload for successful updates
+
+    './index.jsx'
+  ],
   output: {
-    path: __dirname + '/dist',
+    path: path.resolve(__dirname, 'dist'),
     publicPath: './',
-    filename: 'bundle-[hash].js'
+    filename: '[name].bundle.js'
   },
-  devtool: '#eval-source-map',
-  debug: true,
+  context: path.resolve(__dirname, 'src'),
+  devtool: 'source-map',
   devServer: {
     publicPath: '/onestop/',
-    contentBase: './dist',
+    contentBase: path.resolve(__dirname, 'dist'),
     hot: true,
     proxy: {
       '/onestop/api/*': {
@@ -89,21 +38,105 @@ module.exports = {
       }
     }
   },
+  module: {
+    rules: [{
+      enforce: 'pre',
+      test: /\.js$/,
+      use: 'eslint-loader',
+      exclude: /node_modules/
+    }, {
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [ 'es2015', { modules: false } ]
+          ]
+        }
+      }
+    }, {
+      test: /\.css$/,
+      include: /node_modules/,
+      use: [{
+        loader: 'style-loader',
+        options: {
+          sourceMap: true
+        }
+      }, {
+        loader: 'css-loader'
+      }]
+    }, {
+      test: /\.css$/,
+      exclude: /node_modules/,
+      use: [{
+        loader: 'style-loader',
+        options: {
+          sourceMap: true
+        }
+      }, {
+        loader: 'css-loader',
+        options: {
+          modules: true,
+          importLoaders: true,
+          localIdentName: '[name]__[local]___[hash:base64:5]',
+          plugins: function () {
+            return [
+              require('precss'),
+              require('autoprefixer')
+            ]
+          }
+        }
+      }, {
+        loader: 'postcss-loader'
+      }],
+    }, {
+      test: /\.(jpe?g|png|gif)$/,
+      use: [
+        'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+        'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
+      ],
+    }, {
+      test: /\.(svg)(\?v=\d+\.\d+\.\d+)?$/,
+      use: 'url-loader?limit=65000&mimetype=image/svg+xml&name=public/fonts/[name].[ext]'
+    }, {
+        test: /\.(woff)(\?v=\d+\.\d+\.\d+)?$/,
+        use: 'url-loader?limit=65000&mimetype=application/font-woff&name=public/fonts/[name].[ext]'
+    }, {
+        test: /\.(woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        use: 'url-loader?limit=65000&mimetype=application/font-woff2&name=public/fonts/[name].[ext]'
+    }, {
+        test: /\.([ot]tf)(\?v=\d+\.\d+\.\d+)?$/,
+        use: 'url-loader?limit=65000&mimetype=application/octet-stream&name=public/fonts/[name].[ext]'
+    }, {
+        test: /\.(eot)(\?v=\d+\.\d+\.\d+)?$/,
+        use: 'url-loader?limit=65000&mimetype=application/vnd.ms-fontobject&name=public/fonts/[name].[ext]'
+    }]
+  },
+  resolve: {
+    modules: [path.resolve('./node_modules/leaflet/dist', 'root'), 'node_modules'],
+    extensions: ['.js', '.jsx'],
+  },
   plugins: [
-    // TODO - This is not working in the running app right now. Can fix later as needed.
-    // new webpack.DefinePlugin({
-    //   'process.env': {
-    //     'NODE_ENV': process && process.env && process.env.NODE_ENV || 'development'
-    //   }
-    // }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {warnings: false},
-      sourceMap: false
-    }),
     new HtmlWebpackPlugin({
       title: 'NOAA OneStop Demo',
-      favicon: './img/noaa-favicon.ico'
+      favicon: '../img/noaa-favicon.ico'
     }),
-    new webpack.optimize.CommonsChunkPlugin("vendor", "vendor-bundle-[hash].js")
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {warnings: false},
+      sourceMap: true
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: function (module) {
+        return module.context && module.context.indexOf('node_modules') !== -1
+      }
+    }),
+    //new webpack.optimize.CommonsChunkPlugin("vendor", "vendor-bundle-[hash].js")
+    new webpack.HotModuleReplacementPlugin(),
+    // enable HMR globally
+
+    new webpack.NamedModulesPlugin()
+    // prints more readable module names in the browser console on HMR updates
   ]
 }
