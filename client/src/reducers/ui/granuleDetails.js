9773cfa1c70da3c9d0cfef00c5f0faf5d0a9ebd9
Aaron Caldwell <AaronJCaldwell@gmail.com>
2017-06-14 12:52:13 -0600

    Change granule details reducer to handle boolean values. Simplify since it's only meant to handle one focused granule at a time

--- client/src/reducers/ui/granuleDetails.js
+++ client/src/reducers/ui/granuleDetails.js
@@ -8,19 +8,11 @@ export const initialState = Immutable({
 export const granuleDetails = (state = initialState, action) => {
   switch(action.type) {
     case TOGGLE_GRANULE_FOCUS:
-      return Immutable.set(state, 'focusedGranules', toggleId(state.focusedGranules, action.id))
+      return Immutable.set(state, 'focusedGranules', action.focused ? [action.id] : [])
 
     default:
       return state
   }
-}
-
-const toggleId = (selectedIds, value, idx = 0) => {
-  if (idx === selectedIds.length){
-    return selectedIds.concat([value])
-  } else if (selectedIds[idx] === value){
-    return selectedIds.slice(0, idx).concat(selectedIds.slice(idx + 1))
-  } else { return toggleId(selectedIds, value, idx + 1) }
 }
 
 export default granuleDetails
