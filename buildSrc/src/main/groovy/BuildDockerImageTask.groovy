4869453d852e8e28c68f3d8b128db43824a3a2b3
Evan McQuinn <evan.mcquinn@colorado.edu>
2017-10-16 15:54:19 -0600

    Use the project to access the rootProject. Also some cleanup.

--- buildSrc/src/main/groovy/BuildDockerImageTask.groovy
+++ buildSrc/src/main/groovy/BuildDockerImageTask.groovy
@@ -1,7 +1,6 @@
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.Input
-import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
 
 /**
@@ -17,32 +16,27 @@ class BuildDockerImageTask extends DefaultTask {
 
   // TODO description: 'Creates a docker image with the current jar.', group: 'docker'
 
-  @Optional
-  @Input Map<String, String> additionalBuildArgs
+  @Optional @Input
+  Map<String, String> additionalBuildArgs
 
-  @Input String rootProjectName
-
-  def getDateTime() {
-    return new Date().format("YYYY-MM-DD'T'HH:mm:ss.SSS'Z'", TimeZone.getTimeZone('UTC'))
-  }
-
-  @TaskAction download() {
+  @TaskAction
+  buildImage() {
     project.exec {
-      // workingDir etlScriptsRoo
       executable = "bash"
       args = ["-c", "docker build --no-cache " +
           "--build-arg VCS_REF=\$(git rev-parse --short HEAD) " +
           "--build-arg VERSION=${project.version} " +
           "--build-arg DATE=${getDateTime()} " +
-          additionalBuildArgs.collect {
-            "--build-arg ${it.key}=${it.value} "
-            }.join(" ") +
-            "-t cedardevs/${rootProject.name}-${project.name}:${project.version} " +
-            "-t cedardevs/${rootProject.name}-${project.name}:latest " +
-            "-t cedardevs/${rootProject.name}-${project.name}:latest-SNAPSHOT " +
-            "."
-          ]
+          additionalBuildArgs.collect({ "--build-arg ${it.key}=${it.value}" }).join(" ") + " " +
+          "-t cedardevs/${project.rootProject.name}-${project.name}:${project.version} " +
+          "-t cedardevs/${project.rootProject.name}-${project.name}:latest " +
+          "-t cedardevs/${project.rootProject.name}-${project.name}:latest-SNAPSHOT " +
+          "."]
     }
+  }
+
+  static getDateTime() {
+    return new Date().format("YYYY-MM-DD'T'HH:mm:ss.SSS'Z'", TimeZone.getTimeZone('UTC'))
   }
 
 }
