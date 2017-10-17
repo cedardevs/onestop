a44edcedc9f63455c859827f5c86de01fbab3bfe
Zeb <elizabeth.delk@colorado.edu>
2017-10-17 13:35:57 -0600

    Version 1 contrast improvements.

--- client/src/result/collections/CollectionTileComponent.jsx
+++ client/src/result/collections/CollectionTileComponent.jsx
@@ -13,12 +13,14 @@ class CollectionTile extends React.Component {
   render() {
     return <div className={styles.tileContainer}
       onKeyPress={(e)=>this.handleKeyPress(e, this.props.onCardClick)}
-      tabIndex={0}>
+      >
       <div className={styles.tileContent} style={this.thumbnailStyle()}>
+        <div className={styles.overlay2}>
         <div className={styles.overlay} onClick={() => this.props.onCardClick()}
           >
           <h2 className={styles.title}>{this.props.title}</h2>
           {this.renderThumbnailMap()}
+        </div>
         </div>
       </div>
     </div>
