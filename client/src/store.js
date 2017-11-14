3600814eb85979ffae9141ebef445e24418a38ad
Evan McQuinn <evan.mcquinn@colorado.edu>
2017-11-13 17:00:37 -0700

    Add a hook to enable the redux dev tools plugin when present.

--- client/src/store.js
+++ client/src/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { routerMiddleware } from 'react-router-redux'
 import { hashHistory } from 'react-router'
 import Immutable from 'seamless-immutable'
@@ -12,10 +12,18 @@ if (typeof document !== "undefined") {
 }
 const initialState = Immutable(decodeQueryString(queryString))
 
-const store = createStore(reducer, initialState,
+const getCompose = () => {
+  if (typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ !== null) {
+    return window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  }
+  return compose
+}
+
+const composeEnhancers = getCompose()
+const store = createStore(reducer, initialState, composeEnhancers(
     applyMiddleware(
         thunk,
         routerMiddleware(hashHistory)
-    ))
+    )))
 
 export default store
