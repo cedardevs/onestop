480e4c90691870cc88ab3fad221aa97097120cfa
Evan McQuinn <evan.mcquinn@colorado.edu>
2017-11-15 14:29:10 -0700

    Minor tweaks, primarily prevent text from taking up the whole screen

--- client/src/common/info/About.jsx
+++ client/src/common/info/About.jsx
@@ -5,17 +5,19 @@ export default class Help extends React.Component {
 
   render() {
     return (
-      <section className={styles.about}>
-        <h1>OneStop Overview</h1>
-        <p>
-          The OneStop Project is designed to improve NOAA's data discovery and access framework. Focusing on all layers
-          of the framework and not just the user interface, OneStop is addressing data format and metadata best practices,
-          ensuring more data are available through modern web services, working to improve the relevance of dataset searches,
-          and advancing both collection-level metadata management and granule-level metadata systems to accommodate the
-          wide variety and vast scale of NOAA's data.
-        </p>
-        <h2>{this.buildCountString()}</h2>
-      </section>
+        <div className={styles.wrapper}>
+          <section className={styles.about}>
+            <h1>OneStop Overview</h1>
+            <p>
+              The OneStop Project is designed to improve NOAA's data discovery and access framework. Focusing on all layers
+              of the framework and not just the user interface, OneStop is addressing data format and metadata best practices,
+              ensuring more data are available through modern web services, working to improve the relevance of dataset searches,
+              and advancing both collection-level metadata management and granule-level metadata systems to accommodate the
+              wide variety and vast scale of NOAA's data.
+            </p>
+            <h2>{this.buildCountString()}</h2>
+          </section>
+        </div>
     )
   }
 
--- client/src/common/info/Help.jsx
+++ client/src/common/info/Help.jsx
@@ -10,8 +10,8 @@ export default class Help extends React.Component {
 		const accessibleVersion = window.location.hash.includes('508')
 
     const mainSiteSnippet = <li>
-      Use the <span className={styles.icon} aria-hidden="true"><img src={clock}></img></span> time and
-      <span className={styles.icon} aria-hidden="true"><img src={globe}></img></span> space filters
+      Use the time <span className={styles.icon} aria-hidden="true"><img src={clock}></img></span> and
+      space <span className={styles.icon} aria-hidden="true"><img src={globe}></img></span> filters
       (to the right of the input box) to limit results to only those that <u>intersect</u> the given constraints.
       <br/>
       If a filter has been applied, the button will change from
@@ -23,79 +23,79 @@ export default class Help extends React.Component {
       and Bounding Box text boxes to limit results to only those that <u>intersect</u> the given constraints.</li>
 
 		return (
-			<section className={styles.help}>
+		    <div className={styles.wrapper}>
+          <section className={styles.help}>
+            <h1>How to use this interface:</h1>
+            <p>
+              <b>To get started, just type a term into the Search {accessibleVersion ? 'Text ' : ''}Box on the home page
+                and click the Search Button</b>
+              {accessibleVersion ? '' :  <i className={styles.icon} aria-hidden="true"><img src={search}></img></i>}
+            </p>
 
-          <h1>How to use this interface:</h1>
-          <p>
-            <b>To get started, just type a term into the Search {accessibleVersion ? 'Text ' : ''}box on the home page and
-              click the Search Button</b>
-            {accessibleVersion ? '' :
-              <i className={styles.icon} aria-hidden="true"><img src={search}></img></i>}
-          </p>
+            <p>
+              <b>Here are a few querying tips to help narrow your results down further:</b>
+            </p>
 
-          <p>
-            <b>Here are a few querying tips to help narrow your results down further:</b>
-          </p>
+            <ul className={styles.tips}>
+              {accessibleVersion ? accessibleSiteSnippet : mainSiteSnippet}
 
-          <ul className={styles.tips}>
-            {accessibleVersion ? accessibleSiteSnippet : mainSiteSnippet}
+              <li>
+                Wrap a search phrase in double quotes for an exact match:
+                <ul className={styles.examples}>
+                  <li>"sea surface temperature"</li>
+                </ul>
+                <p>
+                  <span className={styles.note}>Note:</span> Capitalization is ignored.
+                </p>
+              </li>
 
-            <li>
-              Wrap a search phrase in double quotes for an exact match:
-              <ul className={styles.examples}>
-                <li>"sea surface temperature"</li>
-              </ul>
-              <p>
-                <span className={styles.note}>Note:</span> Capitalization is ignored.
-              </p>
-            </li>
+              <li>
+                Use <em>+</em> to indicate that a search term <em>must</em> appear in the results and <em>-</em> to
+                indicate that it <em>must not</em>. Terms without a <em>+</em> or <em>-</em> are considered optional.
+                <ul className={styles.examples}>
+                  <li>temperature pressure +air -sea</li>
+                </ul>
+                <p>
+                  <span className={styles.note}>Note:</span> This means hyphens within terms will be treated as spaces;
+                  use double quotes to search for a term with a hyphen in it.
+                </p>
+              </li>
 
-            <li>
-              Use <em>+</em> to indicate that a search term <em>must</em> appear in the results and <em>-</em> to
-              indicate that it <em>must not</em>. Terms without a <em>+</em> or <em>-</em> are considered optional.
-              <ul className={styles.examples}>
-                <li>temperature pressure +air -sea</li>
-              </ul>
-              <p>
-                <span className={styles.note}>Note:</span> This means hyphens within terms will be treated as spaces;
-                use double quotes to search for a term with a hyphen in it.
-              </p>
-            </li>
+              <li>
+                Use of <em>AND</em>, <em>OR</em>, and <em>AND NOT</em> provides similar logic to <em>+</em> and <em>-</em>,
+                but introduces operator precedence which makes for a more complicated query structure.
+                The following example gives the same results as the previous one:
+                <ul className={styles.examples}>
+                  <li>((temperature AND air) OR (pressure AND air) OR air) AND NOT sea</li>
+                </ul>
+              </li>
 
-            <li>
-              Use of <em>AND</em>, <em>OR</em>, and <em>AND NOT</em> provides similar logic to <em>+</em> and <em>-</em>,
-              but introduces operator precedence which makes for a more complicated query structure.
-              The following example gives the same results as the previous one:
-              <ul className={styles.examples}>
-                <li>((temperature AND air) OR (pressure AND air) OR air) AND NOT sea</li>
-              </ul>
-            </li>
+              <li>
+                Not sure if you misspelled something? Not to worry, simply place a tilde after the word
+                you're unsure on:
+                <ul className={styles.examples}>
+                  <li>ghrst~</li>
+                </ul>
+              </li>
 
-            <li>
-              Not sure if you misspelled something? Not to worry, simply place a tilde after the word
-              you're unsure on:
-              <ul className={styles.examples}>
-                <li>ghrst~</li>
-              </ul>
-            </li>
+              <li>
+                The title, description, and keywords of a data set's metadata can be searched directly by appending the
+                field name and a colon to the beginning of your search term (remember -- no spaces before or after the
+                colon and wrap multi-word terms in parentheses). Exact matches can be requested here as well:
+                <ul className={styles.examples}>
+                  <li>description:lakes</li>
+                  <li>title:"Tsunami Inundation"</li>
+                  <li>keywords:(ice deformation)</li>
+                </ul>
+              </li>
+            </ul>
 
-            <li>
-              The title, description, and keywords of a data set's metadata can be searched directly by appending the
-              field name and a colon to the beginning of your search term (remember -- no spaces before or after the
-              colon and wrap multi-word terms in parentheses). Exact matches can be requested here as well:
-              <ul className={styles.examples}>
-                <li>description:lakes</li>
-                <li>title:"Tsunami Inundation"</li>
-                <li>keywords:(ice deformation)</li>
-              </ul>
-            </li>
-          </ul>
+            <p><b>If you'd prefer to interact directly with the OneStop API, you can find more information about it <A
+                target="_blank" href='https://github.com/cedardevs/onestop/wiki/OneStop-Search-API'
+                style={{color: '#277cb2'}}>here</A></b></p>
 
-        <p><b>If you'd prefer to interact directly with the OneStop API, you can find more information about it <A
-            target="_blank" href='https://github.com/cedardevs/onestop/wiki/OneStop-Search-API'
-            style={{color: '#277cb2'}}>here</A></b></p>
-
-			</section>
+			    </section>
+        </div>
 		)
 	}
 }
