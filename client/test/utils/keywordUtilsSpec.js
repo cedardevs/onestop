807e67711cc19fae701ea2f8bbd3a6d157f0f266
Arianna <arianna.jakositz@colorado.edu>
2018-01-31 11:18:59 -0700

    Update tests to reflect that keyword cleaning is done before data reaches the UI

--- client/test/utils/keywordUtilsSpec.js
+++ client/test/utils/keywordUtilsSpec.js
@@ -2,44 +2,6 @@ import '../specHelper'
 import * as keywordUtils from '../../src/utils/keywordUtils'
 
 describe('The keyword utils', function(){
-  describe('title case util', function(){
-    it('reduces screaming', function(){
-      const testCases = [
-        {
-          input: 'TRMM Microwave Imager',
-          output: 'TRMM Microwave Imager',
-        },
-        {
-          input: 'TRMM > TRMM Microwave Imager',
-          output: 'TRMM Microwave Imager',
-        },
-        {
-          input: 'THERMOMETER',
-          output: 'Thermometer',
-        },
-        {
-          input: 'Thermistor',
-          output: 'Thermistor',
-        },
-        {
-          input: 'TEMPERATURE > Thermistor',
-          output: 'Thermistor',
-        },
-        {
-          input:
-            'Ocean Carbon > Biogeochemistry; Southern Ocean Iron Experiment (OCB-SOFeX)',
-          output: 'Biogeochemistry; Southern Ocean Iron Experiment (OCB-SOFeX)',
-        },
-      ]
-
-      testCases.forEach(testCase => {
-        const {input, output} = testCase
-        keywordUtils
-          .titleCaseKeyword(input)
-          .should.equal(output, `Failed for input: ${input}`)
-      })
-    })
-  })
 
   describe('hierarchy map util', function(){
     it('can build a hierarchy map with science theme data', function(){
@@ -169,127 +131,6 @@ describe('The keyword utils', function(){
       result[0].should.deep.equal(expected)
     })
 
-    it('will not fail upon encountering bad science theme data', function(){
-      const input = {
-        science: {
-          'Spectral/Engineering': {count: 10},
-          'Spectral/Engineering >\t\t\t\t\t\t\tmicrowave': {count: 1},
-          'Spectral/Engineering >\t\t\t\t\t\t\tmicrowave > Brightness Temperature': {
-            count: 1,
-          },
-          'Spectral/Engineering > Microwave': {count: 6},
-          'Spectral/Engineering > Microwave > Antenna Temperature': {count: 1},
-          'Spectral/Engineering > Microwave > Brightness Temperature': {
-            count: 6,
-          },
-        },
-      }
-
-      const expected = {
-        name: 'Data Theme',
-        id: 'Data-Theme',
-        keywordFacets: [
-          {
-            count: 10,
-            category: 'science',
-            term: 'Spectral/Engineering',
-            id: 'science-Spectral-Engineering',
-            selected: false,
-            termHierarchy: [],
-            keyword: 'Spectral/Engineering',
-          },
-          {
-            count: 1,
-            category: 'science',
-            term: 'Spectral/Engineering >\t\t\t\t\t\t\tmicrowave',
-            id: 'science-Spectral-Engineering-microwave',
-            selected: false,
-            termHierarchy: [ 'Spectral/Engineering' ],
-            keyword: 'microwave',
-          },
-          {
-            count: 1,
-            category: 'science',
-            term:
-              'Spectral/Engineering >\t\t\t\t\t\t\tmicrowave > Brightness Temperature',
-            id: 'science-Spectral-Engineering-microwave-Brightness-Temperature',
-            selected: false,
-            termHierarchy: [ 'Spectral/Engineering', 'microwave' ],
-            keyword: 'Brightness Temperature',
-          },
-          {
-            count: 6,
-            category: 'science',
-            term: 'Spectral/Engineering > Microwave',
-            id: 'science-Spectral-Engineering-Microwave',
-            selected: false,
-            termHierarchy: [ 'Spectral/Engineering' ],
-            keyword: 'Microwave',
-          },
-          {
-            count: 1,
-            category: 'science',
-            term: 'Spectral/Engineering > Microwave > Antenna Temperature',
-            id: 'science-Spectral-Engineering-Microwave-Antenna-Temperature',
-            selected: false,
-            termHierarchy: [ 'Spectral/Engineering', 'Microwave' ],
-            keyword: 'Antenna Temperature',
-          },
-          {
-            count: 6,
-            category: 'science',
-            term: 'Spectral/Engineering > Microwave > Brightness Temperature',
-            id: 'science-Spectral-Engineering-Microwave-Brightness-Temperature',
-            selected: false,
-            termHierarchy: [ 'Spectral/Engineering', 'Microwave' ],
-            keyword: 'Brightness Temperature',
-          },
-        ],
-        hierarchy: [
-          {
-            id: 'science-Spectral-Engineering',
-            children: [
-              {
-                id: 'science-Spectral-Engineering-microwave',
-                children: [
-                  {
-                    id:
-                      'science-Spectral-Engineering-microwave-Brightness-Temperature',
-                    children: [],
-                    parent: 'science-Spectral-Engineering-microwave',
-                  },
-                ],
-                parent: 'science-Spectral-Engineering',
-              },
-              {
-                id: 'science-Spectral-Engineering-Microwave',
-                children: [
-                  {
-                    id:
-                      'science-Spectral-Engineering-Microwave-Antenna-Temperature',
-                    children: [],
-                    parent: 'science-Spectral-Engineering-Microwave',
-                  },
-                  {
-                    id:
-                      'science-Spectral-Engineering-Microwave-Brightness-Temperature',
-                    children: [],
-                    parent: 'science-Spectral-Engineering-Microwave',
-                  },
-                ],
-                parent: 'science-Spectral-Engineering',
-              },
-            ],
-            parent: null,
-          },
-        ],
-      }
-
-      const result = keywordUtils.buildKeywordHierarchyMap(input, {})
-      result.length.should.equal(1)
-      result[0].should.deep.equal(expected)
-    })
-
     it('will handle non-science category data', function(){
       const input = {
         totesDifferentCategory: {
@@ -309,7 +150,7 @@ describe('The keyword utils', function(){
             id: 'totes-Different-Category-Short-Name-Long-Name',
             selected: false,
             termHierarchy: [],
-            keyword: 'Short Name > Long Name',
+            keyword: 'Long Name',
           },
           {
             count: 5,
@@ -318,7 +159,7 @@ describe('The keyword utils', function(){
             id: 'totes-Different-Category-TLA-Three-Letter-Acronym',
             selected: false,
             termHierarchy: [],
-            keyword: 'TLA > Three Letter Acronym',
+            keyword: 'Three Letter Acronym',
           },
         ],
         hierarchy: [
