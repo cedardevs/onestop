477a0be4c84eaab3ca94821ae87698d9f83718e5
ChrisEsterlein <christopher.esterlein@colorado.edu>
2018-02-07 16:48:02 -0700

    initial parser commit

---
+++ parser/src/main/groovy/org/cedar/psi/parser/ParserMain.groovy
@@ -0,0 +1,20 @@
+package org.cedar.psi.parser
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.support.SpringBootServletInitializer
+
+@SpringBootApplication
+class ParserMain extends SpringBootServletInitializer {
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    return builder.sources(ParserMain)
+  }
+
+  static void main(String[] args) {
+    SpringApplication.run(ParserMain.class, args)
+  }
+
+}
---
+++ parser/src/main/groovy/org/cedar/psi/parser/stream/ParserStreamConfig.groovy
@@ -0,0 +1,54 @@
+package org.cedar.psi.parser.stream
+
+import groovy.util.logging.Slf4j
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.serialization.Serdes
+import org.apache.kafka.streams.KafkaStreams
+import org.apache.kafka.streams.StreamsBuilder
+import org.apache.kafka.streams.StreamsConfig
+import org.apache.kafka.streams.Topology
+import org.apache.kafka.streams.kstream.KStream
+import org.apache.kafka.streams.kstream.Printed
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Slf4j
+@Configuration
+class ParserStreamConfig {
+
+  @Value('${kafka.topic}')
+  static String topic
+
+  static final String id = "${topic}-parser"
+
+  @Value('${kafka.bootstrap.servers}')
+  String bootstrapServers
+
+  @Bean
+  StreamsConfig parserStreamConfig() {
+    return new StreamsConfig([
+        (StreamsConfig.APPLICATION_ID_CONFIG)           : id,
+        (StreamsConfig.BOOTSTRAP_SERVERS_CONFIG)        : bootstrapServers,
+        (StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG)  : Serdes.String().class.name,
+        (StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG): Serdes.String().class.name,
+        (StreamsConfig.COMMIT_INTERVAL_MS_CONFIG)       : 500,
+        (ConsumerConfig.AUTO_OFFSET_RESET_CONFIG)       : "earliest"
+    ])
+  }
+
+  @Bean
+  Topology parserTopology() {
+    def builder = new StreamsBuilder()
+    KStream stream = builder.stream(topic)
+    stream.print(Printed.toSysOut().withLabel(topic))
+    return builder.build()
+  }
+
+
+  @Bean(initMethod = 'start', destroyMethod = 'close')
+  KafkaStreams parserStream(Topology parserTopology, StreamsConfig parserStreamConfig) {
+    return new KafkaStreams(parserTopology, parserStreamConfig)
+  }
+
+}
