887dfa708913d6ce5f23072ed84871da93e00f0b
Semere Ghebrechristos <ghebrecs@colorado.edu>
2018-02-07 17:01:44 -0700

    update parser build script to include jar and remove dependencies

--- parser/src/main/groovy/org/cedar/psi/parser/ParserMain.groovy
+++ parser/src/main/groovy/org/cedar/psi/parser/ParserMain.groovy
@@ -3,7 +3,7 @@ package org.cedar.psi.parser
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
-import org.springframework.boot.web.support.SpringBootServletInitializer
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 class ParserMain extends SpringBootServletInitializer {
