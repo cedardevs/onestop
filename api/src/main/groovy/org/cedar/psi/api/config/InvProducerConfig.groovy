2fca67d2d7b0ae454381695a7fa7f3ecae8d376a
Semere Ghebrechristos <ghebrecs@colorado.edu>
2018-02-23 14:52:03 -0700

    update api from using spring kafka to be consistent with the stream projects and first pass on Schema Registry store (what we get from CI), just for the api

--- api/src/main/groovy/org/cedar/psi/api/config/InvProducerConfig.groovy
+++ api/src/main/groovy/org/cedar/psi/api/config/InvProducerConfig.groovy
@@ -1,13 +1,15 @@
 package org.cedar.psi.api.config
 
+import io.confluent.kafka.serializers.KafkaAvroSerializer
+import io.confluent.kafka.serializers.KafkaAvroSerializerConfig
+import org.apache.avro.generic.GenericRecord
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.Producer
 import org.apache.kafka.clients.producer.ProducerConfig
-import org.apache.kafka.common.serialization.StringSerializer
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.kafka.core.DefaultKafkaProducerFactory
-import org.springframework.kafka.core.KafkaTemplate
-import org.springframework.kafka.core.ProducerFactory
+
 
 @Configuration
 class InvProducerConfig {
@@ -15,17 +17,22 @@ class InvProducerConfig {
     @Value('${kafka.bootstrap.servers}')
     private String bootstrapServer
 
-    @Bean
-    ProducerFactory<String, String> producerFactory() {
-        Map<String, Object> configProps = new HashMap<>()
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer)
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class)
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class)
-        return new DefaultKafkaProducerFactory<String, String>(configProps)
-    }
+    @Value('${kafka.schema.registry}')
+    private String schemaUrl
 
     @Bean
-    KafkaTemplate<String, String> kafkaTemplate() {
-        return new KafkaTemplate<>(producerFactory())
+    Producer<String, GenericRecord> createProducer() {
+        Map<String, Object> configProps = new HashMap<>()
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer)
+        configProps.put(ProducerConfig.CLIENT_ID_CONFIG, "granuleProducer")
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer)
+
+        // Configure the KafkaAvroSerializer.
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer)
+
+        // Schema Registry location.
+        configProps.put(KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaUrl)
+
+        return new KafkaProducer<>(configProps)
     }
 }
--- api/src/main/groovy/org/cedar/psi/api/services/InvPublisher.groovy
+++ api/src/main/groovy/org/cedar/psi/api/services/InvPublisher.groovy
@@ -1,24 +1,50 @@
 package org.cedar.psi.api.services
 
 import groovy.json.JsonSlurper
+import org.apache.avro.Schema
+import org.apache.avro.generic.GenericData
+import org.apache.avro.generic.GenericRecord
+import org.apache.kafka.clients.producer.Producer
+import org.apache.kafka.clients.producer.ProducerRecord
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.context.annotation.Configuration
 import org.springframework.stereotype.Service
 
 @Service
+@Configuration
 class InvPublisher {
     private static final Logger log = LoggerFactory.getLogger(InvPublisher.class)
 
     @Autowired
-    private KafkaTemplate<String, String> kafkaTemplate
+    private Producer<String, GenericRecord> createProducer
 
     @Value('${kafka.granule.topic}')
-    String kafkaTopic
+    String GRANULETOPIC
 
     void publishGranule(String data) {
+
+        String GRANULE_SCHEMA = "{" +
+                "   \"namespace\": \"org.cedar.psi.api\"," +
+                "   \"type\": \"record\", " +
+                "   \"name\": \"Granule\"," +
+                "   \"fields\": [" +
+                "       {\"name\": \"trackingId\", \"type\": \"string\"}," +
+                "       {\"name\": \"dataStream\", \"type\": \"string\"}," +
+                "       {\"name\": \"checksum\", \"type\": \"string\"}," +
+                "       {\"name\": \"relativePath\", \"type\": \"string\"}," +
+                "       {\"name\": \"path\", \"type\": \"string\"}," +
+                "       {\"name\": \"fileSize\", \"type\": [\"null\",\"int\"], \"default\":\"null\"}," +
+                "       {\"name\": \"lastUpdated\", \"type\": [\"null\",\"string\"], \"default\":\"null\" }" +
+                    " ]" +
+                "}"
+
+        Producer<String, GenericRecord> producer = createProducer
+        Schema.Parser parser = new Schema.Parser()
+        Schema schema = parser.parse(GRANULE_SCHEMA)
+
         def slurper = new JsonSlurper()
         def slurpedKey = slurper.parseText(data)
 
@@ -26,8 +52,22 @@ class InvPublisher {
             log.debug("missing trackingid from ='{}", data)
         } else {
             String key = (slurpedKey.trackingId).toString()
-            kafkaTemplate.send(kafkaTopic, key , data)
+            GenericRecord granule = new GenericData.Record(schema)
             log.info("sending data ='{}'", data)
+            granule.put("trackingId", slurpedKey.trackingId)
+            granule.put("dataStream", slurpedKey.dataStream)
+            granule.put("checksum", slurpedKey.checksum)
+            granule.put("relativePath", slurpedKey.relativePath)
+            granule.put("path", slurpedKey.path)
+            granule.put("fileSize", slurpedKey.fileSize)
+            granule.put("lastUpdated", slurpedKey.lastUpdated)
+
+            ProducerRecord<String, GenericRecord> record = new ProducerRecord<String, GenericRecord>(GRANULETOPIC, key, granule)
+            log.info("topic = %s, partition = %s, offset = %d, customer = %s, country = %s\n",
+                    record.topic(), record.partition(),
+                    record.key(), record.value())
+            producer.send(record)
+
         }
     }
 }
