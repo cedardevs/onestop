8d4eef22761c3f9d870f7b47a2ee802ff8dd8b4d
ChrisEsterlein <christopher.esterlein@colorado.edu>
2018-02-27 14:34:19 -0700

    add kafka-rest service, schema-registry, kafka-ui, alphabetized services

---
+++ workflow/src/main/groovy/org/cedar/psi/workflow/WorkflowMain.groovy
@@ -0,0 +1,7 @@
+package org.cedar.psi.workflow
+
+/**
+ * Created by chris on 2/16/18.
+ */
+class WorkflowMain {
+}
---
+++ workflow/src/main/groovy/org/cedar/psi/workflow/stream/WorkflowStreamConfig.groovy
@@ -0,0 +1,7 @@
+package org.cedar.psi.workflow.stream
+
+/**
+ * Created by chris on 2/16/18.
+ */
+class WorkflowStreamConfig {
+}
