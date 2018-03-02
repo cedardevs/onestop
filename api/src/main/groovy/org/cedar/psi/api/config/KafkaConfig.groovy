442be0e32e62ee03d4e4aab7d9f30cbe89953f4f
Evan McQuinn <evan.mcquinn@colorado.edu>
2018-03-02 16:28:10 -0700

    Add an endpoint for receiving collection xml and sending it to kafka
    Also added some tests! Also cleaned some things up.

---
+++ api/src/main/groovy/org/cedar/psi/api/config/KafkaConfig.groovy
@@ -0,0 +1,28 @@
+package org.cedar.psi.api.config
+
+import groovy.transform.CompileStatic
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.Producer
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@CompileStatic
+@Configuration
+class KafkaConfig {
+
+  @Value('${kafka.bootstrap.servers}')
+  private String bootstrapServer
+
+  @Bean
+  Producer<String, String> kafkaProducer() {
+    Map<String, Object> configProps = new HashMap<>()
+    configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer)
+    configProps.put(ProducerConfig.CLIENT_ID_CONFIG, "api_publisher")
+    configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName())
+    configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName())
+    return new KafkaProducer<>(configProps)
+  }
+}
---
+++ api/src/test/groovy/org/cedar/psi/api/service/PublisherSpec.groovy
@@ -0,0 +1,59 @@
+package org.cedar.psi.api.service
+
+import org.apache.kafka.clients.producer.Producer
+import org.apache.kafka.clients.producer.ProducerRecord
+import spock.lang.Specification
+
+
+class PublisherSpec extends Specification {
+
+  def mockProducer = Mock(Producer)
+  def publisher = new Publisher(mockProducer).with {
+    it.GRANULE_TOPIC = 'test_granule'
+    it.COLLECTION_TOPIC = 'test_collection'
+    return it
+  }
+
+  def 'publishes valid granules'() {
+    def data = '{"trackingId": "ABC", "path": "/test/file.txt"}'
+
+    when:
+    publisher.publishGranule(data)
+
+    then:
+    1 * mockProducer.send({it instanceof ProducerRecord && it.key() == 'ABC' && it.value() == data})
+  }
+
+  def 'does not publish granules if no tracking id present'() {
+    def data = '{"path": "/test/file.txt"}'
+
+    when:
+    publisher.publishGranule(data)
+
+    then:
+    0 * mockProducer.send(_)
+  }
+
+  def 'publishes collections with a given id'() {
+    def id = 'ABC'
+    def data = '<text>xml woooo....</text>'
+
+    when:
+    publisher.publishCollection(data, id)
+
+    then:
+    1 * mockProducer.send({it instanceof ProducerRecord && it.key() == 'ABC' && it.value() == data})
+  }
+
+  // TODO - not sure that we actually want to do this in the future
+  def 'makes up an id for collections without a given id'() {
+    def data = '<text>xml woooo....</text>'
+
+    when:
+    publisher.publishCollection(data)
+
+    then:
+    1 * mockProducer.send({it instanceof ProducerRecord && it.key() != null && it.value() == data})
+  }
+
+}
