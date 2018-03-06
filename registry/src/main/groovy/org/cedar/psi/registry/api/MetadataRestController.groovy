3ed1b35f26083fc3b70a180de50b9e1a085a81b1
Evan McQuinn <evan.mcquinn@colorado.edu>
2018-03-05 17:49:11 -0700

    Streams app now reduces and stores collection events as well

--- registry/src/main/groovy/org/cedar/psi/registry/api/MetadataRestController.groovy
+++ registry/src/main/groovy/org/cedar/psi/registry/api/MetadataRestController.groovy
@@ -1,9 +1,10 @@
 package org.cedar.psi.registry.api
 
+import groovy.json.JsonSlurper
 import groovy.util.logging.Slf4j
 import org.apache.kafka.streams.KafkaStreams
 import org.apache.kafka.streams.state.QueryableStoreTypes
-import org.cedar.psi.registry.stream.RawGranuleStreamConfig
+import org.cedar.psi.registry.stream.MetadataStreamConfig
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -12,36 +13,48 @@ import org.springframework.web.bind.annotation.RestController
 import javax.servlet.http.HttpServletResponse
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET
-import static org.springframework.web.bind.annotation.RequestMethod.PUT
 import static org.springframework.web.bind.annotation.RequestMethod.HEAD
 
 @Slf4j
 @RestController
 class MetadataRestController {
 
-  private KafkaStreams rawGranuleStream
+  private KafkaStreams rawMetadataStream
 
   @Autowired
-  MetadataRestController(KafkaStreams rawGranuleStream) {
-    this.rawGranuleStream = rawGranuleStream
+  MetadataRestController(KafkaStreams rawMetadataStream) {
+    this.rawMetadataStream = rawMetadataStream
   }
 
 
   @RequestMapping(path = '/metadata/{type}/{id}', method = [GET, HEAD], produces = 'application/json')
   Map retrieveJson(@PathVariable String type, @PathVariable String id, HttpServletResponse response) {
-    if (type == 'granule') {
-      def granuleStore = this.rawGranuleStream.store(RawGranuleStreamConfig.storeName, QueryableStoreTypes.keyValueStore())
-      return [id: id, value: granuleStore.get(id)]
+    def storeName =
+        type == 'granule' ? MetadataStreamConfig.RAW_GRANULE_STORE :
+            type == 'collection' ? MetadataStreamConfig.RAW_COLLECTION_STORE : null
+
+    if (storeName) {
+      def value = getFromStreamStore(rawMetadataStream, storeName, id)
+      if (value) {
+        return [id: id, value: value]
+      }
     }
+
     response.sendError(404)
     return null
   }
 
+  private static Map getFromStreamStore(KafkaStreams streamsApp, String storeName, String id) {
+    def store = streamsApp.store(storeName, QueryableStoreTypes.keyValueStore())
+    return store ? new JsonSlurper().parseText(store.get(id) as String) as Map : null
+  }
+
+
 //  @RequestMapping(path='/metadata/rebuild', method = [PUT], produces = 'application/json')
 //  Map rebuild() {
-//    this.rawGranuleStream.close()
-//    this.rawGranuleStream.cleanUp()
-//    this.rawGranuleStream.start()
+//    this.metadataStream.close()
+//    this.metadataStream.cleanUp()
+//    this.metadataStream.start()
 //    return ['acknowledged': true]
 //  }
 
