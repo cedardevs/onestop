4b565bfb7de35dc159c2b47734bbc1316296db4a
Zeb <elizabeth.delk@colorado.edu>
2018-03-22 11:16:06 -0600

    Pull out font-awesome svg icon as a component.

--- client/src/detail/SummaryView.jsx
+++ client/src/detail/SummaryView.jsx
@@ -6,6 +6,13 @@ import A from '../common/link/Link'
 import MapThumbnail from '../common/MapThumbnail'
 import FlexRow from '../common/FlexRow'
 import Expandable from '../common/Expandable'
+import {
+  star,
+  star_o,
+  star_half_o,
+  info_circle,
+  SvgIcon,
+} from '../common/SvgIcon'
 
 const styleContainer = {
   padding: '1.618em',
@@ -141,13 +148,13 @@ class SummaryView extends Component {
     else {
       for (let i = 0; i < 5; i++) {
         if (i < fullStars) {
-          stars.push(this.renderStar(i, this.fullStarPath())) //this.renderFullStar(i))
+          stars.push(this.renderStar(i, star))
         }
         else if (i === fullStars && halfStar) {
-          stars.push(this.renderStar(i, this.halfStarPath()))
+          stars.push(this.renderStar(i, star_half_o))
         }
         else {
-          stars.push(this.renderStar(i, this.emptyStarPath()))
+          stars.push(this.renderStar(i, star_o))
         }
       }
     }
@@ -156,15 +163,22 @@ class SummaryView extends Component {
       <FlexRow
         items={[
           <FlexRow
+            key="dsmm-stars"
             items={[
               stars,
               <span
+                key="dsmm-text-value"
                 style={{fontSize: '0px'}}
               >{`${dsmmDesc} DSMM rating`}</span>,
             ]}
           />,
           <Expandable
-            heading={this.renderInfoCircle()}
+            key="dsmm-info"
+            heading={
+              <div aria-label="DSMM info" style={{width: '1em'}}>
+                <SvgIcon style={styleInfoIcon} path={info_circle} />
+              </div>
+            }
             open={false}
             content={
               <div>
@@ -200,39 +214,9 @@ class SummaryView extends Component {
 
   renderStar = (i, path) => {
     return (
-      <span style={{width: '1em'}}>
-        <svg key={i} style={styleStar} viewBox="0 0 1792 1792">
-          {path}
-        </svg>
+      <span key={`dsmm-star-${i}`} style={{width: '1em'}}>
+        <SvgIcon style={styleStar} path={path} />
       </span>
-    )
-  }
-  fullStarPath = () => {
-    // from font-awesome star.svg
-    return (
-      <path d="M1728 647q0 22-26 48l-363 354 86 500q1 7 1 20 0 21-10.5 35.5t-30.5 14.5q-19 0-40-12l-449-236-449 236q-22 12-40 12-21 0-31.5-14.5t-10.5-35.5q0-6 2-20l86-500-364-354q-25-27-25-48 0-37 56-46l502-73 225-455q19-41 49-41t49 41l225 455 502 73q56 9 56 46z" />
-    )
-  }
-  halfStarPath = () => {
-    // from font-awesome star-half-o.svg
-    return (
-      <path d="M1250 957l257-250-356-52-66-10-30-60-159-322v963l59 31 318 168-60-355-12-66zm452-262l-363 354 86 500q5 33-6 51.5t-34 18.5q-17 0-40-12l-449-236-449 236q-23 12-40 12-23 0-34-18.5t-6-51.5l86-500-364-354q-32-32-23-59.5t54-34.5l502-73 225-455q20-41 49-41 28 0 49 41l225 455 502 73q45 7 54 34.5t-24 59.5z" />
-    )
-  }
-  emptyStarPath = () => {
-    // from font-awesome star-o.svg
-    return (
-      <path d="M1201 1004l306-297-422-62-189-382-189 382-422 62 306 297-73 421 378-199 377 199zm527-357q0 22-26 48l-363 354 86 500q1 7 1 20 0 50-41 50-19 0-40-12l-449-236-449 236q-22 12-40 12-21 0-31.5-14.5t-10.5-35.5q0-6 2-20l86-500-364-354q-25-27-25-48 0-37 56-46l502-73 225-455q19-41 49-41t49 41l225 455 502 73q56 9 56 46z" />
-    )
-  }
-  renderInfoCircle = () => {
-    // from font-awesome info-circle.svg
-    return (
-      <div aria-label="DSMM info" style={{width: '1em'}}>
-        <svg style={styleInfoIcon} viewBox="0 0 1792 1792">
-          <path d="M1152 1376v-160q0-14-9-23t-23-9h-96v-512q0-14-9-23t-23-9h-320q-14 0-23 9t-9 23v160q0 14 9 23t23 9h96v320h-96q-14 0-23 9t-9 23v160q0 14 9 23t23 9h448q14 0 23-9t9-23zm-128-896v-160q0-14-9-23t-23-9h-192q-14 0-23 9t-9 23v160q0 14 9 23t23 9h192q14 0 23-9t9-23zm640 416q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z" />
-        </svg>
-      </div>
     )
   }
 
