32df09bf19acf7abc529f9f37e2bb4b966c2330e
forgo <elliott.richerson@gmail.com>
2018-03-26 12:47:56 -0600

    Merge branch 'master' into 445-granule-list-view
    # Conflicts:
    #	client/src/actions/FlowActions.js
    #	client/src/detail/Detail.jsx
    #	client/src/detail/GranuleTab/GranuleMapContainer.js
    #	client/src/detail/SummaryView.jsx
    #	client/src/index.jsx
    #	client/src/reducers/ui/layout.js
    #	client/src/result/granules/GranuleList.jsx
    #	client/src/utils/urlUtils.js
    #	client/test/utils/urlUtilsSpec.js

--- client/src/detail/Detail.jsx
+++ client/src/detail/Detail.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import Tabs from './Tabs'
 import SummaryView from './SummaryView'
 import DescriptionView from './DescriptionView'
-import GranuleViewContainer from './GranuleTab/GranuleViewContainer'
+// import GranuleViewContainer from './GranuleTab/GranuleViewContainer'
 import AccessView from './AccessView'
 import VideoView from './VideoView'
 import {boxShadow} from '../common/defaultStyles'
@@ -88,13 +88,13 @@ class Detail extends Component {
       },
     ]
 
-    if (totalGranuleCount > 0) {
-      tabData.splice(2, 0, {
-        title: 'Matching Files',
-        content: <GranuleViewContainer item={item} />,
-        action: this.showGranules,
-      })
-    }
+    // if (totalGranuleCount > 0) {
+    //   tabData.splice(2, 0, {
+    //     title: 'Matching Files',
+    //     content: <GranuleViewContainer item={item} />,
+    //     action: this.showGranules,
+    //   })
+    // }
 
     const videoLinks = item.links.filter(
       link => link.linkProtocol === 'video:youtube'
@@ -120,51 +120,6 @@ class Detail extends Component {
       </div>
     )
   }
-
-  // None of this links stuff is being used in the new version of the collection view.
-  // Preserving it for now because it may be needed for one of the tabs that we haven't added yet.
-
-  // getLinks() {
-  //   return (this.props && this.props.item && this.props.item.links) || [];
-  // }
-
-  // {/*{this.renderLinks('More Info', this.getLinksByType('information'), this.renderLink)}*/}
-  // {/*{this.renderLinks('Data Access', this.getLinksByType('download'), this.renderLink)}*/}
-
-  // getLinksByType(type) {
-  //   return this.getLinks().filter(link => link.linkFunction === type);
-  // }
-
-  // renderLinks(label, links, linkRenderer) {
-  //   if (!links || links.length === 0) {
-  //     return <div />;
-  //   }
-  //
-  //   return (
-  //     <div className={'pure-g'}>
-  //       <div className={`pure-u-1-6 ${styles.linkRow}`}>
-  //         <span>{label}</span>
-  //       </div>
-  //       <div className={`pure-u-5-6 ${styles.linkRow}`}>
-  //         <ul className={'pure-g'}>{links.map(linkRenderer)}</ul>
-  //       </div>
-  //     </div>
-  //   );
-  // }
-  //
-  // renderLink(link, index) {
-  //   return (
-  //     <li className={'pure-u'} key={index}>
-  //       <A
-  //         href={link.linkUrl}
-  //         target="_blank"
-  //         className={`pure-button pure-button-primary`}
-  //       >
-  //         {link.linkProtocol || 'Link'}
-  //       </A>
-  //     </li>
-  //   );
-  // }
 }
 
 Detail.propTypes = {
