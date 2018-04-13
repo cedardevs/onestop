cfc7dcdd508464d2777231aadd4183bdd9ef21d8
Semere Ghebrechristos <ghebrecs@colorado.edu>
2018-04-13 14:14:18 -0600

    updated script wrapper to take an identity function (prevent script wrapper from calling the iso parsing logic every time)

--- script-wrapper/src/test/groovy/org/cedar/psi/wrapper/ScriptWrapperFunctionsSpec.groovy
+++ script-wrapper/src/test/groovy/org/cedar/psi/wrapper/ScriptWrapperFunctionsSpec.groovy
@@ -4,35 +4,38 @@ import org.cedar.psi.wrapper.stream.ScriptWrapperFunctions
 import spock.lang.Specification
 
 class ScriptWrapperFunctionsSpec extends Specification {
-    def 'Script Publishs stdout'() {
-        def msg = '{"trackingId":"ABC","message":"this is a test","answer": 42}'
-        def command_timeout =  10
-        String command = "echo stdout"
-        def expected = 'stdout\n'
+  def command_timeout = 10
+  def msg = '{"trackingId":"ABC","message":"this is a test","answer": 42}'
 
-        expect:
-        ScriptWrapperFunctions.scriptCaller(msg, command, command_timeout) == expected
-    }
+  def 'Script Publishs stdout'() {
+    String command = "echo stdout"
+    def expected = 'stdout\n'
 
-    void "Script wrapper exit with value 0: command not provided"() {
-        def msg = '{"trackingId":"ABC","message":"this is a test","answer": 42}'
-        def command_timeout = 10
-        String command = " "
-        def expected = 'ERROR: ' + 0
+    expect:
+    ScriptWrapperFunctions.scriptCaller(msg, command, command_timeout) == expected
+  }
 
-        expect:
-        ScriptWrapperFunctions.scriptCaller(msg, command, command_timeout) == expected
-    }
+  void "Script wrapper exit with value 0: command not provided"() {
+    String command = " "
+    def expected = 'ERROR: ' + 0
 
-    void "Script wrapper exit with value non-zero value: command Cannot run program"() {
-        def msg = '{"trackingId":"ABC","message":"this is a test","answer": 42}'
-        def command_timeout = 33
-        String command = "py wrong command"
+    expect:
+    ScriptWrapperFunctions.scriptCaller(msg, command, command_timeout) == expected
+  }
 
-        def expected ='ERROR: Cannot run program "py": error=2, No such file or directory'
+  void "Script wrapper exit with value non-zero value: command Cannot run program"() {
+    String command = "py wrong command"
+    def expected = 'ERROR: Cannot run program "py": error=2, No such file or directory'
 
-        expect:
-        ScriptWrapperFunctions.scriptCaller(msg, command, command_timeout) == expected
-    }
+    expect:
+    ScriptWrapperFunctions.scriptCaller(msg, command, command_timeout) == expected
+  }
+
+  void "Check for identity function (mainly for collection)"() {
+    String command = "echo"
+
+    expect:
+    ScriptWrapperFunctions.scriptCaller(msg, command, command_timeout) == msg
+  }
 
 }
