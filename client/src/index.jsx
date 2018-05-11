0db14366213bde4aff4644f93aa316938889a3d5
Zeb <elizabeth.delk@colorado.edu>
2018-05-11 09:54:09 -0600

    Add NCEI as publisher in json-ld block.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -93,6 +93,17 @@ jsonLdScript.insertAdjacentHTML(
       "@type": "SearchAction",
       "target": "${rootUrl}#/collections?q={search_term_string}",
       "query-input": "required name=search_term_string"
+    },
+    "publisher": {
+      "@type": "Organization",
+      "@id": "https://www.ncei.noaa.gov/",
+      "name": "National Centers for Environmental Information (NCEI)",
+      "logo": {
+          "@type": "ImageObject",
+          "url": "https://www.ncei.noaa.gov/sites/default/files/noaa_logo_circle_72x72.svg",
+          "width": "72",
+          "height": "72"
+      }
     }
   }`
 )
