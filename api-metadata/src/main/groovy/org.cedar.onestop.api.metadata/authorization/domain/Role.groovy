4bacaf8f7b6509d8ac661d561009cdb62ef02fa6
Bao Nguyen <nguyenbv@ciresits-MacBook-Pro-4.local>
2018-06-22 14:48:27 -0600

    Add Role and many-to-many mappings

---
+++ api-metadata/src/main/groovy/org.cedar.onestop.api.metadata/authorization/domain/Role.groovy
@@ -0,0 +1,30 @@
+package org.cedar.onestop.api.metadata.authorization.domain
+
+import org.cedar.onestop.api.metadata.authorization.domain.User
+
+import javax.persistence.CascadeType
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.FetchType
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.ManyToMany
+import javax.persistence.Table
+import javax.validation.constraints.NotEmpty
+
+@Entity
+@Table(name='role')
+class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name='role_id')
+    private Long id
+
+    @NotEmpty
+    @Column(name='role')
+    private String Role
+
+    @ManyToMany(fetch=FetchType.LAZY, cascade={ CascadeType.PERSIST, CascadeType.MERGE }, mappedBy = 'roles')
+    private List<User> users = []
+}
--- api-metadata/src/main/groovy/org.cedar.onestop.api.metadata/authorization/domain/User.groovy
+++ api-metadata/src/main/groovy/org.cedar.onestop.api.metadata/authorization/domain/User.groovy
@@ -1,9 +1,16 @@
-package org.cedar.onestop.api.metadata.authorization.domains
+package org.cedar.onestop.api.metadata.authorization.domain
 
+import org.cedar.onestop.api.metadata.authorization.domain.Role
+
+import javax.persistence.CascadeType
 import javax.persistence.Column
+import javax.persistence.FetchType
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.JoinTable
+import javax.persistence.ManyToMany
 import javax.persistence.Table
 import javax.persistence.Entity
 import javax.validation.constraints.NotEmpty
@@ -13,7 +20,7 @@ import javax.validation.constraints.NotEmpty
 class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name='id')
+    @Column(name='user_id')
     private Long userId
 
     @NotEmpty
@@ -23,4 +30,12 @@ class User {
     @NotEmpty
     @Column(name='password')
     private String password
+
+    @ManyToMany(fetch=FetchType.LAZY, cascade={ CascadeType.PERSIST, CascadeType.MERGE })
+    @JoinTable(
+            name='user_roles',
+            joinColumns=[@JoinColumn(name='user_id')],
+            inverseJoinColumns=[@JoinColumn(name='role_id')]
+    )
+    private List<Role> roles = []
 }
