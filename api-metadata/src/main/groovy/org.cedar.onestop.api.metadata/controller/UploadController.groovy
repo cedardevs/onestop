a7a6600e0910a46f3c629c4011e737a41b69811e
Zeb <elizabeth.delk@colorado.edu>
2018-06-25 13:45:33 -0600

    Changed form to have the correct relative url automatically.

--- api-metadata/src/main/groovy/org.cedar.onestop.api.metadata/controller/UploadController.groovy
+++ api-metadata/src/main/groovy/org.cedar.onestop.api.metadata/controller/UploadController.groovy
@@ -3,13 +3,14 @@ package org.cedar.onestop.api.metadata.controller
 import groovy.util.logging.Slf4j
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.stereotype.Controller
+import org.springframework.beans.factory.annotation.Value
 
 import javax.servlet.http.HttpServletResponse
-// import org.springframework.ui.Model
 
 @Slf4j
 @Controller
 class UploadController {
+
 
   @GetMapping('/upload.html')
   String upload(HttpServletResponse response) {
