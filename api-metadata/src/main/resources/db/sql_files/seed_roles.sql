7fe5ac87bad20eb8ac205be15bf50d2c5d45a4fe
Bao Nguyen <nguyenbv@ciresits-MacBook-Pro-4.local>
2018-06-29 16:17:05 -0600

    Removed SQL files, add seeding as part of changelog.

--- api-metadata/src/main/resources/db/sql_files/seed_roles.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO testSchema.roles (role) VALUES ('admin');
-INSERT INTO testSchema.roles (role) VALUES ('user');
--- api-metadata/src/main/resources/db/sql_files/seed_users.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO testSchema.user (username, password) VALUES ('bao', 'pass01');
-INSERT INTO testSchema.user (username, password) VALUES ('elliot', 'pass02');
-INSERT INTO testSchema.user (username, password) VALUES ('zeb', 'pass03');
