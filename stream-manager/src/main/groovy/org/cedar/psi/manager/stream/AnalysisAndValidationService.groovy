c8a5bd446ed60557cad2af849564d5d8589227d3
arianna <arianna.jakositz@colorado.edu>
2018-07-30 15:52:46 -0600

    Validation placeholder

--- stream-manager/src/main/groovy/org/cedar/psi/manager/stream/AnalysisAndValidationService.groovy
+++ stream-manager/src/main/groovy/org/cedar/psi/manager/stream/AnalysisAndValidationService.groovy
@@ -2,16 +2,19 @@ package org.cedar.psi.manager.stream
 
 import groovy.json.JsonOutput
 import groovy.json.JsonSlurper
+import groovy.util.logging.Slf4j
 
+@Slf4j
 class AnalysisAndValidationService {
 
   static String analyzeParsedMetadata(String msg) {
+    log.debug("Received $msg")
     def msgMap = new JsonSlurper().parseText(msg) as Map
     def analysisMap = [:]
-    def responseMap = msgMap.put('analysis', analysisMap)
+    msgMap.put('analysis', analysisMap)
 
     // TODO Insert analysis functions...
 
-    return JsonOutput.toJson(responseMap)
+    return JsonOutput.toJson(msgMap)
   }
 }
