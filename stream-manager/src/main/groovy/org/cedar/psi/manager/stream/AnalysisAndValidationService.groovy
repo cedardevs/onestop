4f1368fd65ba30222c8d6397d444533843cd7773
arianna <arianna.jakositz@colorado.edu>
2018-08-22 12:22:09 -0600

    layout of analysis map wip

--- stream-manager/src/main/groovy/org/cedar/psi/manager/stream/AnalysisAndValidationService.groovy
+++ stream-manager/src/main/groovy/org/cedar/psi/manager/stream/AnalysisAndValidationService.groovy
@@ -7,11 +7,33 @@ class AnalysisAndValidationService {
 
   static Map analyzeParsedMetadata(Map msgMap) {
     log.debug("Received $msgMap")
-    def analysisMap = [:]
+
+    def analysisMap = [
+        identification: analyzeIdentifiers(msgMap),
+        temporalBoudning: analyzeTemporalBounding(msgMap),
+        spatialBounding: analyzeSpatialBounding(msgMap),
+        titles: analyzeTitles(msgMap),
+        description: analyzeDescription(msgMap),
+        thumbnail: analyzeThumbnail(msgMap),
+        dataAccess: analyzeDataAccess(msgMap)
+    ]
+
     msgMap.put('analysis', analysisMap)
-
-    // TODO Insert analysis functions...
-
     return msgMap
   }
+
+
+  static Map analyzeIdentifiers(Map msgMap) {}
+
+  static Map analyzeTemporalBounding(Map msgMap) {}
+
+  static Map analyzeSpatialBounding(Map msgMap) {}
+
+  static Map analyzeTitles(Map msgMap) {}
+
+  static Map analyzeDescription(Map msgMap) {}
+
+  static Map analyzeThumbnail(Map msgMap) {}
+
+  static Map analyzeDataAccess(Map msgMap) {}
 }
