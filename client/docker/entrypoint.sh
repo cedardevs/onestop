e1862379625520858ef5ee4464b42a21799997aa
forgo <elliott.richerson@gmail.com>
2018-09-13 17:19:06 -0600

    google verification support in nginx config

--- client/docker/entrypoint.sh
+++ client/docker/entrypoint.sh
@@ -3,4 +3,6 @@
 sed -i -e "s/ONESTOP_ENDPOINT/${ONESTOP_ENDPOINT:-api-search:8097}/" /etc/nginx/conf.d/default.conf
 sed -i -e "s/GEOPORTAL_ENDPOINT/${GEOPORTAL_ENDPOINT:-geoportal-search:8080}/" /etc/nginx/conf.d/default.conf
 sed -i -e "s/ONESTOP_ADMIN_ENDPOINT/${ONESTOP_ADMIN_ENDPOINT:-api-metadata:8098}/" /etc/nginx/conf.d/default.conf
+sed -i -e "s/GOOGLE_VERIFY_OWNERSHIP_FILE/${GOOGLE_VERIFY_OWNERSHIP_FILE:google-verify-ownership-file-not-set.html}/g" /etc/nginx/conf.d/default.conf
+
 nginx -g 'daemon off;'
