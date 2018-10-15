084f4c95c1c042fd20acf29cc88b33383252e773
forgo <elliott.richerson@gmail.com>
2018-10-15 17:06:47 -0600

    use href instead of to in FooterLink attribute for external links not using react router

--- client/src/root/Footer.jsx
+++ client/src/root/Footer.jsx
@@ -102,7 +102,7 @@ class Footer extends React.Component {
         i === links.length - 1 ? styleLinkLast : styleLinkNotLast
       return (
         <li style={styleFooterItem} key={i}>
-          <FooterLink to={link.href} title={link.text} style={styleLink}>
+          <FooterLink href={link.href} title={link.text} style={styleLink}>
             {link.text}{' '}
           </FooterLink>
         </li>
