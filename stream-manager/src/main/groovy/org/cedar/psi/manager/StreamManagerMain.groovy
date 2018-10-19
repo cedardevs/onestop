858f8e5ab4a60b0fdfad4bf8136c0cf634e47de2
Evan McQuinn <evan.mcquinn@colorado.edu>
2018-10-19 15:32:03 -0600

    Factor out environment access and test

--- stream-manager/src/main/groovy/org/cedar/psi/manager/StreamManagerMain.groovy
+++ stream-manager/src/main/groovy/org/cedar/psi/manager/StreamManagerMain.groovy
@@ -8,13 +8,23 @@ import org.cedar.psi.manager.stream.StreamManager
 class StreamManagerMain {
 
   static void main(String[] args) {
-    def bootstrapServers = getBootstrapServers(System.getenv(), args)
+    def bootstrapServers = getBootstrapServers(getEnv(), args)
     log.info("Starting stream with bootstrap servers ${bootstrapServers}")
     def streams = StreamManager.buildStreamsApp(bootstrapServers)
     streams.start()
 
     // Add shutdown hook to respond to SIGTERM and gracefully close Kafka Streams
     Runtime.getRuntime().addShutdownHook(new Thread({ streams.close() }))
+  }
+
+  static Map getEnv() {
+    try {
+      return System.getenv()
+    }
+    catch(SecurityException e) {
+      log.error 'Application does not have permission to read environment variables. Using defaults.'
+      return [:]
+    }
   }
 
   // Check for supplied bootstrap servers before using default.
@@ -24,14 +34,7 @@ class StreamManagerMain {
       return args[0]
     }
 
-    try {
-      return env['KAFKA_BOOTSTRAP_SERVERS'] ?: env['IM_BOOTSTRAP_SERVERS'] ?: Constants.BOOTSTRAP_DEFAULT
-    }
-    catch(SecurityException e) {
-      log.error 'Application does not have permission to read environment variables.\n' +
-          'Defaulting to "localhost:9092" as bootstrap servers.'
-      return Constants.BOOTSTRAP_DEFAULT
-    }
+    return env['KAFKA_BOOTSTRAP_SERVERS'] ?: env['IM_BOOTSTRAP_SERVERS'] ?: Constants.BOOTSTRAP_DEFAULT
   }
 
 }
--- stream-manager/src/test/groovy/org/cedar/psi/manager/StreamManagerMainSpec.groovy
+++ stream-manager/src/test/groovy/org/cedar/psi/manager/StreamManagerMainSpec.groovy
@@ -5,6 +5,19 @@ import spock.lang.Specification
 
 class StreamManagerMainSpec extends Specification {
 
+  def 'retrieves env'() {
+    expect:
+    StreamManagerMain.getEnv() ==  System.getenv()
+  }
+
+  def 'handles secured env'() {
+    GroovySpy(System, global: true)
+    System.getenv() >> { throw new SecurityException() }
+
+    expect:
+    StreamManagerMain.getEnv() ==  [:]
+  }
+
   def 'pulls bootstrap config from env'() {
     def envServers = 'env:9092'
     def env = ['KAFKA_BOOTSTRAP_SERVERS': envServers]
