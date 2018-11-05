b371f4830ca891ad12a4e2919d45f6b04214f1ef
Zeb <elizabeth.delk@colorado.edu>
2018-11-05 12:49:20 -0700

    Updated client test coverage.
    Note: coverage thresholds are currently commented out because there is so little coverage that there aren't any good thresholds to set that won't fail the build.

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,19 +1,39 @@
 module.exports = {
-  // verbose: true,
+  verbose: true,
   coverageDirectory: "./coverage/",
   collectCoverage: true,
+  collectCoverageFrom: [
+    "**/*.{js,jsx}"
+  ],
   coverageReporters: [
-    "text",
     "text-summary",
     "html",
     "lcov"
   ],
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "/test/",
+  ],
+  coverageThreshold: {
+    // "global": {
+    //   "branches": 50,
+    //   "functions": 50,
+    //   "lines": 50,
+    //   "statements": 50
+    // },
+    // "./src/reducers/**/*.js": {
+    //   "statements": 90
+    // },
+    // "./src/utils/**/*.js": {
+    //   "branches": 70,
+    //   "functions": 80,
+    //   "lines": 70,
+    //   "statements": 70
+    // },
+  },
+  testMatch: [ "**/test/**/*.test.js?(x)"],
   reporters: [
     "default",
-    ["./node_modules/jest-html-reporter", {
-        "pageTitle": "Test Report",
-        "outputPath": "./coverage/junit/index.html"
-    }],
     [ "jest-junit", { outputDirectory: "./coverage/junit" } ]
   ]
 };
