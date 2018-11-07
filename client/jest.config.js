45e6384ed980801b7695cd8cb2b04ca8785ff00f
forgo <elliott.richerson@gmail.com>
2018-11-06 18:46:48 -0700

    fix minor annoyance using quoted key in jest config

--- client/jest.config.js
+++ client/jest.config.js
@@ -36,7 +36,7 @@ module.exports = {
     "default",
     [ "jest-junit", { outputDirectory: "./coverage/junit" } ]
   ],
-  "moduleNameMapper": {
+  moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/fileMock.js",
     "\\.(css|less)$": "<rootDir>/styleMock.js"
   },
