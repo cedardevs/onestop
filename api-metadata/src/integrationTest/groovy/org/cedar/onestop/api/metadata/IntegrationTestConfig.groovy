3012d2e444be9f54e2137c04e675dc67815d01e5
forgo <elliott.richerson@gmail.com>
2018-11-06 19:12:09 -0700

    adding some additional time before testcontainer timeout and looking for success code 200 (not just http response)

--- api-metadata/src/integrationTest/groovy/org/cedar/onestop/api/metadata/IntegrationTestConfig.groovy
+++ api-metadata/src/integrationTest/groovy/org/cedar/onestop/api/metadata/IntegrationTestConfig.groovy
@@ -11,9 +11,15 @@ import org.springframework.context.annotation.Profile
 import org.testcontainers.containers.GenericContainer
 import org.testcontainers.containers.wait.strategy.Wait
 
+import java.time.Duration
+
 @Profile("integration")
 @TestConfiguration
 class IntegrationTestConfig {
+
+  // the default startup timeout is 60s, but we tend to run into issues when resources are dragging
+  // upping this to 120s as the first safeguard against failed integration tests
+  static final long SECONDS_TO_WAIT_FOR_CONTAINER_TO_STARTUP = 120
 
   @Value('${elasticsearch.version}')
   String esVersion
@@ -22,8 +28,10 @@ class IntegrationTestConfig {
   GenericContainer esContainer() {
     String dockerImageName = "docker.elastic.co/elasticsearch/elasticsearch:${esVersion}"
     GenericContainer esContainer = new GenericContainer(dockerImageName)
+        .withStartupTimeout(Duration.ofSeconds(SECONDS_TO_WAIT_FOR_CONTAINER_TO_STARTUP))
         .withExposedPorts(9200)
-        .waitingFor(Wait.forHttp('/'))
+        // adding success status code to the root elasticsearch endpoint wait conditions
+        .waitingFor(Wait.forHttp('/').forStatusCode(200))
         .withEnv("xpack.security.enabled", "false")
         .withEnv("transport.host", "127.0.0.1")
         .withEnv("http.host", "0.0.0.0")
