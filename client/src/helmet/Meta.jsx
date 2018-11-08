41f679c3a0f44de7c1183e1052a854ad9a506eec
forgo <elliott.richerson@gmail.com>
2018-11-08 13:55:10 -0700

    fix nohashes issues in production

--- client/src/helmet/Meta.jsx
+++ client/src/helmet/Meta.jsx
@@ -56,7 +56,7 @@ export default class Meta extends Component {
 
     return (
       <Helmet>
-        <link href="/noaa-favicon.ico" rel="shortcut icon" />
+        <link href="/onestop/static/noaa-favicon.ico" rel="shortcut icon" />
 
         <meta property="robots" content={robotsValue} />
 
