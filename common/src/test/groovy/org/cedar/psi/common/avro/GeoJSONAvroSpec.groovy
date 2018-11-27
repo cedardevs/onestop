05ea38e1175352c49f1c7b6d76601b265ca441b9
arianna <arianna.jakositz@colorado.edu>
2018-11-27 16:01:06 -0700

    so many tests!

--- common/src/test/groovy/org/cedar/psi/common/avro/GeoJSONAvroSpec.groovy
+++ common/src/test/groovy/org/cedar/psi/common/avro/GeoJSONAvroSpec.groovy
@@ -2,6 +2,9 @@ package org.cedar.psi.common.avro
 
 import org.cedar.psi.common.util.MockSchemaRegistrySerde
 import org.apache.kafka.common.errors.SerializationException
+import org.everit.json.schema.loader.SchemaLoader
+import org.json.JSONObject
+import org.json.JSONTokener
 import spock.lang.Specification
 import spock.lang.Unroll
 
@@ -9,8 +12,12 @@ import spock.lang.Unroll
 class GeoJSONAvroSpec extends Specification {
 
   static final pointCoords = [30D, 10D]
-  static final multiPointCoords = [[10D, 40D], [40D, 30D], [20D, 20D], [30D, 10D]]
-  static final lineStringCoords = [[30D, 10D], [10D, 30D], [40D, 40D]]
+  static final multiPointCoords = [
+      [10D, 40D], [40D, 30D], [20D, 20D], [30D, 10D]
+  ]
+  static final lineStringCoords = [
+      [30D, 10D], [10D, 30D], [40D, 40D]
+  ]
   static final multiLineStringCoords = [
       [[10D, 10D], [20D, 20D], [10D, 40D]],
       [[40D, 40D], [30D, 30D], [40D, 20D], [30D, 10D]]
@@ -78,15 +85,51 @@ class GeoJSONAvroSpec extends Specification {
 
     where:
     // Coordinates formats same for LineString & MultiPoint and Polygon & MultiLineString types
-    testType        | testBuilder        | testCoordinates
-    PointType.Point | Point.newBuilder() | multiPointCoords
-    PointType.Point | Point.newBuilder() | polygonWithHoleCoords
-    PointType.Point | Point.newBuilder() | multiPolygonCoords
-    // TODO finish type combos
+    testType                            | testBuilder                  | testCoordinates
+    PointType.Point                     | Point.newBuilder()           | multiPointCoords
+    PointType.Point                     | Point.newBuilder()           | polygonWithHoleCoords
+    PointType.Point                     | Point.newBuilder()           | multiPolygonCoords
+    MultiPointType.MultiPoint           | MultiPoint.newBuilder()      | multiLineStringCoords
+    MultiPointType.MultiPoint           | MultiPoint.newBuilder()      | multiPolygonCoords
+    LineStringType.LineString           | LineString.newBuilder()      | pointCoords
+    LineStringType.LineString           | LineString.newBuilder()      | polygonCoords
+    MultiLineStringType.MultiLineString | MultiLineString.newBuilder() | pointCoords
+    MultiLineStringType.MultiLineString | MultiLineString.newBuilder() | lineStringCoords
+    PolygonType.Polygon                 | Polygon.newBuilder()         | multiPointCoords
+    PolygonType.Polygon                 | Polygon.newBuilder()         | multiPolygonWithHolesCoords
+    MultiPolygonType.MultiPolygon       | MultiPolygon.newBuilder()    | pointCoords
+    MultiPolygonType.MultiPolygon       | MultiPolygon.newBuilder()    | multiPointCoords
+    MultiPolygonType.MultiPolygon       | MultiPolygon.newBuilder()    | polygonWithHoleCoords
   }
 
-  def "#testType represents valid correlated GeoJSON"() {
-    // TODO
+  def "#testType represents valid corresponding GeoJSON"() {
+    when:
+    // Setup schema for validation
+    InputStream input = ClassLoader.systemClassLoader.getResourceAsStream('jsonSchema/geometry-schema.json')
+    def rawSchema = new JSONObject(new JSONTokener(input))
+    def geoSchema = SchemaLoader.load(rawSchema)
+
+    // Build geometry avro object
+    def builder = testBuilder
+    builder.type = testType
+    builder.coordinates = testCoordinates
+    def geometry = builder.build()
+
+    geoSchema.validate(new JSONObject(geometry.toString()))
+
+    then:
+    noExceptionThrown()
+
+    where:
+    testType                            | testBuilder                  | testCoordinates
+    PointType.Point                     | Point.newBuilder()           | pointCoords
+    MultiPointType.MultiPoint           | MultiPoint.newBuilder()      | multiPointCoords
+    LineStringType.LineString           | LineString.newBuilder()      | lineStringCoords
+    MultiLineStringType.MultiLineString | MultiLineString.newBuilder() | multiLineStringCoords
+    PolygonType.Polygon                 | Polygon.newBuilder()         | polygonCoords
+    PolygonType.Polygon                 | Polygon.newBuilder()         | polygonWithHoleCoords
+    MultiPolygonType.MultiPolygon       | MultiPolygon.newBuilder()    | multiPolygonCoords
+    MultiPolygonType.MultiPolygon       | MultiPolygon.newBuilder()    | multiPolygonWithHolesCoords
   }
 
 }
