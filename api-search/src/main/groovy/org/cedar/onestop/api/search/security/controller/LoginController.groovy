d0dd23d37c55ada37006ae07a340354d06af6f02
forgo <elliott.richerson@gmail.com>
2018-11-29 16:01:05 -0700

    clean up comments

--- api-search/src/main/groovy/org/cedar/onestop/api/search/security/controller/LoginController.groovy
+++ api-search/src/main/groovy/org/cedar/onestop/api/search/security/controller/LoginController.groovy
@@ -11,7 +11,6 @@ import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken
 import org.springframework.stereotype.Controller
 import org.springframework.util.StringUtils
-import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.ResponseBody
@@ -19,13 +18,6 @@ import org.springframework.web.client.RestTemplate
 
 import javax.servlet.http.HttpServletResponse
 
-//@CrossOrigin(
-//    origins = '*',
-//    maxAge = 3600L,
-//    allowedHeaders = ["x-auth-token","Authorization","Access-Control-Allow-Origin","Access-Control-Allow-Credentials"],
-//    exposedHeaders = ["x-auth-token","Authorization","Access-Control-Allow-Origin","Access-Control-Allow-Credentials"],
-//    allowCredentials = "true"
-//)
 @Controller
 class LoginController {
 
