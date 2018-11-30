636a5422b254c2caceaf4b80df8df23d50a554ac
forgo <elliott.richerson@gmail.com>
2018-11-29 17:58:45 -0700

    add exposed headers and 'x-auth-token' header, specifically to allowed and exposed

--- api-search/src/main/groovy/org/cedar/onestop/api/search/security/config/SecurityConfig.groovy
+++ api-search/src/main/groovy/org/cedar/onestop/api/search/security/config/SecurityConfig.groovy
@@ -50,9 +50,6 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
         allowedOrigin = loginGovConfiguration.allowedOrigin
         loginSuccessRedirect = loginGovConfiguration.loginSuccessRedirect
         logoutSuccessRedirect = loginGovConfiguration.logoutSuccessRedirect
-
-
-        System.out.println("okay")
     }
 
     @Autowired
@@ -108,11 +105,14 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
             // fix OPTIONS preflight login profile request failure with 403 Invalid CORS request
             CorsConfiguration config = new CorsConfiguration()
             config.addAllowedOrigin(allowedOrigin)
-            config.setAllowedHeaders(ImmutableList.of("Authorization", "Cache-Control", "Content-Type"))
+            config.setAllowedHeaders(ImmutableList.of("x-auth-token", "Authorization", "Cache-Control", "Content-Type"))
+            config.setExposedHeaders(ImmutableList.of("x-auth-token", "Authorization", "Cache-Control", "Content-Type"))
             config.addAllowedMethod(HttpMethod.OPTIONS)
             config.addAllowedMethod(HttpMethod.GET)
+
             UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource()
             source.registerCorsConfiguration(LOGIN_PROFILE_ENDPOINT, config)
+
             return new CorsFilter(source)
         }
         else {
