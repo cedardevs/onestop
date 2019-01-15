a4c12e48dbe5d85fd23c6c2f28f10ba69c785706
Zeb <elizabeth.delk@colorado.edu>
2019-01-15 10:17:25 -0700

    Switch client prefix path to depend on ENV instead of arg to build image. Still trying to fix proxying...

--- client/docker/entrypoint.sh
+++ client/docker/entrypoint.sh
@@ -1,4 +1,4 @@
 
-sed -i -e "s/ONESTOP_PREFIX_PATH/${1}/g" /usr/local/apache2/conf/httpd.conf
-
+sed -i -e "s/ONESTOP_PREFIX_PATH/${ONESTOP_PREFIX_PATH}/g" /usr/local/apache2/conf/httpd.conf
+mv /usr/app/client /usr/local/apache2/htdocs/${ONESTOP_PREFIX_PATH}
 httpd-foreground
