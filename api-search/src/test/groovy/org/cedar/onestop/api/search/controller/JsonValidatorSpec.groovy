4d49a40fc20d462dad2a96999d9515b18a79842b
Zeb <elizabeth.delk@colorado.edu>
2019-02-12 13:07:43 -0700

    Add test labels to JsonValidatorSpec

--- api-search/src/test/groovy/org/cedar/onestop/api/search/controller/JsonValidatorSpec.groovy
+++ api-search/src/test/groovy/org/cedar/onestop/api/search/controller/JsonValidatorSpec.groovy
@@ -28,7 +28,7 @@ class JsonValidatorSpec extends Specification {
     globalReport.success
   }
 
-  def 'valid requests return success true and no errors'() {
+  def 'valid requests return success true and no errors (#desc)'() {
     when: 'valid json is validated'
     def jsonSlurper = new JsonSlurper()
     def params = jsonSlurper.parseText(request)
@@ -49,30 +49,30 @@ class JsonValidatorSpec extends Specification {
     !validation.errors
 
     where:
-    request << [
+    request | desc
         """\
 {
-}""",
+}""" | 'blank'
         """\
 {
   "queries": [
     {"type": "queryText", "value": "temperature"}
   ]
-}""",
+}""" | 'query for temperature'
         """\
 {
   "queries": [
     {"type": "queryText", "value": "temperature"}
   ],
   "facets": true
-}""",
+}"""| 'query for temperature with facets'
         """\
 {
   "queries": [
     {"type": "queryText", "value": "temperature"}
   ],
   "facets": false
-}""",
+}""" | 'query for temperature without facets'
 
         """\
 {
@@ -80,7 +80,7 @@ class JsonValidatorSpec extends Specification {
     {"type": "queryText", "value": "temperature"}
   ],
   "summary": false
-}""",
+}""" | 'query for temperature without summary'
 
 
         """\
@@ -89,44 +89,44 @@ class JsonValidatorSpec extends Specification {
     {"type": "queryText", "value": "temperature"}
   ],
   "summary": true
-}""",
+}""" | 'query for temperature as summary'
         """\
 {
   "queries": [
     {"type": "queryText", "value": "temperature"},
     {"type": "queryText", "value": "pressure"}
   ]
-}""",
+}""" | 'query for temperature and pressure'
         """\
 {
   "filters": [
     {"type": "datetime", "before": "2016-06-15T20:20:58Z", "after": "2015-09-22T10:30:06.000Z"}
   ]
-}""",
+}""" | 'filter with datetime range'
         """\
 {
   "filters": [
     {"type": "datetime", "relation": "within", "before": "2016-06-15T20:20:58Z"}
   ]
-}""",
+}""" | 'filter for datetime within range with unbounded end'
         """\
 {
   "filters": [
     {"type": "year", "relation": "contains", "after": -5000000}
   ]
-}""",
+}""" | 'filter for year containing year after -5000000'
         """\
 {
   "filters": [
     {"type": "facet", "name": "science", "values": ["Atmosphere"]}
   ]
-}""",
+}""" | 'filter Atomosphere facet'
         """\
 {
   "filters": [
     {"type": "geometry", "relation": "contains", "geometry": {"type": "Point", "coordinates": [22.123, -45.245]}}
   ]
-}""",
+}""" | 'filter containing point geometry'
         """\
 {
   "filters": [
@@ -134,7 +134,7 @@ class JsonValidatorSpec extends Specification {
       {"type": "Polygon", "coordinates": [[[-5.99, 45.99], [-5.99, 36.49], [36.49, 30.01], [36.49, 45.99], [-5.99, 45.99]]]}
     }
   ]
-}""",
+}""" | 'filter intersecting polygon geometry'
         """\
 {
   "filters": [
@@ -145,15 +145,15 @@ class JsonValidatorSpec extends Specification {
       {"type": "Polygon", "coordinates": [[[-5.99, 45.99], [-5.99, 36.49], [36.49, 30.01], [36.49, 45.99], [-5.99, 45.99]]]}
     }
   ]
-}""",
+}""" | 'multiple filter types'
         """\
 {
   "page": { "max": 10, "offset": 10 }
-}""",
+}""" | 'paging'
         """\
 {
   "page": { "max": 25, "offset": 10 }
-}""",
+}""" | 'paging with max 25'
         """\
 {
   "queries": [
@@ -165,27 +165,26 @@ class JsonValidatorSpec extends Specification {
     {"type": "geometry", "relation": "contains", "geometry": {"type": "Point", "coordinates": [22.123, -45.245]}}
   ],
   "page": { "max": 100, "offset": 0 }
-}""",
+}""" | 'request with query, filters, and paging'
         """\
 {
   "filters": [
     { "type": "excludeGlobal", "value": true}
   ]
-}""",
+}""" | 'exclude global filter'
         """\
 {
   "filters": [
     {"type":"collection", "values":["fakeUUID"]}
   ],
   "summary": false
-}""",
+}""" | 'filter by collection uuid'
 """\
 {"queries":[], "filters":[{"type":"datetime", "after":"0000-01-01T00:00:00Z", "before":"2018-08-31T00:00:00Z"}], "facets":true, "page":{"max":1000, "offset":0}}
-}"""
-    ]
+}""" | 'empty query with filters, facets, paging'
   }
 
-  def 'invalid requests throw an exception to be caught by the default exception handler'() {
+  def 'invalid requests throw an exception to be caught by the default exception handler (#desc)'() {
     when: "invalid json is validated"
     def jsonSlurper = new JsonSlurper()
     def params = jsonSlurper.parseText(request)
@@ -196,116 +195,115 @@ class JsonValidatorSpec extends Specification {
     e.message.contains('not a valid request')
 
     where:
-    request << [
+    request | desc
         """\
 {
   "queries": {
     "queryText": {"value": "temperature"}
   }
 }
-""",
+""" | 'badly nested querytext'
         """\
 {
   "queries": [
     {"type": "queryText", "value": "temperature", "cat": "meow"}
   ]
-}""",
+}""" | 'invalid field \'cat\''
         """\
 {
   "queries": [
     {"type": "queryText", "value": "?temperature"}
   ]
-}""",
+}""" | 'query beginning with \'?\''
         """\
 {
   "queries": [
     {"type": "queryText", "value": "*water"}
   ]
-}""",
+}""" | 'query beginning with \'*\''
         """\
 {
   "queries": [
     {"type": "queryText", "value": " *anything"}
   ]
-}""",
+}""" | 'query beginning with \'*\', anything'
         """\
 {
   "queries": [
     {"type": "queryText", "value": "temperature"}
   ],
   "summary": "gibberish"
-}""",
+}""" | 'non-boolean value for summary'
         """\
 {
   "facets": "false"
-}""",
+}""" | 'string instead of boolean'
         """\
 {
   "filters": [
     { "type": "datetime", "before": "2012-12-31", "after": "2012-01-01"}
   ]
-}""",
+}""" | 'datetime without timestamps'
         """\
 {
   "filters": [
     {"type": "datetime", "relation": "outside", "before": "2016-06-15T20:20:58Z"}
   ]
-}""",
+}""" | 'datetime relation that does not exist'
         """\
 {
   "filters": [
     {"type": "year", "before": -200.5}
   ]
-}""",
+}""" | 'year type is long not double'
         """\
 {
   "filters": [
     {"type": "year", "after": "-100000"}
   ]
-}""",
+}""" | 'year type is long not string'
         """\
 {
   "filters": [
     { "type": "geopoint", "coordinates": {"lat": -100, "lon": -100}}
   ]
-}""",
+}""" | 'badly formatted geopoint'
         """\
 {
   "filters": [
     { "type": "geopoint", "coordinates": {"lat": 50, "lon": 200}}
   ]
-}""",
+}""" | 'another badly formatted geopoint'
         """\
 {
   "filters": [
     { "type": "geopoint", "lat": -45.123, "lon": 75.245}
   ]
-}""",
+}""" | 'a third badly formatted geopoint'
         """\
 {
   "filters": [
     { "type": "bbox", "topLeft": {"lat": 45.99, "lon": -5.99}, "bottomRight": {"lat": 30.01, "lon": 36.49}}
   ]
-}""",
+}""" | 'badly formatted bbox'
         """\
 {
   "filters": [
     { "type": "excludeGlobal", "value": "taco tuesday"}
   ]
-}""",
+}""" | 'non-boolean value for excludeGlobal'
         """\
 {
   "filters": [
     {"type": "facet", "name": "notScience", "values": ["Atmosphere"]}
   ]
-}""",
+}""" | 'facet notScience that does not exist'
 """\
 {"queries":[], "filters":[{"type":"datetime", "after":"0000-01-01T00:00:00Z", "before":"2018-08-31T00:00:00Z"}], "facets":true, "page":{"max":9999999, "offset":0}}
-}""",
+}""" | 'max page number far far far too big'
 """\
 {"queries":[], "filters":[{"type":"datetime", "after":"0000-01-01T00:00:00Z", "before":"2018-08-31T00:00:00Z"}], "facets":true, "page":{"max":1001, "offset":0}}
-}"""
-    ]
+}""" | 'max page number ever so slightly too big'
   }
 
 }
