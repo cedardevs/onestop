7c84267f6d307815877e8041582f7aa757593f34
arianna <arianna.jakositz@colorado.edu>
2019-02-20 16:44:29 -0700

    no json, no patch

--- registry/src/main/groovy/org/cedar/psi/registry/api/PublisherController.groovy
+++ registry/src/main/groovy/org/cedar/psi/registry/api/PublisherController.groovy
@@ -19,38 +19,54 @@ import static org.springframework.web.bind.annotation.RequestMethod.*
 @Slf4j
 @CompileStatic
 @RestController
-@RequestMapping(value = "/metadata", consumes = ["application/xml", "application/json"])
+@RequestMapping(value = "/metadata")
 class PublisherController {
 
   @Autowired
   Publisher publisher
 
-  @RequestMapping(value = "/{type}", method = [POST, PUT])
-  Map receiveContent(HttpServletRequest request, HttpServletResponse response, @RequestBody String data, @PathVariable String type) throws Exception {
-    receiveContent(request, response, data, type, Topics.DEFAULT_SOURCE, null)
-  }
-
-  @RequestMapping(value = "/{type}/{id}", method = [POST, PUT, PATCH])
-  Map receiveContent(HttpServletRequest request, HttpServletResponse response, @RequestBody(required = false) String data, @PathVariable String type, @PathVariable UUID id) throws Exception {
-    receiveContent(request, response, data, type, Topics.DEFAULT_SOURCE, id)
-  }
-
-  @RequestMapping(value = "/{type}/{id}", method = [DELETE], consumes = ['*'])
-  Map removeContent(HttpServletRequest request, HttpServletResponse response, @PathVariable String type, @PathVariable UUID id) throws Exception {
-    receiveContent(request, response, null, type, Topics.DEFAULT_SOURCE, id)
-  }
-
-  @RequestMapping(value = "/{type}/{source}/{id}", method = [DELETE], consumes = ['*'])
-  Map removeContent(HttpServletRequest request, HttpServletResponse response, @PathVariable String type, @PathVariable String source, @PathVariable UUID id) throws Exception {
-    receiveContent(request, response, null, type, source, id)
-  }
-
-  @RequestMapping(value = "/{type}/{source}/{id}", method = [POST, PUT, PATCH])
+  @RequestMapping(value = "/{type}/{source}/{id}", method = [POST, PUT], consumes = ["application/xml", "application/json"], produces = 'application/json')
   Map receiveContent(HttpServletRequest request, HttpServletResponse response, @RequestBody(required = false) String data, @PathVariable String type, @PathVariable String source, @PathVariable UUID id) throws Exception {
     RecordType recordType = type in RecordType.values()*.name() ? RecordType.valueOf(type) : null
     def result = publisher.publishMetadata(request, recordType, data, source, id as String)
     response.status = result.status as Integer
     return result.content as Map
   }
+
+  @RequestMapping(value = "/{type}", method = [POST, PUT], consumes = ["application/xml", "application/json"], produces = 'application/json')
+  Map receiveContent(HttpServletRequest request, HttpServletResponse response, @RequestBody String data, @PathVariable String type) throws Exception {
+    receiveContent(request, response, data, type, Topics.DEFAULT_SOURCE, null)
+  }
+
+  @RequestMapping(value = "/{type}/{id}", method = [POST, PUT], consumes = ["application/xml", "application/json"], produces = 'application/json')
+  Map receiveContent(HttpServletRequest request, HttpServletResponse response, @RequestBody(required = false) String data, @PathVariable String type, @PathVariable UUID id) throws Exception {
+    receiveContent(request, response, data, type, Topics.DEFAULT_SOURCE, id)
+  }
+
+  @RequestMapping(value = "/{type}/{id}", method = PATCH, consumes = "application/json", produces = 'application/json')
+  Map patchContent(HttpServletRequest request, HttpServletResponse response, @RequestBody(required = false) String data, @PathVariable String type, @PathVariable UUID id) throws Exception {
+    receiveContent(request, response, data, type, Topics.DEFAULT_SOURCE, id)
+  }
+
+  @RequestMapping(value = "/{type}/{source}/{id}", method = PATCH, consumes = "application/json", produces = 'application/json')
+  Map patchContent(HttpServletRequest request, HttpServletResponse response, @RequestBody(required = false) String data, @PathVariable String type, @PathVariable String source, @PathVariable UUID id) throws Exception {
+    receiveContent(request, response, data, type, source, id)
+  }
+
+  @RequestMapping(value = "/{type}/{id}", method = [DELETE], consumes = ['*'],produces = 'application/json')
+  Map removeContent(HttpServletRequest request, HttpServletResponse response, @PathVariable String type, @PathVariable UUID id) throws Exception {
+    receiveContent(request, response, null, type, Topics.DEFAULT_SOURCE, id)
+  }
+
+  @RequestMapping(value = "/{type}/{source}/{id}", method = [DELETE], consumes = ['*'],produces = 'application/json')
+  Map removeContent(HttpServletRequest request, HttpServletResponse response, @PathVariable String type, @PathVariable String source, @PathVariable UUID id) throws Exception {
+    receiveContent(request, response, null, type, source, id)
+  }
+
+//  @RequestMapping(value = "/{type}/{source}/{id}/resurrection", method = GET, produces = 'application/json')
+//  Map resurrectContent(@PathVariable String type, @PathVariable String source, @PathVariable String id,
+//                       HttpServletRequest request, HttpServletResponse response) {
+//
+//  }
 
 }
