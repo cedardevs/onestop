224a990a90100d4641f942b0d0569451210d1d58
Zeb <elizabeth.delk@colorado.edu>
2019-02-25 15:07:27 -0700

    Add command to do git commit and push, plus call doc generation from circle.

--- generate-docs/commit.sh
+++ generate-docs/commit.sh
@@ -6,3 +6,5 @@ mkdir -p $DIR
 cp -r build/docs/api/ $DIR
 cp -r build/docs/schema $DIR
 git add ../docs
+git commit -m "Updated generated docs with version '${VERSION}'."
+git push -q https://${GITHUB_TOKEN}@github.com/cedardevs/onestop.git gh-pages
