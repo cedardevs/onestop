9f2c2c619a0764e9665108d0d25a33a6106b32a7
Zeb <elizabeth.delk@colorado.edu>
2019-02-25 14:13:24 -0700

    Docgen commit script has optional verison.

--- docgen/commit.sh
+++ docgen/commit.sh
@@ -1,8 +1,17 @@
-git checkout gh-pages
-rm -rf ../docs/api
-mkdir -p ../docs/api
-cp -r build/docs/api/ ../docs/api/
-cp -r build/docs/schema ../docs/api/
-git add ../docs
+# git checkout gh-pages
+# rm -rf ../docs/api
+# mkdir -p ../docs/api
+# cp -r build/docs/api/ ../docs/api/
+# cp -r build/docs/schema ../docs/api/
+# git add ../docs
 # git commit -m "Updating generated api docs"
 # git push
+
+VERSION=$1
+DIR=../docs/api/$VERSION/
+git checkout gh-pages
+rm -rf $DIR
+mkdir -p $DIR
+cp -r build/docs/api/ $DIR
+cp -r build/docs/schema $DIR
+git add ../docs
