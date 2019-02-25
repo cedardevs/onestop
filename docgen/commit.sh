5b975d28513c10af473e9bf760b290c1718a4216
Zeb <elizabeth.delk@colorado.edu>
2019-02-25 14:02:09 -0700

    More testing commit script for docs.

--- docgen/commit.sh
+++ docgen/commit.sh
@@ -2,9 +2,9 @@ git checkout gh-pages
 rm -rf ../docs/api
 rm -rf ../docs/schema
 mkdir -p ../docs/api
-mkdir -p ../docs/schema
+mkdir -p ../docs/api/schema
 mv build/docs/api ../docs/
-mv build/docs/schema ../docs/
+mv build/docs/schema ../docs/api/
 git add ../docs
 # git commit -m "Updating generated api docs"
 # git push
