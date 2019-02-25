f269b0bea6110dd5d7f502acbb3c8d52bcc9d38c
Zeb <elizabeth.delk@colorado.edu>
2019-02-25 13:56:05 -0700

    Testing crude commit generated docs script.

--- docgen/commit.sh
+++ docgen/commit.sh
@@ -1,6 +1,8 @@
 git checkout gh-pages
 rm -rf ../docs/api
 rm -rf ../docs/schema
+mkdir -p ../docs/api
+mkdir -p ../docs/schema
 mv build/docs/api ../docs/
 mv build/docs/schema ../docs/
 git add ../docs
