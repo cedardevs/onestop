5042954e0d5344c01e3a2e50b159f1d292bbf070
Evan McQuinn <evan.mcquinn@colorado.edu>
2019-03-05 17:22:55 -0700

    Don't set http status based on error data for a record

--- registry/src/main/groovy/org/cedar/psi/registry/api/MetadataRestController.groovy
+++ registry/src/main/groovy/org/cedar/psi/registry/api/MetadataRestController.groovy
@@ -4,7 +4,6 @@ import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
 import org.cedar.psi.common.constants.Topics
 import org.cedar.psi.registry.service.MetadataStore
-import org.cedar.schemas.avro.psi.ErrorEvent
 import org.cedar.schemas.avro.psi.RecordType
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
@@ -103,7 +102,6 @@ class MetadataRestController {
 
     if (result) {
       if (result?.errors?.size() > 0) {
-        response.status = result.errors.collect({ ErrorEvent error -> error?.status ?: null }).max()
         return [
             links : links,
             errors: result.errors
