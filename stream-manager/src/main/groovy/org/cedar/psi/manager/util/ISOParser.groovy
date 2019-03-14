496336d2110fc170201fa35e69b8a922ef8bf18f
Zeb <elizabeth.delk@colorado.edu>
2019-03-14 15:19:41 -0600

    Bounding falsiness check bugfix.

--- stream-manager/src/main/groovy/org/cedar/psi/manager/util/ISOParser.groovy
+++ stream-manager/src/main/groovy/org/cedar/psi/manager/util/ISOParser.groovy
@@ -306,7 +306,8 @@ class ISOParser {
     def north = (bbox.northBoundLatitude == "null" || bbox.northBoundLatitude == "") ? null : bbox.northBoundLatitude.Decimal.toDouble()
     def south = (bbox.southBoundLatitude == "null" || bbox.southBoundLatitude == "") ? null : bbox.southBoundLatitude.Decimal.toDouble()
 
-    if (!west || !east || !north || !south) {
+    // explicitly check for null-iness, since 0.0 will evaluate to false
+    if (west == null || east == null || north == null || south == null) {
       return null
     }
 
