f3da8c8ad46efe00d2767c0108e720e61527da48
Evan McQuinn <evan.mcquinn@colorado.edu>
2019-05-08 13:53:56 -0600

    Simplify, remove dead code

--- stream-manager/src/main/java/org/cedar/psi/manager/util/RoutingUtils.java
+++ stream-manager/src/main/java/org/cedar/psi/manager/util/RoutingUtils.java
@@ -1,11 +1,8 @@
 package org.cedar.psi.manager.util;
 
-import org.apache.kafka.streams.kstream.Predicate;
 import org.cedar.schemas.avro.psi.Input;
 import org.cedar.schemas.avro.psi.RecordType;
 
-import java.util.Arrays;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -13,23 +10,11 @@ import java.util.Map;
 public class RoutingUtils {
 
   // for each input type, a list of input sources which should be routed to the extractors
-  public static final Map<RecordType, List<String>> extractableInputSources = new LinkedHashMap<>();
-
-  static {
-    extractableInputSources.put(RecordType.granule, Arrays.asList("common-ingest"));
-  }
+  public static final Map<RecordType, List<String>> extractableInputSources = Map.of(
+      RecordType.granule, List.of("common-ingest"));
 
   public static boolean requiresExtraction(String key, Input value) {
     return extractableInputSources.get(value.getType()).contains(value.getSource());
   };
-
-  static final Predicate not(Predicate orig) {
-    return new Predicate() {
-      @Override
-      public boolean test(Object key, Object value) {
-        return !orig.test(key, value);
-      }
-    };
-  }
 
 }
