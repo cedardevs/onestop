5a22f594f8ece80de4f3ae213cc2fd745e033b8f
Elliott <elliott.richerson@gmail.com>
2019-05-31 16:58:54 -0600

    create livenessProbe's for APIs to allow Tomcat enough time to deploy WARs and for APIs to see Elasticsearch version from running ES, exclude websocket-* jars from api-admin because they conflict with Tomcat server-provided websocket-* jars. Improve/fix how api-admin was conditionally using DB autoconfig with ICAM,

--- api-metadata/src/main/groovy/org.cedar.onestop.api.metadata/Application.groovy
+++ api-metadata/src/main/groovy/org.cedar.onestop.api.metadata/Application.groovy
@@ -1,18 +1,30 @@
 package org.cedar.onestop.api.metadata
 
 import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.Import
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @EnableAsync
 @EnableScheduling
-// note: no @SpringApplication annotation here
-// see "Reference:" link in `DatabaseConfig`
-@Import(DatasourceConfig.class)
+// Default to excluding auto data source configurations based on certain DB classpath dependencies.
+// These auto-configurations will be re-included via the following @Profile("icam") based class extensions:
+// - DataSourceAutoConfigurationConditional
+// - DataSourceTransactionManagerAutoConfigurationConditional
+// - HibernateJpaAutoConfigurationConditional
+//
+// This should prevent a gross looking, but non-fatal stacktrace when ICAM/in-memory DB is not enabled
+@SpringBootApplication(exclude = [
+    DataSourceAutoConfiguration.class ,
+    DataSourceTransactionManagerAutoConfiguration.class,
+    HibernateJpaAutoConfiguration.class
+])
 @ComponentScan(["org.cedar.onestop.api.metadata", "org.cedar.onestop.elastic.common"])
 class Application extends SpringBootServletInitializer {
 
