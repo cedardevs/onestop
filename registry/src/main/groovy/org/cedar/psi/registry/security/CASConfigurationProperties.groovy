7718bc1564db7c055464ed02feb376ed351c1271
Elliott <elliott.richerson@gmail.com>
2019-06-26 11:33:52 -0600

    CAS initial commit for registry lockdown

---
+++ registry/src/main/groovy/org/cedar/psi/registry/security/CASConfigurationProperties.groovy
@@ -0,0 +1,90 @@
+package org.cedar.psi.registry.security
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Profile
+import org.springframework.stereotype.Component
+
+@Profile('cas')
+@Component
+@ConfigurationProperties(prefix = 'cas')
+class CASConfigurationProperties {
+	// The callback URL of the service the user is authenticating to
+  // e.g. - "https://sciapps.colorado.edu/registry/login/cas"
+//  @NotNull
+  String service
+
+  // The CAS server URL prefix
+  // e.g. - "https://auth.ncdc.noaa.gov/cas"
+//  @NotNull
+  String serverUrlPrefix
+
+  // The enterprise-wide CAS login URL.
+  // e.g. - "https://auth.ncdc.noaa.gov/cas/login"
+//  @NotNull
+  String loginUrl
+
+  // After logout, a redirect will be performed to this URL
+  // e.g. - "https://auth.ncdc.noaa.gov/cas/logout"
+//  @NotNull
+  String logoutSuccessUrl
+
+  // A key is required so CasAuthenticationProvider can identify tokens it previously authenticated
+  // e.g. - "CAS_PROVIDER_INVENTORY_MANAGER_SCIAPPS"
+  //      - "CAS_PROVIDER_INVENTORY_MANAGER_LOCALHOST"
+//  @NotNull
+  String providerKey
+
+  // Force CAS to authenticate the user again (even if the user has previously authenticated).
+  // During ticket validation it will require the ticket was generated as a consequence of an explicit login.
+  // High-security application would probably set this to `true`. Defaults to `false`, providing automated SSO.
+  boolean sendRenew
+
+  // getters and setters
+  String getService() {
+    return service
+  }
+
+  void setService(String service) {
+    this.service = service
+  }
+
+  String getServerUrlPrefix() {
+    return serverUrlPrefix
+  }
+
+  void setServerUrlPrefix(String serverUrlPrefix) {
+    this.serverUrlPrefix = serverUrlPrefix
+  }
+
+  String getLoginUrl() {
+    return loginUrl
+  }
+
+  void setLoginUrl(String loginUrl) {
+    this.loginUrl = loginUrl
+  }
+
+  String getLogoutSuccessUrl() {
+    return logoutSuccessUrl
+  }
+
+  void setLogoutSuccessUrl(String logoutSuccessUrl) {
+    this.logoutSuccessUrl = logoutSuccessUrl
+  }
+
+  String getProviderKey() {
+    return providerKey
+  }
+
+  void setProviderKey(String providerKey) {
+    this.providerKey = providerKey
+  }
+
+  boolean isSendRenew() {
+    return sendRenew
+  }
+
+  void setSendRenew(boolean sendRenew) {
+    this.sendRenew = sendRenew
+  }
+}
