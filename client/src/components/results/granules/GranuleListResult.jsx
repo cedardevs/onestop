bc52d5ad0f1817d7ee788db2a359726d4b443670
StrayCat1 <christopher.esterlein@colorado.edu>
2019-07-17 10:07:14 -0600

    granule result headers down one level

--- client/src/components/results/granules/GranuleListResult.jsx
+++ client/src/components/results/granules/GranuleListResult.jsx
@@ -150,11 +150,11 @@ class ListResult extends React.Component {
   ) {
     return (
       <div key={'ListResult::timeAndSpace'}>
-        <h3 style={styleSectionHeader}>Time Period:</h3>
+        <h4 style={styleSectionHeader}>Time Period:</h4>
         <div style={styleSectionContent}>
           {util.buildTimePeriodString(beginDate, beginYear, endDate, endYear)}
         </div>
-        <h3 style={styleSectionHeader}>Bounding Coordinates:</h3>
+        <h4 style={styleSectionHeader}>Bounding Coordinates:</h4>
         <div style={styleSectionContent}>
           {util.buildCoordinatesString(spatialBounding)}
         </div>
@@ -251,7 +251,7 @@ class ListResult extends React.Component {
 
     return (
       <div key={'ListResult::accessLinks'}>
-        <h3 style={styleSectionHeader}>Data Access Links:</h3>
+        <h4 style={styleSectionHeader}>Data Access Links:</h4>
         <ul style={util.styleProtocolList}>{badgesElement}</ul>
       </div>
     )
@@ -317,7 +317,7 @@ class ListResult extends React.Component {
     }
 
     const title = (
-      <h2
+      <h3
         id={`ListResult::title::${itemId}`}
         key={`ListResult::title::${itemId}`}
         tabIndex={-1}
@@ -329,7 +329,7 @@ class ListResult extends React.Component {
         style={styleOverallHeadingApplied}
       >
         {item.title}
-      </h2>
+      </h3>
     )
 
     const granuleDownloadable = granuleDownloadableLinks([ item ]).length > 0
