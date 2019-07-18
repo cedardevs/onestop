78fede1ecb8f62bc212b95ac7d79e0ef1e1c5fe5
StrayCat1 <christopher.esterlein@colorado.edu>
2019-07-18 09:23:20 -0600

    access statement update

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -86,7 +86,7 @@ export default class About extends React.Component {
               amended in 1998.
             </p>
             <p>
-              We have completed a 3rd party review that confirm the site is Level AA
+              We have completed a 3rd party review that confirms the site is Level AA
               accessibility for all of our features. We will continue to make
               improvements across our entire site aiming for AAA compliance
               where possible.
