7e054273dfc23d797174fa36faf986d43739cdc2
StrayCat1 <christopher.esterlein@colorado.edu>
2019-10-07 12:37:13 -0600

    WIP - OS CLI

---
+++ cli/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/danielgtaylor/openapi-cli-generator/cli"
+	"crypto/tls"
+	gtls "gopkg.in/h2non/gentleman.v2/plugins/tls"
+)
+
+func main() {
+	cli.Init(&cli.Config{
+		AppName:   "onestop-cli",
+		EnvPrefix: "ONESTOP_CLI",
+		Version:   "1.0.0",
+	})
+
+	cli.Client.Use(gtls.Config(&tls.Config{InsecureSkipVerify: true}))
+
+	// TODO: Add register commands here.
+	openapiRegister(false)
+
+	cli.Root.Execute()
+}
