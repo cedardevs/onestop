5602187830467e4adc986c8393e0cc56317dbf7b
StrayCat1 <christopher.esterlein@colorado.edu>
2019-10-11 10:33:28 -0600

    cli add support for text query and scdr regex

--- cli/main.go
+++ cli/main.go
@@ -28,7 +28,6 @@ func parseDate(params *viper.Viper) []string {
 			fmt.Println("Date syntax not supported.")
 		}
 	}
-	fmt.Println(t)
 	beginDateTime := t.Format("2006-01-01T00:00:00Z")
 	endDateTime := t.AddDate(0,0,1).Format("2006-01-01T00:00:00Z")
 
@@ -49,6 +48,22 @@ func parseFileIdentifier(params *viper.Viper) []string {
 		return []string{}
 	}
 	return []string{"{\"type\":\"queryText\", \"value\":\"fileIdentifier:\\\"" + fileId + "\\\"\"}"}
+}
+
+func parseParentIdentifierRegex(params *viper.Viper) []string {
+	regex := params.GetString("re-file")
+	if len(regex) == 0 {
+		return []string{}
+	}
+	return []string{"{\"type\":\"queryText\", \"value\":\"parentIdentifier:" + regex + "\"}"}
+}
+
+func parseTextQuery(params *viper.Viper) []string {
+	query := params.GetString("q")
+	if len(query) == 0 {
+		return []string{}
+	}
+	return []string{"{\"type\":\"queryText\", \"value\":\"" + query + "\"}"}
 }
 
 func parsePolygon(params *viper.Viper) []string {
@@ -83,24 +98,28 @@ func main() {
 	cli.Client.Use(gtls.Config(&tls.Config{InsecureSkipVerify: true}))
 
     cli.AddFlag("scdr-files", "date", "", "DATE must be a string describing a date, e.g. 2010-01-30. Current year is assumed if the year part in DATE is ommited, e.g. 01-30. Any time information in DATE is disregarded. Files with data between DATE midnight and next day midnight are selected.", "")
-	// cli.AddFlag("scdr-files", "file", "", "description", "")
 	cli.AddFlag("scdr-files", "type", "", "Search only for files of the specified data collection using the collection's file identfier. Using this option is highly recommended for any kind of file searches. Collection identifiers are case sensitive.", "")
 	cli.AddFlag("scdr-files", "area", "", "Locate files which intersect with the specified polygon AREA. The polygon must be a regular one (closed, no self-intersection, no hole) with coordinates (longitude, latitude) separated by \",\" like POLYGON((30.31 60.2, 31.21 60.2, 31.21 60.76, 30.31 60.76, 30.31 60.2)) ", "")
-	//cli.AddFlag("scdr-files", "q", "", "Free text search on granules", "")
+	cli.AddFlag("scdr-files", "re-file", "", "Locate files whose names match the case-insensitive regular expression REGEX. Only one regular expression is allowed, not longer than 100 characters.", "")
+    cli.AddFlag("scdr-files", "q", "", "Search flattened granules with text query", "")
+
 	cli.RegisterBefore("scdr-files", func(cmd string, params *viper.Viper, req *gentleman.Request) {
-		fmt.Println("BEFORE")
-		// fmt.Println(req.Body)
 		filters := []string{}
 		queries := []string{}
+
 		dateTimeFilter := parseDate(params)
         filters = append(filters, dateTimeFilter...)
         geoSpatialFilter := parsePolygon(params)
         filters = append(filters, geoSpatialFilter...)
+
         parentIdentifierQuery := parseParentIdentifier(params)
         queries = append(queries, parentIdentifierQuery...)
         fileIdentifierQuery := parseFileIdentifier(params)
         queries = append(queries, fileIdentifierQuery...)
-
+        regexQuery := parseParentIdentifierRegex(params)
+        queries = append(queries, regexQuery...)
+        query := parseTextQuery(params)
+        queries = append(queries, query...)
 
 		req.AddHeader("content-type", "application/json")
 		req.BodyString("{\"filters\":[" + strings.Join(filters, ", ") + "], \"queries\":[" + strings.Join(queries, ", ") + "]}")
