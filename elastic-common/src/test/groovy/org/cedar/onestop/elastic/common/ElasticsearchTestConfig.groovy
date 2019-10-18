5197528a29565fa69a56979ac95d2bac3a4d38fe
Evan McQuinn <evan.mcquinn@colorado.edu>
2019-10-18 17:52:26 -0600

    use parameter autowiring to ensure that initMethod will have been called

--- elastic-common/src/test/groovy/org/cedar/onestop/elastic/common/ElasticsearchTestConfig.groovy
+++ elastic-common/src/test/groovy/org/cedar/onestop/elastic/common/ElasticsearchTestConfig.groovy
@@ -13,24 +13,23 @@ import org.springframework.context.annotation.Profile
 @TestConfiguration
 class ElasticsearchTestConfig {
 
-    @Value('${elasticsearch.version}')
-    String elasticsearchVersion
+  @Value('${elasticsearch.version}')
+  String elasticsearchVersion
 
-    // the `ci` (continuous integration) profile should not use a testcontainer beans to derive a RestClient,
-    // as currently our CircleCI workflow can manage its own Elasticsearch docker image;
-    // however, we still do need these testcontainer beans for running tests for local dev
-    @Profile("!ci")
-    @Bean(name = 'elasticsearchTestContainer', initMethod = 'start', destroyMethod = 'stop')
-    ElasticsearchTestContainer elasticsearchTestContainer() {
-        String dockerImageName = "docker.elastic.co/elasticsearch/elasticsearch:${elasticsearchVersion}"
-        return new ElasticsearchTestContainer(dockerImageName)
-    }
-    @Profile("!ci")
-    @Bean(name = 'restClient', destroyMethod = 'close')
-    @DependsOn('elasticsearchTestContainer')
-    RestClient elasticsearchRestClient() {
-        ElasticsearchTestContainer container = elasticsearchTestContainer()
-        RestClient restClient = container.restClient
-        return restClient
-    }
+  // the `ci` (continuous integration) profile should not use a testcontainer beans to derive a RestClient,
+  // as currently our CircleCI workflow can manage its own Elasticsearch docker image;
+  // however, we still do need these testcontainer beans for running tests for local dev
+  @Profile("!ci")
+  @Bean(name = 'elasticsearchTestContainer', initMethod = 'start', destroyMethod = 'stop')
+  ElasticsearchTestContainer elasticsearchTestContainer() {
+    String dockerImageName = "docker.elastic.co/elasticsearch/elasticsearch:${elasticsearchVersion}"
+    return new ElasticsearchTestContainer(dockerImageName)
+  }
+
+  @Profile("!ci")
+  @Bean(name = 'restClient', destroyMethod = 'close')
+  @DependsOn('elasticsearchTestContainer')
+  RestClient elasticsearchRestClient(ElasticsearchTestContainer elasticsearchTestContainer) {
+    return elasticsearchTestContainer.restClient
+  }
 }
