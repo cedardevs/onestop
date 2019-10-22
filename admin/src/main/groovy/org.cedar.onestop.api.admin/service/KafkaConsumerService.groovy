98d59525d98ba7c08f92f42c28aa19d22a7d5610
Evan McQuinn <evan.mcquinn@colorado.edu>
2019-10-22 13:20:14 -0600

    Improve clarity by grouping records with labels and adding logging

--- admin/src/main/groovy/org.cedar.onestop.api.admin/service/KafkaConsumerService.groovy
+++ admin/src/main/groovy/org.cedar.onestop.api.admin/service/KafkaConsumerService.groovy
@@ -2,6 +2,7 @@ package org.cedar.onestop.api.admin.service
 
 import groovy.util.logging.Slf4j
 import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.velocity.runtime.directive.Parse
 import org.cedar.schemas.analyze.Analyzers
 import org.cedar.schemas.avro.psi.ParsedRecord
 import org.cedar.schemas.parse.DefaultParser
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Profile
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Service
 
+import java.util.function.Function
 import java.util.stream.Collectors
 
 @Slf4j
@@ -20,32 +22,38 @@ class KafkaConsumerService {
 
   @Value('${kafka.topic.collections}')
   String parsedCollectionTopic
-  
+
   @Value('${kafka.topic.granules}')
   String parsedGranulesTopic
-  
+
   @Autowired
   private MetadataManagementService metadataManagementService
-  
+
   @KafkaListener(topics = ['${kafka.topic.collections}', '${kafka.topic.granules}'])
   void listen(List<ConsumerRecord<String, ParsedRecord>> records) {
     // Update collections & granules
     log.info("consuming message from kafka topic")
     try {
-      def partitionedRecords = records.stream()
+      // fills in missing fields in parsed records and classifies them as "tombstone" "valid" or "invalid"
+      def groupedRecords = records.stream()
           .filter({ it != null })
-          .map({ [id: it.key(), parsedRecord: fillInRecord(it.value())] })
-          .filter({it.parsedRecord == null || Indexer.validateMessage(it.id, it.parsedRecord)?.valid})
-          .collect(Collectors.partitioningBy({it.parsedRecord == null}))
+          .map(this.&consumerRecordToMap)
+          .collect(Collectors.groupingBy(this.&classifyResults))
 
-      def tombstones = partitionedRecords.getOrDefault(true, Collections.emptyList())
+      def tombstones = groupedRecords.getOrDefault("tombstone", Collections.emptyList())
       tombstones.forEach({
+        log.info("Handling tombstone message for record [${it.id}]")
         metadataManagementService.deleteMetadata(it.id as String, true, false)
       })
-      def validRecords = partitionedRecords.getOrDefault(false, Collections.emptyList())
+      def validRecords = groupedRecords.getOrDefault("valid", Collections.emptyList())
       if (validRecords.size() > 0) {
+        validRecords.forEach({ log.info("Indexing valid record [${it.id}]") })
         metadataManagementService.loadParsedRecords(validRecords)
       }
+      def invalidRecords = groupedRecords.getOrDefault("invalid", Collections.emptyList())
+      invalidRecords.forEach({
+        log.info("Ignoring record [${it.id}] as it is not valid for onestop indexing")
+      })
     }
     catch (Exception e) {
       log.error("Unexpected error", e)
@@ -65,6 +73,27 @@ class KafkaConsumerService {
       record = Analyzers.addAnalysis(record)
     }
     return record
+  }
+
+  /**
+   * Transforms a Kafka ConsumerRecord to a Map with keys "id" and "parsedRecord"
+   * Fills in the parsedRecord with {@link #fillInRecord}
+   * @param record The ConsumerRecord
+   * @return The Map
+   */
+  private static Map<String, Object> consumerRecordToMap(ConsumerRecord<String, ParsedRecord> record) {
+    return [id: record.key(), parsedRecord: fillInRecord(record.value())]
+  }
+
+  /**
+   * Classifies a map produced by {@link #consumerRecordToMap} as "tombstone", "valid", or "invalid"
+   * @param result The Map
+   * @return The classifier string
+   */
+  private static String classifyResults(Map result) {
+    return result.parsedRecord == null ? "tombstone" :
+        Indexer.validateMessage(result.id, result.parsedRecord)?.valid ? "valid" :
+            "invalid"
   }
 
 }
