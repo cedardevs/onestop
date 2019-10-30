b7e8a2cbe62e8e0af2b492e949fa72ff0f6cff64
Evan McQuinn <evan.mcquinn@colorado.edu>
2019-10-30 16:50:11 -0600

    Move logic to wait on streamsApp into the context config

--- registry/src/integrationTest/groovy/org/cedar/onestop/registry/IntegrationBeanConfig.groovy
+++ registry/src/integrationTest/groovy/org/cedar/onestop/registry/IntegrationBeanConfig.groovy
@@ -2,6 +2,7 @@ package org.cedar.onestop.registry
 
 import io.confluent.kafka.schemaregistry.RestApp
 import org.apache.kafka.clients.admin.AdminClient
+import org.apache.kafka.streams.KafkaStreams
 import org.cedar.onestop.registry.stream.TopicInitializer
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
@@ -21,5 +22,19 @@ class IntegrationBeanConfig {
   @Bean
   TopicInitializer topicInitializer(AdminClient adminClient) {
     new TopicInitializer(adminClient, 1, 1 as short)
+  }
+
+  @Bean
+  Boolean waitForStreamsToRun(KafkaStreams streamsApp) {
+    println "waiting for streams app to be running"
+    def tries = 0
+    while (streamsApp.state() != KafkaStreams.State.RUNNING) {
+      if (tries >= 5) {
+        throw new RuntimeException("Streams app still in bad state ${streamsApp.state()} after ${tries} seconds")
+      }
+      sleep(1000)
+      tries += 1
+    }
+    return true
   }
 }
