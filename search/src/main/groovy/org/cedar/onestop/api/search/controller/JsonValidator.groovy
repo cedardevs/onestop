7f7946e0b6bbb2b9686eb344c456af1133b2ee41
Zeb <elizabeth.delk@colorado.edu>
2019-11-15 12:59:32 -0700

    Use openapi spec directly for request body validation.

--- search/src/main/groovy/org/cedar/onestop/api/search/controller/JsonValidator.groovy
+++ search/src/main/groovy/org/cedar/onestop/api/search/controller/JsonValidator.groovy
@@ -4,6 +4,7 @@ import groovy.util.logging.Slf4j
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.github.fge.jsonschema.main.JsonSchemaFactory
 import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.node.ObjectNode
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
 import org.springframework.http.converter.HttpMessageNotReadableException
 
@@ -12,7 +13,7 @@ import org.springframework.http.converter.HttpMessageNotReadableException
 class JsonValidator {
 
   static Map validateSearchRequestSchema(Map request) {
-    validateSchema(request, 'onestop-request-schema.json')
+    validateRequestAgainstSpec(request, 'requestBody')
   }
 
   static Map validateSchema(Map params, String schemaName) {
@@ -36,11 +37,28 @@ class JsonValidator {
     ObjectMapper yamlMapper = new ObjectMapper(new YAMLFactory())
     JsonNode apiSpec = yamlMapper.readTree(this.classLoader.getResource('schema/openapi.yml').text)
     JsonNode schemaJson = apiSpec.get('components').get('schemas').get(schemaName)
+    ObjectNode schemaJsonObj = (ObjectNode)schemaJson
+
+    // dereference the requestBody schema, so it can be used correctly for validation
+    [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25].each{ // TODO should be while loop, but this does, hilariously, cover it
+      // TODO probably horribly inefficient - cache this ?
+    JsonNode ref = schemaJson.findValue('$ref')
+    if(ref != null) {
+      def paths = ref.textValue().replace('#/', '').split('/')
+      JsonNode deref = apiSpec
+      paths.each{
+        deref = deref.findValue(it)
+      }
+      ObjectNode parent = schemaJsonObj.findParent('$ref')
+      parent.remove('$ref')
+      parent.setAll(deref)
+      }
+    }
 
     final factory = JsonSchemaFactory.byDefault()
     // final schemaJson = mapper.readTree(this.classLoader.getResource(schemaName).text)
     // TODO get the schema from OpenAPI spec: final schema = factory.getJsonSchema(schemaJson, '/components/schemas/requestBody')
-    final schema = factory.getJsonSchema(schemaJson)
+    final schema = factory.getJsonSchema(schemaJsonObj)
     final requestJson = yamlMapper.valueToTree(params)
     final report = schema.validate(requestJson)
 
