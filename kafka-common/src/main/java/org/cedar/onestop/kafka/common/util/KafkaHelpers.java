d4af62e5315072ee2a881d032d43d6272817a87b
Evan McQuinn <evan.mcquinn@colorado.edu>
2019-11-25 13:25:19 -0700

    System.exit(1) when kill switch triggered

--- kafka-common/src/main/java/org/cedar/onestop/kafka/common/util/KafkaHelpers.java
+++ kafka-common/src/main/java/org/cedar/onestop/kafka/common/util/KafkaHelpers.java
@@ -2,6 +2,8 @@ package org.cedar.onestop.kafka.common.util;
 
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.Topology;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 import java.util.concurrent.CompletableFuture;
@@ -10,14 +12,17 @@ import static org.apache.kafka.streams.KafkaStreams.State.ERROR;
 import static org.apache.kafka.streams.KafkaStreams.State.NOT_RUNNING;
 
 public class KafkaHelpers {
+  private static final Logger log = LoggerFactory.getLogger(KafkaHelpers.class);
 
   public static KafkaStreams buildStreamsAppWithKillSwitch(Topology topology, Properties streamsConfig) {
     var killSwitch = new CompletableFuture<KafkaStreams.State>();
     killSwitch.thenAcceptAsync((state) -> {
-      throw new IllegalStateException("KafkaStreams app entered bad state: " + state);
+      log.error("kill switch triggered with state " + state + "exiting...");
+      System.exit(1);
     });
     KafkaStreams.StateListener killSwitchListener = (newState, oldState) -> {
       if (!killSwitch.isDone() && (newState == ERROR || newState == NOT_RUNNING)) {
+        log.error("app entered bad state " + newState + ", executing kill switch");
         killSwitch.complete(newState);
       }
     };
