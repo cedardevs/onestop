a1310604a4602dd10e9003b6aca104f963b032a3
Elliott <elliott.richerson@gmail.com>
2019-12-17 13:34:07 -0700

    finalize Modal for use with interactive Map inline with filters

--- client/src/components/common/ui/Modal.jsx
+++ client/src/components/common/ui/Modal.jsx
@@ -15,22 +15,14 @@ const styleRelative = zIndex => {
   }
 }
 
-const styleAbsolute = (left, top, width, visible) => {
+const styleAbsolute = (left, top, width) => {
   return {
     position: 'absolute',
     left: `${left}px`,
     top: `${top}px`,
-    width: visible ? `${width}px` : 0,
+    width: `${width}px`,
     height: `auto`,
-
-    visibility: visible ? 'visible' : 'hidden',
-    opacity: visible ? 1 : 0,
     transition: `opacity ${ANIMATION_DURATION}ms, width ${ANIMATION_DURATION}ms`,
-
-    // Animating width has visual side-effects with overflow and wrapping, so we must
-    // prevent these effects while the modal is animating
-    //overflow: animating ? 'hidden' : 'initial',
-    //whiteSpace: animating ? 'nowrap' : 'initial',
   }
 }
 
@@ -39,26 +31,14 @@ export function useModal(open){
   const relativeRef = useRef(null)
   const contentRef = useRef(null)
 
-  const [ targetInitialized, setTargetInitialized ] = useState(false)
-  const [ contentInitialized, setContentInitialized ] = useState(false)
-
-  const [ relativePosition, setRelativePosition ] = useState(null)
-
-  // tracks the '100%' width (in `px`) of the target container (desired visual location of modal)
-  // so that the absolutely positioned modal content knows the `px` width to render and appear as if it were actually there
-  const [ targetWidth, setTargetWidth ] = useState(null)
-
   // tracks the fully open height of the content, given it spans the same '100%' width of the target
   const [ contentHeight, setContentHeight ] = useState(null)
 
-  const [ targetOpen, setTargetOpen ] = useState(false)
-  const [ contentOpen, setContentOpen ] = useState(false)
+  const [ styleContent, setStyleContent ] = useState(() => {
+    styleAbsolute(0, 0, 0)
+  })
 
-  const [ styleContent, setStyleContent ] = useState(
-    styleAbsolute(0, 0, 0, false)
-  )
-
-  const updateRelativePosition = () => {
+  const calculateRelativePosition = () => {
     const targetElement = targetRef.current
     const relativeElement = relativeRef.current
 
@@ -77,10 +57,10 @@ export function useModal(open){
     const {left: xRelative, top: yRelative} = relativeRect
 
     // relative coordinates of target element
-    setRelativePosition({left: xTarget - xRelative, top: yTarget - yRelative})
+    return {left: xTarget - xRelative, top: yTarget - yRelative}
   }
 
-  const updateTargetWidth = () => {
+  const calculateTargetWidth = () => {
     const contentElement = contentRef.current
     const targetElement = targetRef.current
     // allow the target room for the content's margin, border width, and padding
@@ -108,10 +88,10 @@ export function useModal(open){
     if (targetElement) {
       targetRect = targetElement.getBoundingClientRect()
     }
-    setTargetWidth(targetRect.width - space)
+    return targetRect.width - space
   }
 
-  const updateContentHeight = () => {
+  const calculateContentHeight = () => {
     const contentElement = contentRef.current
     // allow the target room for the content's margin, border width, and padding
     // `.getBoundingClientRect()` values are relative to outer-most boundary
@@ -135,31 +115,43 @@ export function useModal(open){
       space = topSpace + bottomSpace
       contentRect = contentElement.getBoundingClientRect()
     }
-    console.log('contentRect ->', contentRect)
-    setContentHeight(contentRect.height - space)
+    return contentRect.height - space
   }
 
-  const updateStyleContent = visible => {
-    if (relativePosition && targetWidth) {
-      const {left, top} = relativePosition
-      setStyleContent(styleAbsolute(left, top, targetWidth, visible))
+  const updateStyleContent = () => {
+    if (contentRef.current && targetRef.current) {
+      const {left, top} = calculateRelativePosition()
+      const targetWidth = calculateTargetWidth()
+      const contentHeight = calculateContentHeight()
+      setContentHeight(contentHeight)
+      const newStyle = styleAbsolute(left, top, targetWidth)
+      setStyleContent(newStyle)
     }
   }
 
-  const [ ro ] = useState(
+  const [ roc ] = useState(() => {
+    return new ResizeObserver((entries, observer) => {
+      updateStyleContent()
+    })
+  })
+
+  const [ rot ] = useState(
     () =>
-      new ResizeObserver(([ entry ]) => {
-        updateRelativePosition()
-        updateTargetWidth()
-        updateContentHeight()
-        updateStyleContent(contentOpen)
+      new ResizeObserver((entries, observer) => {
+        updateStyleContent()
       })
   )
 
-  // observe resize changes to the modal region
+  // observe resize changes to the content
   useEffect(() => {
-    if (targetRef.current) ro.observe(targetRef.current)
-    return () => ro.disconnect()
+    if (contentRef.current) roc.observe(contentRef.current)
+    return () => roc.disconnect()
+  }, [])
+
+  // observe resize changes to the target
+  useEffect(() => {
+    if (targetRef.current) rot.observe(targetRef.current)
+    return () => rot.disconnect()
   }, [])
 
   return {
@@ -172,137 +164,21 @@ export function useModal(open){
     relativeRef,
     contentRef,
 
-    // has the target component been mounted?
-    targetInitialized,
-    setTargetInitialized,
-
-    // has the content component been mounted?
-    contentInitialized,
-    setContentInitialized,
-
-    // modal relative position
-    relativePosition,
-    setRelativePosition,
-    updateRelativePosition,
-
-    // modal target width
-    targetWidth,
-    setTargetWidth,
-    updateTargetWidth,
-
     // modal content height
     contentHeight,
     setContentHeight,
-    updateContentHeight,
 
     // modal style content
     styleContent,
     setStyleContent,
     updateStyleContent,
-
-    // ???
-    targetOpen,
-    setTargetOpen,
-
-    contentOpen,
-    setContentOpen,
   }
 }
 
-const ModalComponent = ({
-  targetRef,
-  targetInitialized,
-  setTargetInitialized,
-  setTargetWidth,
-  updateTargetWidth,
-  setTargetOpen,
-  contentHeight,
-  setContentOpen,
-}) => {
-  // since our modal context is borrowed between content and target,
-  // this mount/unmount operation allows us to track when both content and target are available
-  useLayoutEffect(() => {
-    console.log('Modal initialized')
-    setTargetInitialized(true)
-    return () => {
-      setTargetInitialized(false)
-    }
-  }, [])
-
-  // get the 100% width and corresponding height of the modal region intially (before render)
-  // and update the style for the content
-  useEffect(
-    () => {
-      // if the target has been mounted
-      if (targetInitialized) {
-        console.log('updating target width')
-        // then it's reliable to evaluate the '100%' target width
-        updateTargetWidth()
-      }
-      return () => {
-        setTargetWidth(null)
-      }
-    },
-    [ targetInitialized ]
-  )
-
-  const handleTransitionStart = event => {
-    const property = event.propertyName
-    if (property === 'height') {
-      const targetCSS = window.getComputedStyle(targetRef.current)
-      if (targetCSS.height === '0px') {
-        console.log('target starting to open')
-      }
-      else {
-        console.log('target starting to close')
-        setContentOpen(false)
-      }
-    }
-  }
-
-  const handleTransitionEnd = event => {
-    // this ensures the map tiles get loaded properly around the animation
-    const property = event.propertyName
-    if (property === 'height') {
-      const targetCSS = window.getComputedStyle(targetRef.current)
-      if (targetCSS.height === '0px') {
-        console.log('target finished closing')
-        setTargetOpen(false)
-      }
-      else {
-        console.log('target finished opening')
-        setTargetOpen(true)
-      }
-    }
-  }
-
-  useEffect(() => {
-    // on mount
-    if (targetRef.current) {
-      targetRef.current.addEventListener(
-        'transitionstart',
-        handleTransitionStart
-      )
-      targetRef.current.addEventListener('transitionend', handleTransitionEnd)
-    }
-    return () => {
-      if (targetRef.current) {
-        targetRef.current.removeEventListener(
-          'transitionend',
-          handleTransitionEnd
-        )
-        targetRef.current.removeEventListener(
-          'transitionstart',
-          handleTransitionStart
-        )
-      }
-    }
-  }, [])
-
+const ModalComponent = ({targetRef, open, contentHeight}) => {
   const styleTarget = {
     width: '100%',
-    height: contentHeight !== null && open ? `${contentHeight}px` : 0,
-    background: 'yellow',
+    height: open ? `${contentHeight}px` : 0,
     transition: `height ${ANIMATION_DURATION}ms`,
   }
   return <div style={styleTarget} ref={targetRef} />
@@ -321,64 +197,17 @@ const Modal = props => {
 export default Modal
 
 const ModalContentComponent = ({
+  open,
   zIndex,
   children,
   relativeRef,
   contentRef,
-  relativePosition,
-  targetWidth,
-  contentHeight,
-  setContentHeight,
-  updateContentHeight,
-  contentInitialized,
-  setContentInitialized,
   styleContent,
-  updateStyleContent,
-  contentOpen,
 }) => {
-  useLayoutEffect(() => {
-    // since our modal context is borrowed between content and target,
-    // this mount/unmount operation allows us to track when both content and target are available
-    console.log('ModalContent initialized')
-    setContentInitialized(true)
-    return () => {
-      setContentInitialized(false)
-    }
-  }, [])
-
-  useEffect(
-    () => {
-      // if the content & target have been mounted, and the target width has been evaluated
-      if (contentInitialized && targetWidth !== null) {
-        console.log('updating content height')
-        // then this `useEffect` allows to measure the effective content height at that width from a resize after render
-        updateContentHeight()
-      }
-      return () => {
-        setContentHeight(null)
-      }
-    },
-    [ targetWidth ]
-  )
-
-  useLayoutEffect(
-    () => {
-      console.log(
-        `updating style content because (relative position=${JSON.stringify(
-          relativePosition
-        )}, targetWidth=${targetWidth}, or contentHeight=${contentHeight} changed)`
-      )
-      updateContentHeight()
-      updateStyleContent(contentOpen)
-    },
-    [ relativePosition, targetWidth, contentHeight ]
-  )
-
-  //console.log("styleContent", styleContent)
   return (
     <div style={styleRelative(zIndex)} ref={relativeRef}>
-      <div style={styleContent} ref={contentRef}>
-        {children}
+      <div style={{...styleContent, opacity: open ? 1 : 0}} ref={contentRef}>
+        {open ? children : null}
       </div>
     </div>
   )
