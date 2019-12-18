985721f4d06d4a5d6ecc66d2c4da90f09007eee4
arianna <arianna.jakositz@colorado.edu>
2019-12-18 16:14:36 -0700

    step 1 in transition to java complete

---
+++ search/src/main/java/org/cedar/onestop/api/search/service/DocumentationService.java
@@ -0,0 +1,74 @@
+package org.cedar.onestop.api.search.service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DocumentationService {
+
+  public static final Map<String, String> facetNameMappings = new HashMap<>();
+  public static final Map<String, String> filterableFields = new HashMap<>();
+
+  static {
+    facetNameMappings.put("dataFormats"         , "dataFormat");
+    facetNameMappings.put("linkProtocols"       , "linkProtocol");
+    facetNameMappings.put("serviceLinkProtocols", "serviceLinkProtocol");
+    facetNameMappings.put("science"             , "gcmdScience");
+    facetNameMappings.put("services"            , "gcmdScienceServices");
+    facetNameMappings.put("locations"           , "gcmdLocations");
+    facetNameMappings.put("instruments"         , "gcmdInstruments");
+    facetNameMappings.put("platforms"           , "gcmdPlatforms");
+    facetNameMappings.put("projects"            , "gcmdProjects");
+    facetNameMappings.put("dataCenters"         , "gcmdDataCenters");
+    facetNameMappings.put("horizontalResolution", "gcmdHorizontalResolution");
+    facetNameMappings.put("verticalResolution"  , "gcmdVerticalResolution");
+    facetNameMappings.put("temporalResolution"  , "gcmdTemporalResolution");
+
+    filterableFields.put("beginDate"                , "dateTimeFilter");
+    filterableFields.put("endDate"                  , "dateTimeFilter");
+    filterableFields.put("beginYear"                , "yearFilter");
+    filterableFields.put("endYear"                  , "yearFilter");
+    filterableFields.put("spatialBounding"          , "geometryFilter");
+    filterableFields.put("isGlobal"                 , "excludeGlobalFilter");
+    filterableFields.put("internalParentIdentifier" , "collectionFilter");
+
+    facetNameMappings.forEach( (name, field) ->
+        filterableFields.put(field, "facetFilter with \"name\" value of " + name)
+    );
+  }
+
+  public static Map<String, Map> generateAttributesInfo(Map esMapping) {
+    return generateAttributesInfo(esMapping, null);
+  }
+
+  public static Map<String, Map> generateAttributesInfo(Map<String, Map> esMapping, String groupName) {
+    Map<String, Map> attributesInfo = new HashMap<>();
+    Map<String, Map> fields = esMapping.get("properties");
+    fields.forEach( (String name, Map details) -> {
+      String fieldName = groupName == null ? name : groupName + "." + name;
+      if(details.get("type") == "nested") {
+        attributesInfo.putAll(generateAttributesInfo(details, fieldName));
+      }
+      else {
+        Map<String, Object> info = new HashMap<>();
+        info.put("queryable", isQueryable(details));
+        info.put("exactMatchRequired", isExactMatchRequired(details));
+        info.put("applicableFilter", filterableFields.get(name) == null ? "None" : filterableFields.get(name));
+        attributesInfo.put(fieldName, info);
+      }
+    });
+
+    return attributesInfo;
+  }
+
+  private static boolean isQueryable(Map fieldMappingDetails) {
+    Boolean indexVal = (Boolean) fieldMappingDetails.get("index");
+    return indexVal == null || indexVal;
+  }
+
+  private static Boolean isExactMatchRequired(Map fieldMappingDetails) {
+    if(isQueryable(fieldMappingDetails)) {
+      return !fieldMappingDetails.get("type").equals("text");
+    }
+    return null;
+  }
+}
