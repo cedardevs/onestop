0765f5cff498a9f2cc3e37e1cbb3827942fdbfe2
Elliott <elliott.richerson@gmail.com>
2019-12-20 11:49:22 -0700

    remove unneeded showMap/open state from the granule/collection map containers, because the map no longer needs this information and doesn't handle animating itself

--- client/src/components/filters/collections/CollectionMapContainer.js
+++ client/src/components/filters/collections/CollectionMapContainer.js
@@ -14,7 +14,6 @@ const mapStateToProps = state => {
   return {
     filterType: 'collectionFilter',
     geoJsonSelection: geoJSON,
-    open: true, //state.layout.showMap,
   }
 }
 
--- client/src/components/filters/granules/GranuleMapContainer.js
+++ client/src/components/filters/granules/GranuleMapContainer.js
@@ -14,7 +14,6 @@ const mapStateToProps = state => {
   return {
     filterType: 'granuleFilter',
     geoJsonSelection: geoJSON,
-    showMap: state.layout.showMap,
   }
 }
 
