465f33ee21a188e9d494074a7084159c95c8c6b5
Zeb <elizabeth.delk@colorado.edu>
2020-01-03 11:12:36 -0700

    Refactor to make tests more readable.

--- client/test/components/filters/spatial/BoundingBoxEffect.test.js
+++ client/test/components/filters/spatial/BoundingBoxEffect.test.js
@@ -3,15 +3,24 @@ import {renderHook, act} from '@testing-library/react-hooks'
 
 import {useBoundingBox} from '../../../../src/components/filters/spatial/BoundingBoxEffect'
 
+const initHook = (hookFunction, ...hookArgs) => {
+  // generic init hook function
+  let hook = null
+  act(() => {
+    const {result} = renderHook(() => hookFunction(...hookArgs)) // TODO test ...hookArgs with hook that has more than one param
+    hook = result
+  })
+
+  return hook
+}
+
+const init = bbox => {
+  return initHook(useBoundingBox, bbox)
+}
+
 describe('The BoundingBoxEffect hook', () => {
   it('should set init value - no bbox', () => {
-    let hook = null
-    act(() => {
-      const {result} = renderHook(() => useBoundingBox(null))
-
-      hook = result
-    })
-    // let { hook } = wrapper.find('div').props();
+    const hook = init(null)
     let [ bounds ] = hook.current
     expect(bounds.east.value).toEqual('')
     expect(bounds.north.value).toEqual('')
@@ -19,23 +28,9 @@ describe('The BoundingBoxEffect hook', () => {
     expect(bounds.west.value).toEqual('')
   })
   it('should set init value - bbox', () => {
-    // let wrapper = null
-    // act(() => {
-    //   wrapper= mount(<HookWrapper hook={() => useBoundingBox({north: 34, south: -20, west: 17, east: -123})} />);
-    // })
-    let hook = null
-    act(() => {
-      const {result} = renderHook(() =>
-        useBoundingBox({north: 34, south: -20, west: 17, east: -123})
-      )
+    const hook = init({north: 34, south: -20, west: 17, east: -123})
 
-      hook = result
-    })
-
-    // let { hook } = wrapper.find('div').props();
-    console.log('????', hook)
     let [ bounds ] = hook.current
-    console.log('????', bounds)
     expect(bounds.east.value).toEqual('-123')
     expect(bounds.north.value).toEqual('34')
     expect(bounds.south.value).toEqual('-20')
@@ -43,18 +38,12 @@ describe('The BoundingBoxEffect hook', () => {
   })
 
   it('should update', () => {
-    let hook = null
-    act(() => {
-      const {result} = renderHook(() => useBoundingBox(null))
-
-      hook = result
-    })
+    const hook = init(null)
     act(() => {
       let [ bounds ] = hook.current
       bounds.west.set('111')
     })
 
-    // let { hook } = wrapper.find('div').props();
     let [ bounds ] = hook.current
     expect(bounds.west.value).toEqual('111')
     expect(bounds.west.number).toEqual(111)
@@ -62,18 +51,12 @@ describe('The BoundingBoxEffect hook', () => {
   })
 
   it('should update -  invalid', () => {
-    let hook = null
-    act(() => {
-      const {result} = renderHook(() => useBoundingBox(null))
-
-      hook = result
-    })
+    const hook = init(null)
     act(() => {
       let [ bounds ] = hook.current
       bounds.west.set('abc')
     })
 
-    // let { hook } = wrapper.find('div').props();
     let [ bounds ] = hook.current
     expect(bounds.west.value).toEqual('abc')
     expect(bounds.west.number).toBeNull()
