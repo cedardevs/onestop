39373117a679e0ba1861d53fd5553f638a5d21c8
Zeb <elizabeth.delk@colorado.edu>
2020-01-03 16:38:23 -0700

    Test individual field validation better.

--- client/test/components/filters/spatial/BoundingBoxEffect.test.js
+++ client/test/components/filters/spatial/BoundingBoxEffect.test.js
@@ -19,47 +19,190 @@ const init = bbox => {
 }
 
 describe('The BoundingBoxEffect hook', () => {
-  it('should set init value - no bbox', () => {
-    const hook = init(null)
-    let [ bounds ] = hook.current
-    expect(bounds.east.value).toEqual('')
-    expect(bounds.north.value).toEqual('')
-    expect(bounds.south.value).toEqual('')
-    expect(bounds.west.value).toEqual('')
-  })
-  it('should set init value - bbox', () => {
-    const hook = init({north: 34, south: -20, west: 17, east: -123})
+  describe('initial conditions', () => {
+    it('default - no bbox provided', () => {
+      const hook = init(null)
+      let [ bounds ] = hook.current
+      expect(bounds.east.value).toEqual('')
+      expect(bounds.north.value).toEqual('')
+      expect(bounds.south.value).toEqual('')
+      expect(bounds.west.value).toEqual('')
+    })
 
-    let [ bounds ] = hook.current
-    expect(bounds.east.value).toEqual('-123')
-    expect(bounds.north.value).toEqual('34')
-    expect(bounds.south.value).toEqual('-20')
-    expect(bounds.west.value).toEqual('17')
+    it('with a bbox', () => {
+      const hook = init({north: 34, south: -20, west: 17, east: -123})
+
+      let [ bounds ] = hook.current
+      expect(bounds.east.value).toEqual('-123')
+      expect(bounds.north.value).toEqual('34')
+      expect(bounds.south.value).toEqual('-20')
+      expect(bounds.west.value).toEqual('17')
+    })
   })
 
-  it('should update', () => {
+  it('simple interaction - should update value and basic side effects', () => {
     const hook = init(null)
+    // before interacting TODO why isn't this working?
+    // let [ bounds ] = hook.current
+    // expect(bounds.west.isSet()).toBeFalsy()
+
     act(() => {
       let [ bounds ] = hook.current
+      // this is currently how a component is expected to update values based on user interaction events:
       bounds.west.set('111')
     })
 
     let [ bounds ] = hook.current
+    // primary effect: value is set
     expect(bounds.west.value).toEqual('111')
+    // secondary effects:
     expect(bounds.west.number).toEqual(111)
-    expect(bounds.west.valid).toBeTruthy()
+    expect(bounds.west.isSet()).toBeTruthy()
   })
 
-  it('should update -  invalid', () => {
-    const hook = init(null)
-    act(() => {
-      let [ bounds ] = hook.current
-      bounds.west.set('abc')
+  describe('validation - field level', () => {
+    // let hook = null
+    // beforeAll(async () => {
+    //   // init hook with defaults
+    //   hook = init(null)
+    // })
+
+    const NAN_testCases = [
+      {
+        field: 'west',
+        value: 'foo',
+        error: 'West: Invalid coordinates entered.',
+      },
+      {
+        field: 'east',
+        value: '1-2',
+        error: 'East: Invalid coordinates entered.',
+      },
+      {
+        field: 'south',
+        value: 'hi',
+        error: 'South: Invalid coordinates entered.',
+      },
+      {
+        field: 'north',
+        value: '...',
+        error: 'North: Invalid coordinates entered.',
+      },
+    ]
+
+    const coordLimitTestCases = {
+      valid: [
+        {field: 'west', value: '-180'},
+        {field: 'west', value: '180'},
+        {field: 'west', value: '0'},
+        {field: 'east', value: '-180'},
+        {field: 'east', value: '180'},
+        {field: 'east', value: '0'},
+        {field: 'north', value: '-90'},
+        {field: 'north', value: '90'},
+        {field: 'north', value: '0'},
+        {field: 'south', value: '-90'},
+        {field: 'south', value: '90'},
+        {field: 'south', value: '0'},
+      ],
+      invalid: [
+        {
+          field: 'west',
+          value: '-181',
+          error:
+            'West: Invalid coordinates entered. Valid longitude are between -180 and 180.',
+        }, // TODO the phrasing 'valid longitude' should be 'valid latitude coordinates'
+        {
+          field: 'east',
+          value: '181',
+          error:
+            'East: Invalid coordinates entered. Valid longitude are between -180 and 180.',
+        },
+        {
+          field: 'north',
+          value: '91',
+          error:
+            'North: Invalid coordinates entered. Valid latitude are between -90 and 90.',
+        },
+        {
+          field: 'south',
+          value: '-91',
+          error:
+            'South: Invalid coordinates entered. Valid latitude are between -90 and 90.',
+        },
+      ],
+    }
+
+    _.each(NAN_testCases, c => {
+      it(`not a number - for ${c.field}='${c.value}'`, function(){
+        const hook = init(null)
+        act(() => {
+          let [ bounds ] = hook.current
+          bounds[c.field].set(c.value)
+        })
+
+        let [ bounds ] = hook.current
+
+        // generic invalid expectations
+        expect(bounds[c.field].valid).toBeFalsy()
+
+        // NaN expectations
+        expect(bounds[c.field].number).toBeNull()
+        expect(bounds[c.field].isSet()).toBeFalsy()
+
+        // specific invalid expectations
+        expect(bounds[c.field].value).toEqual(c.value)
+        expect(bounds[c.field].error).toEqual(c.error)
+      })
     })
 
-    let [ bounds ] = hook.current
-    expect(bounds.west.value).toEqual('abc')
-    expect(bounds.west.number).toBeNull()
-    expect(bounds.west.valid).toBeFalsy()
+    _.each(coordLimitTestCases.invalid, c => {
+      it(`invalid limits - for ${c.field}='${c.value}'`, function(){
+        const hook = init(null)
+
+        act(() => {
+          let [ bounds ] = hook.current
+          bounds[c.field].set(c.value)
+        })
+
+        let [ bounds ] = hook.current
+
+        // generic invalid expectations
+        expect(bounds[c.field].valid).toBeFalsy()
+
+        // coord limitations expectations
+        expect(Number.isFinite(bounds[c.field].number)).toBeTruthy()
+        expect(bounds[c.field].isSet()).toBeTruthy()
+
+        // specific invalid expectations
+        expect(bounds[c.field].value).toEqual(c.value)
+        expect(bounds[c.field].error).toEqual(c.error)
+      })
+    })
+
+    _.each(coordLimitTestCases.valid, c => {
+      it(`valid limits - for ${c.field}='${c.value}'`, function(){
+        const hook = init(null)
+
+        act(() => {
+          let [ bounds ] = hook.current
+          bounds[c.field].set(c.value)
+        })
+
+        let [ bounds ] = hook.current
+
+        console.log(`for ${c.field}='${c.value}'`, bounds[c.field])
+        // generic invalid expectations
+        expect(bounds[c.field].valid).toBeTruthy()
+
+        // coord limitations expectations
+        expect(Number.isFinite(bounds[c.field].number)).toBeTruthy()
+        expect(bounds[c.field].isSet()).toBeTruthy()
+
+        // specific invalid expectations
+        expect(bounds[c.field].value).toEqual(c.value)
+        expect(bounds[c.field].error).toEqual('')
+      })
+    })
   })
 })
