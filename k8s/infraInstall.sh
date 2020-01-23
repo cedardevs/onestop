e0927d05e34b13d37598b62b658baa2ecc6a4fc3
Elliott <elliott.richerson@gmail.com>
2020-01-23 11:59:24 -0700

    add bootstrap password for Elastic operator, and upgrade to latest ES 7.5.2

--- k8s/infraInstall.sh
+++ k8s/infraInstall.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 kubectl apply -f https://download.elastic.co/downloads/eck/1.0.0-beta1/all-in-one.yaml
+
+# workaround to customize the bootstrap password BEFORE creating the Elastic resources
+# as the operator currently doesn't support configuring the bootstrap password declaratively
+PASSWORD="foamcat"
+kubectl create secret generic onestop-es-elastic-user --from-literal=elastic="${PASSWORD}"
+
 kubectl apply -f ./elasticsearch.yaml
+
