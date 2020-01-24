61fdf6c9a2d1fde01acfe6f75c584dac1ef464c6
Evan McQuinn <evan.mcquinn@colorado.edu>
2020-01-24 16:59:42 -0700

    Upgrade to ECK 1.0.0

--- k8s/infraInstall.sh
+++ k8s/infraInstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-kubectl apply -f https://download.elastic.co/downloads/eck/1.0.0-beta1/all-in-one.yaml
+kubectl apply -f https://download.elastic.co/downloads/eck/1.0.0/all-in-one.yaml
 
 # workaround to customize the bootstrap password BEFORE creating the Elastic resources
 # as the operator currently doesn't support configuring the bootstrap password declaratively
