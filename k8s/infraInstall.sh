f270ac30aaa263b9ba54a5e53df113140962c2d2
Semere Ghebrechristos <ghebrecs@colorado.edu>
2020-01-24 19:38:46 -0700

    Increase default kafka producer message to 3mb and ingress body size and rename ECK yml config

--- k8s/infraInstall.sh
+++ k8s/infraInstall.sh
@@ -7,5 +7,5 @@ kubectl apply -f https://download.elastic.co/downloads/eck/1.0.0/all-in-one.yaml
 PASSWORD="foamcat"
 kubectl create secret generic onestop-es-elastic-user --from-literal=elastic="${PASSWORD}"
 
-kubectl apply -f ./elasticsearch.yaml
+kubectl apply -f ./k8s/es_kibana_apm.yaml
 
