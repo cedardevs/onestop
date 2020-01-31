2e8fbdd1dc3885f2cdfd733b3f9f8d7788e2df72
Elliott <elliott.richerson@gmail.com>
2020-01-31 13:57:30 -0700

    npm run format and updated package-lock.json for clean build

--- client/src/utils/polyfillDOMRect.js
+++ client/src/utils/polyfillDOMRect.js
@@ -6,32 +6,37 @@ For some reason, importing geometry-polyfill was giving us problems in IE11 due 
 {
   class DOMRect {
     constructor(x = 0, y = 0, width = 0, height = 0) {
-      this.x = x;
-      this.y = y;
-      this.width = width;
-      this.height = height;
+      this.x = x
+      this.y = y
+      this.width = width
+      this.height = height
     }
 
     static fromRect(otherRect) {
-      return new DOMRect(otherRect.x, otherRect.y, otherRect.width, otherRect.height);
+      return new DOMRect(
+        otherRect.x,
+        otherRect.y,
+        otherRect.width,
+        otherRect.height
+      )
     }
 
     get top() {
-      return this.y;
+      return this.y
     }
 
     get left() {
-      return this.x;
+      return this.x
     }
 
     get right() {
-      return this.x + this.width;
+      return this.x + this.width
     }
 
     get bottom() {
-      return this.y + this.height;
+      return this.y + this.height
     }
   }
 
-  window.DOMRect = window.DOMRect || DOMRect;
+  window.DOMRect = window.DOMRect || DOMRect
 }
