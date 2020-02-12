675281881d072d5f96909b3c0c7b3408992f9f82
Zeb <elizabeth.delk@colorado.edu>
2020-02-12 16:30:20 -0700

    Attempting more nuanced styling, but things are getting funky.

--- client/src/components/landing/FeaturedDatasets.jsx
+++ client/src/components/landing/FeaturedDatasets.jsx
@@ -8,7 +8,6 @@ import ResultGraphic from '../results/ResultGraphic'
 import {play_circle_o, pause_circle_o, SvgIcon} from '../common/SvgIcon'
 import {SiteColors, boxShadow} from '../../style/defaultStyles'
 import DetailGrid from '../collections/detail/DetailGrid'
-import {Visible} from 'react-grid-system'
 import {ScreenClassRender} from 'react-grid-system'
 
 const styleFeaturedDatasetsWrapper = {
@@ -27,16 +26,14 @@ const styleListItem = {
   display: 'flex',
 }
 
-const styleButtonWrapper = {
-  // width: '100%',
+const styleButtonWrapper = (isReversed, screenClass) => { return {
+  borderRadius: [ 'xs' ].includes(screenClass) ? '0.309em 0.309em 0 0': isReversed ? '0 0.309em 0.309em 0': '0.309em 0 0 0.309em',
   borderBottom: '1px solid black',
-
   boxShadow: boxShadow,
   background: '#F9F9F9',
-  // flex: 1,
   height: 'fit-content',
-  // justifyContent: 'flex-end',
-}
+      margin: 0,
+}}
 
 const styleFeaturedButton = {
   textDecoration: 'underline',
@@ -45,9 +42,7 @@ const styleFeaturedButton = {
   width: '100%',
   height: 'fit-content',
   background: 'transparent',
-  // color: 'inherit',
   color: SiteColors.LINK,
-  // textAlign: isReversed ? 'left' : 'right',
   padding: '.309em',
   display: 'block',
 }
@@ -62,67 +57,56 @@ const styleFeaturedButtonHover = {
   background: 'transparent',
 }
 
-const styleImageWrapper = (isReversed, screenClass) => {
+const styleImageWrapper = (isReversed, screenClass) => { // TODO need more styling for image wrapper borderRadius when there is NO description!!
   return {
+    // xs layout is vertically stacked, needs no left or right border
     borderLeft: ![ 'xs' ].includes(screenClass)
       ? isReversed ? '' : '1px solid black'
       : '',
     borderRight: ![ 'xs' ].includes(screenClass)
       ? isReversed ? '1px solid black' : ''
       : '',
+              borderRadius: [ 'xs' ].includes(screenClass) ? '' : [ 'sm', 'md' ].includes(screenClass) ? isReversed? '0.309em 0 0 0': '0 0.309em 0 0': isReversed ? '0 0 0.309em 0' : '0 0 0 0.309em',
     boxShadow: boxShadow,
     background: '#F9F9F9',
-    // flex: 3,
-    // display: 'flex',
-    // justifyContent: 'center',
-    // alignSelf: 'stretch',
     height: '100%',
-    cursor: 'pointer',
-    margin: 0,
+    // margin: 0,
+
   }
 }
 
-const styleDescriptionWrapper = {
-  // borderLeft: isReversed? '':'1px solid black',
-  // borderRight: isReversed? '1px solid black':'',
+  const styleDescriptionWrapper = (isReversed, screenClass) => {
+    return {
   boxShadow: boxShadow,
+    borderLeft: [ 'sm', 'md' ].includes(screenClass)
+      ? isReversed ? '' : '1px solid black'
+      : '',
+    borderRight: [ 'sm', 'md' ].includes(screenClass)
+      ? isReversed ? '1px solid black' : ''
+      : '',
+
+        borderRadius: [ 'xs', 'sm', 'md' ].includes(screenClass) ? '0 0 0.309em 0.309em': isReversed ? '0.309em 0 0 0.309em' : '0 0.309em 0.309em 0',
   background: '#F9F9F9',
-  // flex: 3,
   display: 'flex',
   justifyContent: 'center',
   alignSelf: 'stretch',
   height: '100%',
   margin: 0,
+  // overflowY: 'hidden',
+}
 }
 
-// const styleImageWrapper = {
-//
-//   padding: '0.618em 1.618em 1.618em 1.618em',
-//   margin: 0,
-//   fontSize: '1.1em',
-//     display: 'flex',
-//     justifyContent: 'center',
-//     alignSelf: 'stretch',
-// }
 const styleDescription = {
-  // width: '100%',
-  padding: '0.618em 1.618em 1.618em 1.618em',
+  padding: '1.618em',
   margin: 0,
   fontSize: '1.1em',
-  // display: 'flex',
-  // justifyContent: 'center',
-  // alignSelf: 'stretch',
 }
 
 const styleImage = {
-  // float: 'left',
-  // alignSelf: 'flex-start',
-  margin: '0 1.618em 1.618em 0',
-  // width: '32%',
-  // width: '60%',
+  // margin: '0 1.618em 1.618em 0',
   width: '100%',
-  // minWidth: '15em',
   objectFit: 'cover',
+    cursor: 'pointer',
   border: '1px solid black',
 }
 
@@ -132,29 +116,75 @@ const FeaturedDatasets = props => {
     submit(query)
   }
 
-  const renderCollectionImage = thumbnail => {
-    const imgUrl = processUrl(thumbnail)
-    return <img style={styleImage} src={imgUrl} alt="" aria-hidden="true" />
-  }
-
   if (props.featured !== null && props.featured.length > 0) {
     const featuredList = props.featured.map((f, i, arr) => {
       const backgroundURL = processUrl(f.imageUrl)
       const isReversed = i % 2 == 1
-      const collectionImage = renderCollectionImage(backgroundURL)
+      const collectionImage = <img onClick={() => {
+          search(f.searchTerm)
+        }} style={styleImage} src={backgroundURL} alt="" aria-hidden="true" />
 
-      // TODO pull out embedded styles!
-      /*
-      <FlexRow
-        style={{flexDirection: isReversed ? 'row-reverse' : 'row'}}
-        items={[
-
-        ]}
-      />
-      */
+      if(f.description == null || _.isEmpty(f.description)) {
+        return (
+          <ScreenClassRender
+            render={screenClass => {
+              // grid totals must add up to 12 for all the columns
+              const titleGrid = isReversed
+                ? {
+                    sm: 4,
+                    md: 3,
+                    lg: 2,
+                    push: {sm: 8, md: 9, lg: 10},
+                    style: {zIndex: [ 'xs' ].includes(screenClass) ? 1 : 5},
+                  }
+                : {sm: 4, md: 3, lg: 2}
+              const imageGrid = isReversed
+                ? {
+                    sm: 8,
+                    md: 9,
+                    lg: 10,
+                    pull: {sm: 4, md: 3, lg: 2},
+                    style: {zIndex: [ 'xs' ].includes(screenClass) ? 1 : 3},
+                    // push: {lg: 1},
+                  }
+                : {sm: 8, md: 9, lg: 10}
+              return (
+                <li style={styleListItem} key={i}>
+                  <DetailGrid
+                    grid={[
+                      [
+                        <h3 key="button" style={styleButtonWrapper(isReversed, screenClass)}>
+                          <Button
+                            role="link"
+                            text={f.title}
+                            title={`${f.title} Featured Data Search`}
+                            onClick={() => search(f.searchTerm)}
+                            style={styleFeaturedButton}
+                            styleHover={styleFeaturedButtonHover}
+                            styleFocus={styleFeaturedButtonFocus}
+                          />
+                      </h3>,
+                        <div
+                          key="img"
+                          style={styleImageWrapper(isReversed, screenClass)}
+                        >
+                        <p style={{padding: '1.618em',margin:0}}>
+                          {collectionImage}</p>
+                        </div>,
+                      ],
+                    ]}
+                    colWidths={[ titleGrid, imageGrid ]}
+                  />
+                </li>
+              )
+            }}
+          />
+        )
+      }
       return (
         <ScreenClassRender
           render={screenClass => {
+            // grid totals must add up to 12 for all the columns
             const titleGrid = isReversed
               ? {
                   sm: 4,
@@ -182,8 +212,9 @@ const FeaturedDatasets = props => {
                 <DetailGrid
                   grid={[
                     [
-                      <div key="button" style={styleButtonWrapper}>
+                      <h3 key="button" style={styleButtonWrapper(isReversed, screenClass)}>
                         <Button
+                          role="link"
                           text={f.title}
                           title={`${f.title} Featured Data Search`}
                           onClick={() => search(f.searchTerm)}
@@ -191,17 +222,15 @@ const FeaturedDatasets = props => {
                           styleHover={styleFeaturedButtonHover}
                           styleFocus={styleFeaturedButtonFocus}
                         />
-                      </div>,
+                    </h3>,
                       <div
                         key="img"
                         style={styleImageWrapper(isReversed, screenClass)}
-                        onClick={() => {
-                          search(f.searchTerm)
-                        }}
                       >
-                        <p style={styleDescription}>{collectionImage}</p>
+                      <p style={{padding: '1.618em',margin:0}}>
+                        {collectionImage}</p>
                       </div>,
-                      <div key="desc" style={styleDescriptionWrapper}>
+                      <div key="desc" style={styleDescriptionWrapper(isReversed, screenClass)}>
                         <p style={styleDescription}>{f.description}</p>
                       </div>,
                     ],
@@ -223,25 +252,6 @@ const FeaturedDatasets = props => {
         <h2 style={styleFeaturedDatasetsLabel} id="featuredDatasets">
           Featured Data Sets
         </h2>
-        <p>
-          <span>Your current screen class is </span>
-          <Visible xs>
-            <strong>xs</strong>
-          </Visible>
-          <Visible sm>
-            <strong>sm</strong>
-          </Visible>
-          <Visible md>
-            <strong>md</strong>
-          </Visible>
-          <Visible lg>
-            <strong>lg</strong>
-          </Visible>
-          <Visible xl>
-            <strong>xl</strong>
-          </Visible>
-          <span>.</span>
-        </p>
 
         <ul>
           <FlexColumn items={[ featuredList ]} />
