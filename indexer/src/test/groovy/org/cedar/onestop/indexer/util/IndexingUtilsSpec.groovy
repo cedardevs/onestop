853e39b0f0bb6bae31c1fd0337b78e69840dbf38
arianna <arianna.jakositz@colorado.edu>
2020-04-23 14:52:00 -0600

    indexing utils tests fixed

--- indexer/src/test/groovy/org/cedar/onestop/indexer/util/IndexingUtilsSpec.groovy
+++ indexer/src/test/groovy/org/cedar/onestop/indexer/util/IndexingUtilsSpec.groovy
@@ -1,96 +1,108 @@
 package org.cedar.onestop.indexer.util
 
 import org.apache.kafka.streams.state.ValueAndTimestamp
-import org.cedar.onestop.indexer.stream.BulkIndexingConfig
 import org.cedar.schemas.avro.psi.ParsedRecord
 import org.cedar.schemas.avro.psi.Publishing
-import org.cedar.schemas.avro.psi.RecordType
 import org.elasticsearch.action.delete.DeleteRequest
 import org.elasticsearch.action.index.IndexRequest
 import spock.lang.Specification
 import spock.lang.Unroll
 
-import java.time.Duration
 
 @Unroll
 class IndexingUtilsSpec extends Specification {
 
-  static testIndex = 'testindex'
-
-
   ////////////////////////////
   // Transform ES Requests  //
   ////////////////////////////
-  def "tombstones create delete requests"() {
+  def "tombstones create delete requests for collections"() {
     def testKey = "ABC"
 
     when:
-    def results = IndexingUtils.mapRecordToRequests(new IndexingInput(testKey, null, TestUtils.esConfig))
-
-    then:
-    results.size() == 1
-    results[0] instanceof DeleteRequest
-    results[0].index() == testIndex
-    results[0].id() == testKey
-  }
-
-  def "tombstones create multiple delete requests when configured"() {
-    def testKey = 'ABC'
-    def multipleDeleteConfig = BulkIndexingConfig.newBuilder()
-        .withStoreName('teststore')
-        .withMaxPublishInterval(Duration.ofSeconds(10))
-        .withMaxPublishBytes(10000)
-        .withMaxPublishActions(1000)
-        .build()
-
-    when:
-    def results = IndexingUtils.mapRecordToRequests(new IndexingInput(testKey, null, TestUtils.esConfig))
+    def results = IndexingUtils.mapRecordToRequests(new IndexingInput(testKey, null, TestUtils.collectionTopic, TestUtils.esConfig))
 
     then:
     results.size() == 2
-    results.every { it instanceof DeleteRequest }
-    results.every { it.id() == testKey }
-    results*.index() == ['a', 'b']
+    results.each { r -> r instanceof DeleteRequest }
+    results.each { r -> r.id() == testKey }
+    results*.index() == [
+        TestUtils.esConfig.COLLECTION_SEARCH_INDEX_ALIAS,
+        TestUtils.esConfig.COLLECTION_ERROR_AND_ANALYSIS_INDEX_ALIAS
+    ]
   }
 
-  def "creates multiple index requests when configured"() {
-    def testKey = 'ABC'
-    def testRecord = ParsedRecord.newBuilder()
-        .setType(RecordType.collection)
-        .setAnalysis(TestUtils.inputCollectionAnalysis)
-        .setDiscovery(TestUtils.inputCollectionDiscovery)
-        .setPublishing(Publishing.newBuilder().build()).build()
-    def testValue = ValueAndTimestamp.make(testRecord, System.currentTimeMillis())
-    def multipleIndexConfig = BulkIndexingConfig.newBuilder()
-        .withStoreName('teststore')
-        .withMaxPublishInterval(Duration.ofSeconds(10))
-        .withMaxPublishBytes(10000)
-        .withMaxPublishActions(1000)
-        .build()
+  def "tombstones create delete requests for granules"() {
+    def testKey = "ABC"
 
     when:
-    def results = IndexingUtils.mapRecordToRequests(new IndexingInput(testKey, testValue, TestUtils.esConfig))
+    def results = IndexingUtils.mapRecordToRequests(new IndexingInput(testKey, null, TestUtils.granuleTopic, TestUtils.esConfig))
+
+    then:
+    results.size() == 3
+    results.each { r -> r instanceof DeleteRequest }
+    results.each { r -> r.id() == testKey }
+    results*.index() == [
+        TestUtils.esConfig.GRANULE_SEARCH_INDEX_ALIAS,
+        TestUtils.esConfig.GRANULE_ERROR_AND_ANALYSIS_INDEX_ALIAS,
+        TestUtils.esConfig.FLAT_GRANULE_SEARCH_INDEX_ALIAS
+    ]
+  }
+
+  def "creates index requests for collections"() {
+    def testKey = 'ABC'
+    def testRecord = ParsedRecord.newBuilder(TestUtils.inputCollectionRecord)
+        .setPublishing(Publishing.newBuilder().build()).build()
+    def testValue = ValueAndTimestamp.make(testRecord, System.currentTimeMillis())
+
+    when:
+    def results = IndexingUtils.mapRecordToRequests(new IndexingInput(testKey, testValue, TestUtils.collectionTopic, TestUtils.esConfig))
 
     then:
     results.size() == 2
     results.every { it instanceof IndexRequest }
     results.every { it.id() == testKey }
-    results*.index() == ['a', 'b']
+    results*.index() == [
+        TestUtils.esConfig.COLLECTION_SEARCH_INDEX_ALIAS,
+        TestUtils.esConfig.COLLECTION_ERROR_AND_ANALYSIS_INDEX_ALIAS
+    ]
+  }
+
+  def "creates index requests for granules"() {
+    def testKey = 'ABC'
+    def testRecord = ParsedRecord.newBuilder(TestUtils.inputGranuleRecord)
+        .setPublishing(Publishing.newBuilder().build()).build()
+    def testValue = ValueAndTimestamp.make(testRecord, System.currentTimeMillis())
+
+    when:
+    def results = IndexingUtils.mapRecordToRequests(new IndexingInput(testKey, testValue, TestUtils.granuleTopic, TestUtils.esConfig))
+
+    then:
+    results.size() == 2
+    results.every { it instanceof IndexRequest }
+    results.every { it.id() == testKey }
+    results*.index() == [
+        TestUtils.esConfig.GRANULE_SEARCH_INDEX_ALIAS,
+        TestUtils.esConfig.GRANULE_ERROR_AND_ANALYSIS_INDEX_ALIAS,
+    ]
   }
 
   def "record that is #testCase creates delete request"() {
     def testKey = "ABC"
-    def testRecord = ParsedRecord.newBuilder().setPublishing(publishingObject).build()
+    def testRecord = ParsedRecord.newBuilder(TestUtils.inputCollectionRecord)
+        .setPublishing(publishingObject).build()
     def testValue = ValueAndTimestamp.make(testRecord, System.currentTimeMillis())
 
     when:
-    def results = IndexingUtils.mapRecordToRequests(new IndexingInput(testKey, testValue, TestUtils.esConfig))
+    def results = IndexingUtils.mapRecordToRequests(new IndexingInput(testKey, testValue, TestUtils.collectionTopic, TestUtils.esConfig))
 
     then:
-    results.size() == 1
-    results[0].id() == testKey
-    results[0].index() == testIndex
-    results[0] instanceof DeleteRequest
+    results.size() == 2
+    results.every { it instanceof DeleteRequest }
+    results.every { it.id() == testKey }
+    results*.index() == [
+        TestUtils.esConfig.COLLECTION_SEARCH_INDEX_ALIAS,
+        TestUtils.esConfig.COLLECTION_ERROR_AND_ANALYSIS_INDEX_ALIAS
+    ]
 
     where:
     testCase               | publishingObject
@@ -101,20 +113,21 @@ class IndexingUtilsSpec extends Specification {
 
   def "record that is #testCase creates index request"() {
     def testKey = "ABC"
-    def testRecord = ParsedRecord.newBuilder()
-        .setType(RecordType.collection)
-        .setAnalysis(TestUtils.inputCollectionAnalysis)
-        .setDiscovery(TestUtils.inputCollectionDiscovery)
+    def testRecord = ParsedRecord.newBuilder(TestUtils.inputCollectionRecord)
         .setPublishing(publishingObject).build()
     def testValue = ValueAndTimestamp.make(testRecord, System.currentTimeMillis())
 
     when:
-    def result = IndexingUtils.mapRecordToRequests(new IndexingInput(testKey, testValue, TestUtils.esConfig))
+    def results = IndexingUtils.mapRecordToRequests(new IndexingInput(testKey, testValue, TestUtils.collectionTopic, TestUtils.esConfig))
 
     then:
-    result[0].id() == testKey
-    result[0].index() == testIndex
-    result[0] instanceof IndexRequest
+    results.size() == 2
+    results.every { it instanceof IndexRequest }
+    results.every { it.id() == testKey }
+    results*.index() == [
+        TestUtils.esConfig.COLLECTION_SEARCH_INDEX_ALIAS,
+        TestUtils.esConfig.COLLECTION_ERROR_AND_ANALYSIS_INDEX_ALIAS
+    ]
 
     where:
     testCase              | publishingObject
--- indexer/src/test/groovy/org/cedar/onestop/indexer/util/TestUtils.groovy
+++ indexer/src/test/groovy/org/cedar/onestop/indexer/util/TestUtils.groovy
@@ -2,6 +2,8 @@ package org.cedar.onestop.indexer.util
 
 import org.cedar.onestop.elastic.common.ElasticsearchConfig
 import org.cedar.onestop.elastic.common.ElasticsearchVersion
+import org.cedar.onestop.kafka.common.constants.StreamsApps
+import org.cedar.onestop.kafka.common.constants.Topics
 import org.cedar.schemas.analyze.Analyzers
 import org.cedar.schemas.avro.psi.ParsedRecord
 import org.cedar.schemas.avro.psi.RecordType
@@ -10,15 +12,21 @@ import org.cedar.schemas.parse.ISOParser
 
 class TestUtils {
 
+  static collectionTopic = Topics.parsedChangelogTopic(StreamsApps.REGISTRY_ID, RecordType.collection)
+  static granuleTopic = Topics.parsedChangelogTopic(StreamsApps.REGISTRY_ID, RecordType.granule)
+
   static final inputStream = ClassLoader.systemClassLoader.getResourceAsStream('example-record-avro.json')
   static final inputRecord = AvroUtils.<ParsedRecord> jsonToAvro(inputStream, ParsedRecord.classSchema)
 
   static final inputCollectionXml = ClassLoader.systemClassLoader.getResourceAsStream('test-iso-collection.xml').text
   static inputCollectionRecord = buildRecordFromXML(inputCollectionXml)
-  static inputCollectionDiscovery = ISOParser.parseXMLMetadataToDiscovery(inputCollectionXml)
-  static inputCollectionAnalysis = Analyzers.analyze(inputCollectionDiscovery)
+//  static inputCollectionDiscovery = ISOParser.parseXMLMetadataToDiscovery(inputCollectionXml)
+//  static inputCollectionAnalysis = Analyzers.analyze(inputCollectionDiscovery)
 
   static inputGranuleXml = ClassLoader.systemClassLoader.getResourceAsStream('test-iso-granule.xml').text
+  static inputGranuleRecord = buildRecordFromXML(inputGranuleXml)
+//  static inputGranuleDiscovery = ISOParser.parseXMLMetadataToDiscovery(inputGranuleXml)
+//  static inputGranuleAnalysis = Analyzers.analyze(inputGranuleDiscovery)
 
   public static final esConfig = new ElasticsearchConfig(
       new ElasticsearchVersion("7.5.1"),
