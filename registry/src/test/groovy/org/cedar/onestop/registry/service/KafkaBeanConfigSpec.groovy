9d5cb6105181310dfb35c8fa89068370b4da7859
Evan McQuinn <evan.mcquinn@noaa.gov>
2020-05-21 15:15:32 -0600

    update test to verify error future is completed

--- registry/src/test/groovy/org/cedar/onestop/registry/service/KafkaBeanConfigSpec.groovy
+++ registry/src/test/groovy/org/cedar/onestop/registry/service/KafkaBeanConfigSpec.groovy
@@ -1,31 +1,34 @@
 package org.cedar.onestop.registry.service
 
-import org.apache.kafka.streams.KafkaStreams
-import org.cedar.onestop.registry.stream.TopicInitializer
 import spock.lang.Specification
 import spock.lang.Unroll
+
+import java.util.concurrent.CompletableFuture
+
+import static org.apache.kafka.streams.KafkaStreams.State.*
 
 @Unroll
 class KafkaBeanConfigSpec extends Specification {
 
   def beanConfig = new KafkaBeanConfig()
 
-  def "streams app throws exception when transitioning to bad state #state"() {
-    def mockInitializer = Mock(TopicInitializer)
+  def "streams app completes future when transitioning to error state"() {
     def streamsConfig = beanConfig.streamsConfig([
         'bootstrap.servers': 'http://localhost:9092',
         'schema.registry.url': 'http://localhost:8081'
     ])
-    def streamsApp = beanConfig.streamsApp(streamsConfig, mockInitializer)
+    def testFuture = new CompletableFuture()
+    def streamsApp = beanConfig.streamsApp(streamsConfig, testFuture)
+    // valid transitions to running state
+    streamsApp.setState(REBALANCING)
+    streamsApp.setState(RUNNING)
 
     when:
-    streamsApp.setState(state)
+    streamsApp.setState(ERROR)
 
     then:
-    thrown(IllegalStateException)
-
-    where:
-    state << [KafkaStreams.State.NOT_RUNNING, KafkaStreams.State.ERROR]
+    noExceptionThrown()
+    testFuture.isDone()
   }
 
 }
