bcf2dbc78e84eeeeda4bf1d7ef57b5da748d6a97
StrayCat1 <christopher.esterlein@colorado.edu>
2020-07-06 23:00:35 -0600

    add saved search reducers

--- client/src/reducers/user.js
+++ client/src/reducers/user.js
@@ -5,12 +5,14 @@ import {
   USER_PROFILE_FAILURE,
   USER_LOGOUT,
 } from '../actions/UserActions'
+import {SAVED_SEARCH_FAILURE, SAVED_SEARCH_REQUEST, SAVED_SEARCH_SUCCESS} from "../actions/SavedSearchActions";
 
 export const initialState = Immutable({
   isAuthenticated: false,
   profile: null,
   expired: false,
   isFetching: false,
+  searches: [],
 })
 
 export const user = (state = initialState, action) => {
@@ -19,9 +21,9 @@ export const user = (state = initialState, action) => {
       return state.setIn([ 'isFetching' ], true, action.item)
 
     case USER_PROFILE_SUCCESS:
-      if (action.payload.email) {
+      if (action.payload.data) {
         return state
-          .setIn([ 'profile' ], action.profile)
+          .setIn([ 'profile' ], action.payload.data)
           .setIn([ 'isFetching' ], false)
           .setIn([ 'isAuthenticated' ], true)
           .setIn([ 'expired' ], false)
@@ -40,6 +42,21 @@ export const user = (state = initialState, action) => {
         .setIn([ 'profile' ], {})
         .setIn([ 'expired' ], true)
         .setIn([ 'isAuthenticated' ], false)
+
+    case SAVED_SEARCH_REQUEST:
+      return state.setIn([ 'isFetchingSearches' ], true, action.item)
+
+    case SAVED_SEARCH_SUCCESS:
+      if (action.payload.data) {
+        return state
+            .setIn([ 'searches' ], action.payload.data)
+            .setIn([ 'isFetchingSearches' ], false)
+      }
+
+    case SAVED_SEARCH_FAILURE:
+      return state
+          .setIn([ 'error' ], action.error)
+          .setIn([ 'isFetchingSearches' ], false)
     default:
       return state
   }
