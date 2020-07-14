cc026ac581c95336a7201f8ff1ae3a36891b6a30
StrayCat1 <christopher.esterlein@colorado.edu>
2020-07-13 21:51:09 -0600

    remove null constraint on saved search name

--- user/src/main/resources/schema.sql
+++ user/src/main/resources/schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE savesearch
 (
     id VARCHAR(255) PRIMARY KEY NOT NULL,
     user_id VARCHAR(255) NOT NULL,
-    name VARCHAR(255) NOT NULL,
+    name VARCHAR(255),
     value VARCHAR(2047),
     created_on DATE ,
     last_updated_on DATE
