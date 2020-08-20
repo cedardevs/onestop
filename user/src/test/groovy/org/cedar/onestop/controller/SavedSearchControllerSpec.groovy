13ee7ecc47edadcc1bcfbb4656934d88f2b9529f
Erin <erin.rowland@colorado.edu>
2020-08-19 18:11:31 -0600

    1315 - Refactored successful response generation into a method. Adjusted SavedSearchController endpoints to return a JsonApiResponse for success and failure so as to be consistent with Json Api spec

--- user/src/test/groovy/org/cedar/onestop/controller/SavedSearchControllerSpec.groovy
+++ user/src/test/groovy/org/cedar/onestop/controller/SavedSearchControllerSpec.groovy
@@ -8,7 +8,9 @@ import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import spock.lang.Shared
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class SavedSearchControllerSpec extends Specification {
 
@@ -16,7 +18,9 @@ class SavedSearchControllerSpec extends Specification {
   SavedSearchRepository mockSaveSearchRepository = Mock(SavedSearchRepository)
   SavedSearchController controller = new SavedSearchController(mockSaveSearchRepository)
 
+  @Shared
   SavedSearch searchResult1 = new SavedSearch('1', '11', 'name1', 'filter1', 'value1')
+  @Shared
   SavedSearch searchResult2 = new SavedSearch('2', '12', 'name2', 'filter2', 'value2')
 
   def setup() {
@@ -25,14 +29,15 @@ class SavedSearchControllerSpec extends Specification {
 
   def "save search items "() {
     when:
-    def postSearch = mockMvc.perform(MockMvcRequestBuilders
+    def results = mockMvc.perform(MockMvcRequestBuilders
         .post("/v1/saved-search")
         .contentType("application/json")
         .content(('{ "userId": "u1", "name": "test", "value": "value" }'))
         .accept(MediaType.APPLICATION_JSON))
 
     then:
-    postSearch.andExpect(MockMvcResultMatchers.status().isUnauthorized())
+//    results.andExpect(MockMvcResultMatchers.status().isUnauthorized())
+    results.andReturn().getResponse().getContentAsString() == """{"meta":null,"id":null,"status":"UNAUTHORIZED","code":"Unauthorized","title":null,"detail":null,"source":null}"""
   }
 
   def "get save searches by id"() {
@@ -42,7 +47,8 @@ class SavedSearchControllerSpec extends Specification {
         .accept(MediaType.APPLICATION_JSON))
 
     then:
-    results.andExpect(MockMvcResultMatchers.status().isUnauthorized())
+//    results.andExpect(MockMvcResultMatchers.status().isUnauthorized())
+    results.andReturn().getResponse().getContentAsString() == """{"meta":null,"id":null,"status":"UNAUTHORIZED","code":"Unauthorized","title":null,"detail":null,"source":null}"""
   }
 
 //  def "bad request"() {
@@ -65,55 +71,78 @@ class SavedSearchControllerSpec extends Specification {
         .accept(MediaType.APPLICATION_JSON))
 
     then:
-    results.andExpect(MockMvcResultMatchers.status().isUnauthorized())
+//    results.andExpect(MockMvcResultMatchers.status().isUnauthorized())
+    results.andReturn().getResponse().getContentAsString() == """{"meta":null,"id":null,"status":"UNAUTHORIZED","code":"Unauthorized","title":null,"detail":null,"source":null}"""
   }
 
-  def "get saved search all returns empty data response"() {
+  @Unroll
+  def "endpoint 'saved-search/all' returns json api spec response for repository response of #mockRepoResponse"() {
     when:
     def results = mockMvc.perform(MockMvcRequestBuilders
         .get("/v1/saved-search/all")
         .accept(MediaType.APPLICATION_JSON))
 
     then:
+    1 * mockSaveSearchRepository.findAll() >> mockRepoResponse
     results.andExpect(MockMvcResultMatchers.status().isOk())
-    String content = results.andReturn().getResponse().getContentAsString()
-    content == """{"data":[],"meta":null}"""
+    results.andReturn().getResponse().getContentAsString() == expResponse
+
+    where:
+    mockRepoResponse               | expResponse
+    []                             | """{"data":[],"meta":null}"""
+    [searchResult1, searchResult2] | '{"data":[{"id":"' + searchResult1.id + '",' +
+                                    '"type":"' + SavedSearchController.type + '",' +
+                                    '"attributes":{' +
+                                    '"filter":"' + searchResult1.filter + '",' +
+                                    '"name":"' + searchResult1.name + '",' +
+                                    '"lastUpdatedOn":null,' +
+                                    '"id":"' + searchResult1.id + '",' +
+                                    '"userId":"' + searchResult1.userId + '",' +
+                                    '"value":"' + searchResult1.value + '",' +
+                                    '"createdOn":null}' +
+                                    '},' +
+                                    '{"id":"' + searchResult2.id + '",' +
+                                    '"type":"' + SavedSearchController.type + '",' +
+                                    '"attributes":{' +
+                                    '"filter":"' + searchResult2.filter + '",' +
+                                    '"name":"' + searchResult2.name + '",' +
+                                    '"lastUpdatedOn":null,' +
+                                    '"id":"' + searchResult2.id + '",' +
+                                    '"userId":"' + searchResult2.userId + '",' +
+                                    '"value":"' + searchResult2.value + '",' +
+                                    '"createdOn":null}' +
+                                    '}' +
+                                    '],' +
+                                    '"meta":null}'
   }
 
-  def "getAll() returns expected Json Api Spec response for multiple results"() {
+  @Unroll
+  def "endpoint (update) 'saved-search/{id}' returns json api spec response for repository response of #mockRepoResponse"() {
     when:
     def results = mockMvc.perform(MockMvcRequestBuilders
         .get("/v1/saved-search/all")
+        .content(('{ "userId": "' + searchResult1.userId + '" }'))
         .accept(MediaType.APPLICATION_JSON))
 
-    then: 'Return conforms to Json Api Spec'
-    1 * mockSaveSearchRepository.findAll() >> [searchResult1, searchResult2]
-
+    then:
+    1 * mockSaveSearchRepository.findAll() >> mockRepoResponse
     results.andExpect(MockMvcResultMatchers.status().isOk())
-    String content = results.andReturn().getResponse().getContentAsString()
-    content == '{"data":[{"id":"' + searchResult1.id + '",' +
-        '"type":"' + SavedSearchController.type + '",' +
-        '"attributes":{' +
-        '"filter":"'+ searchResult1.filter + '",' +
-        '"name":"'+ searchResult1.name + '",' +
-        '"lastUpdatedOn":null,' +
-        '"id":"'+ searchResult1.id + '",' +
-        '"userId":"'+ searchResult1.userId + '",' +
-        '"value":"'+ searchResult1.value + '",' +
-        '"createdOn":null}' +
-        '},' +
-        '{"id":"' + searchResult2.id + '",' +
-        '"type":"' + SavedSearchController.type + '",' +
-        '"attributes":{' +
-        '"filter":"'+ searchResult2.filter + '",' +
-        '"name":"'+ searchResult2.name + '",' +
-        '"lastUpdatedOn":null,' +
-        '"id":"'+ searchResult2.id + '",' +
-        '"userId":"'+ searchResult2.userId + '",' +
-        '"value":"'+ searchResult2.value + '",' +
-        '"createdOn":null}' +
-        '}' +
-        '],' +
-        '"meta":null}'
+    results.andReturn().getResponse().getContentAsString() == expResponse
+
+    where:
+    params                                         | mockRepoResponse | expResponse
+    '{}'                                           | []               | """{"data":[],"meta":null}"""
+    '{ "userId": "' + searchResult1.userId + '" }' | [searchResult1]  | '{"data":[{"id":"' + searchResult1.id + '",' +
+                                                                        '"type":"' + SavedSearchController.type + '",' +
+                                                                        '"attributes":{' +
+                                                                        '"filter":"' + searchResult1.filter + '",' +
+                                                                        '"name":"' + searchResult1.name + '",' +
+                                                                        '"lastUpdatedOn":null,' +
+                                                                        '"id":"' + searchResult1.id + '",' +
+                                                                        '"userId":"' + searchResult1.userId + '",' +
+                                                                        '"value":"' + searchResult1.value + '",' +
+                                                                        '"createdOn":null}' +
+                                                                        '}],' +
+                                                                        '"meta":null}'
   }
 }
