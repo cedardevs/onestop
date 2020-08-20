674dc017a6d51b49fa0a5a8f09b0c163a253bcb5
Erin <erin.rowland@colorado.edu>
2020-08-19 18:07:42 -0600

    1315 - For JsonApiMeta added fields, setters, getters, and builders.

--- user/src/main/java/org/cedar/onestop/user/common/JsonApiMeta.java
+++ user/src/main/java/org/cedar/onestop/user/common/JsonApiMeta.java
@@ -1,9 +1,32 @@
 package org.cedar.onestop.user.common;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Temporary copy of what should eventually exist in array-iteration module.
  */
-
 public class JsonApiMeta {
+  Map nonStandardMetadata;
 
+  public JsonApiMeta(Map nonStandardMetadata) {
+    this.nonStandardMetadata = nonStandardMetadata;
+  }
+
+  public Map getNonStandardMetadata() {
+    return nonStandardMetadata;
+  }
+
+  static public class Builder {
+    Map nonStandardMetadata = new HashMap();
+
+    public Builder setNonStandardMetadata(Map nonStandardMetadata) {
+      this.nonStandardMetadata = nonStandardMetadata;
+      return this;
+    }
+
+    public JsonApiMeta build() {
+      return new JsonApiMeta(this.nonStandardMetadata);
+    }
+  }
 }
