f420bd2ff52209ba60689c85920b95335e5f2780
StrayCat1 <christopher.esterlein@colorado.edu>
2020-09-02 22:44:32 -0600

    update domain classes for json serialized responses

--- user/src/main/java/org/cedar/onestop/user/domain/OnestopRole.java
+++ user/src/main/java/org/cedar/onestop/user/domain/OnestopRole.java
@@ -1,13 +1,16 @@
 package org.cedar.onestop.user.domain;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
-import java.util.Collection;
+import java.util.*;
 
 @Entity
 @Table(name="onestop_roles")
 public class OnestopRole {
+
     @Id
     @Column(name= "role_id")
     @GeneratedValue(generator = "uuid")
@@ -19,6 +22,14 @@ public class OnestopRole {
 
     @ManyToMany(mappedBy = "roles")
     private Collection<OnestopUser> users;
+
+    @Column(name = "createdOn", updatable = false)
+    @CreationTimestamp
+    public Date createdOn; //TODO do we also accept part of the query
+
+    @Column(name = "lastUpdatedOn")
+    @UpdateTimestamp
+    public Date lastUpdatedOn; ///TODO do we also accept part of the query
 
     @ManyToMany
     @JoinTable(
@@ -33,11 +44,23 @@ public class OnestopRole {
     }
 
     public OnestopRole(String id, String name) {
+        this.id = id;
         this.name = name;
     }
 
     public OnestopRole(String name) {
         this.name = name;
+    }
+
+    public OnestopRole(String id, String name, Collection<OnestopPrivilege> privileges) {
+        this.id = id;
+        this.name = name;
+        this.privileges = privileges;
+    }
+
+    public OnestopRole(String name, Collection<OnestopPrivilege> privileges) {
+        this.name = name;
+        this.privileges = privileges;
     }
 
     public String getId() {
@@ -70,5 +93,30 @@ public class OnestopRole {
 
     public void setPrivileges(Collection<OnestopPrivilege> privileges) {
         this.privileges = privileges;
+    }
+
+    public Map<String, Object> toMap() {
+        Map<String, Object> result = new HashMap<String, Object>();
+        result.put("id", id);
+        result.put("name", name);
+        result.put("privileges", privToStringList());
+        result.put("createdOn", createdOn);
+        result.put("lastUpdatedOn", lastUpdatedOn);
+        return result;
+    }
+
+    public String toString(){
+        return toMap().toString();
+    }
+
+    public List<Map> privToStringList(){
+        List<Map> privList = new ArrayList<>();
+        if(privileges == null){
+            return privList;
+        }
+        for(int i = 0; i < privileges.size(); i++) {
+            privList.add(((List<OnestopPrivilege>)privileges).get(i).toMap());
+        }
+        return privList;
     }
 }
