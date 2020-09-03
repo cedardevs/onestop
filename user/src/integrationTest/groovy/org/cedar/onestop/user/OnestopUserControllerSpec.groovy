04353ecfa53a383133a72be931c135e78fa40e74
StrayCat1 <christopher.esterlein@colorado.edu>
2020-09-02 22:49:39 -0600

    controller integration tests for user with role and privs

--- user/src/integrationTest/groovy/org/cedar/onestop/user/OnestopUserControllerSpec.groovy
+++ user/src/integrationTest/groovy/org/cedar/onestop/user/OnestopUserControllerSpec.groovy
@@ -1,5 +1,13 @@
 package org.cedar.onestop.user
 
+import groovy.json.JsonOutput
+import groovy.json.JsonSlurper
+import org.cedar.onestop.user.config.SecurityConfig
+import org.cedar.onestop.user.domain.OnestopPrivilege
+import org.cedar.onestop.user.domain.OnestopRole
+import org.cedar.onestop.user.domain.OnestopUser
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
@@ -8,6 +16,7 @@ import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfig
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.web.context.WebApplicationContext
@@ -20,12 +29,15 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 @SpringBootTest(classes = [UserApplication.class], webEnvironment = RANDOM_PORT)
 class OnestopUserControllerSpec extends Specification  {
 
+  Logger logger = LoggerFactory.getLogger(OnestopUserControllerSpec.class)
   private static final PostgreSQLContainer postgres = new PostgreSQLContainer()
 
   @Autowired
   private WebApplicationContext context
 
   private MockMvc mvc
+
+  JsonSlurper slurper = new JsonSlurper()
 
   def setup() {
     mvc = MockMvcBuilders
@@ -44,20 +56,7 @@ class OnestopUserControllerSpec extends Specification  {
     postgres.stop()
   }
 
-//  @WithMockUser(username = "mockUser", roles = "PUBLIC")
-//  def "public user can hit profile endpoint"() {
-//    when:
-//    def getResults = mvc.perform(MockMvcRequestBuilders
-//        .get("/v1/user")
-//        .accept(MediaType.APPLICATION_JSON))
-//
-//    then:
-//    getResults.andExpect(MockMvcResultMatchers.status().isOk())
-//        .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
-//        .andExpect(MockMvcResultMatchers.jsonPath("\$.data.userId").value("mockUser"))
-//  }
-//
-  @WithMockUser(username = "mockUser", roles = "PUBLIC")
+  @WithMockUser(username = "mockUser", roles = SecurityConfig.PUBLIC_PRIVILEGE)
   def "user is created"() {
     when:
     def postSearch = mvc.perform(MockMvcRequestBuilders
@@ -72,7 +71,7 @@ class OnestopUserControllerSpec extends Specification  {
 
   }
 
-  @WithMockUser(username = "mockUser", roles = "PUBLIC")
+  @WithMockUser(username = "mockUser", roles = SecurityConfig.PUBLIC_PRIVILEGE)
   def "user is created, search is saved"() {
     when:
     def postUser = mvc.perform(MockMvcRequestBuilders
@@ -93,9 +92,77 @@ class OnestopUserControllerSpec extends Specification  {
         .accept(MediaType.APPLICATION_JSON))
 
     then:
-    postSearch.andExpect(MockMvcResultMatchers.status().isCreated())
+    postSearch.andExpect(MockMvcResultMatchers.status().isCreated()).andDo(MockMvcResultHandlers.print())
         .andExpect(MockMvcResultMatchers.jsonPath("\$.data[0].attributes.value").value("test/test"))
         .andExpect(MockMvcResultMatchers.jsonPath("\$.data[0].attributes.name").value("test search"))
+        .andExpect(MockMvcResultMatchers.jsonPath("\$.data[0].attributes.user.id").value("mockUser"))
+  }
 
+  @WithMockUser(username = "mockUser", roles = SecurityConfig.ADMIN_PRIVILEGE)
+  def "user is created with roles and privileges"() {
+    given:
+    OnestopPrivilege readPriv = new OnestopPrivilege("read")
+    OnestopPrivilege writePriv = new OnestopPrivilege("write")
+
+    when:
+    def postPriv1 = mvc.perform(MockMvcRequestBuilders
+        .post("/v1/privilege")
+        .contentType("application/json")
+        .content(JsonOutput.toJson(readPriv.toMap()))
+        .accept(MediaType.APPLICATION_JSON))
+
+    def postPriv2 = mvc.perform(MockMvcRequestBuilders
+        .post("/v1/privilege")
+        .contentType("application/json")
+        .content(JsonOutput.toJson(writePriv.toMap()))
+        .accept(MediaType.APPLICATION_JSON))
+
+    then:
+    postPriv1.andExpect(MockMvcResultMatchers.status().isCreated()).andDo(MockMvcResultHandlers.print())
+        .andExpect(MockMvcResultMatchers.jsonPath("\$.data[0].attributes.name").value("read"))
+
+    postPriv2.andExpect(MockMvcResultMatchers.status().isCreated()).andDo(MockMvcResultHandlers.print())
+        .andExpect(MockMvcResultMatchers.jsonPath("\$.data[0].attributes.name").value("write"))
+
+    def priv1ResponseJson = postPriv1.andReturn().getResponse().getContentAsString()
+    def priv2ResponseJson = postPriv2.andReturn().getResponse().getContentAsString()
+
+    Map priv1Map = slurper.parseText(priv1ResponseJson)["data"][0]
+    Map priv2Map = slurper.parseText(priv2ResponseJson)["data"][0]
+
+    OnestopPrivilege savedReadPriv = new OnestopPrivilege(priv1Map["id"] as String, priv1Map["attributes"]["name"] as String)
+    OnestopPrivilege savedWritePriv = new OnestopPrivilege(priv2Map["id"] as String, priv2Map["attributes"]["name"] as String)
+
+    OnestopRole role = new OnestopRole("public",  Arrays.asList(savedReadPriv, savedWritePriv))
+
+    and:
+    def postRole = mvc.perform(MockMvcRequestBuilders
+        .post("/v1/role")
+        .contentType("application/json")
+        .content(JsonOutput.toJson(role.toMap()))
+        .accept(MediaType.APPLICATION_JSON))
+
+    then:
+    postRole.andExpect(MockMvcResultMatchers.status().isCreated()).andDo(MockMvcResultHandlers.print())
+        .andExpect(MockMvcResultMatchers.jsonPath("\$.data[0].attributes.name").value("public"))
+        .andExpect(MockMvcResultMatchers.jsonPath("\$.data[0].attributes.privileges[0].name").value("read"))
+        .andExpect(MockMvcResultMatchers.jsonPath("\$.data[0].attributes.privileges[1].name").value("write"))
+
+
+    def role1JsonString = postRole.andReturn().getResponse().getContentAsString()
+    Map roleMap = slurper.parseText(role1JsonString)["data"][0]
+    OnestopRole savedRole = new OnestopRole(roleMap.id as String, "public",  Arrays.asList(savedReadPriv, savedWritePriv))
+
+    and:
+    def postUser = mvc.perform(MockMvcRequestBuilders
+        .post("/v1/user")
+        .contentType("application/json")
+        .content("{\"roles\":[${JsonOutput.toJson(savedRole.toMap())}]}")
+        .accept(MediaType.APPLICATION_JSON))
+
+    then:
+    postUser.andExpect(MockMvcResultMatchers.status().isCreated()).andDo(MockMvcResultHandlers.print())
+        .andExpect(MockMvcResultMatchers.jsonPath("\$.data[0].id").value("mockUser"))
+        .andExpect(MockMvcResultMatchers.jsonPath("\$.data[0].attributes.roles[0].name").value("public"))
   }
 }
