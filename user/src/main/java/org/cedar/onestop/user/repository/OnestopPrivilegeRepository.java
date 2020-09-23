d913332e9b107c8fd48f8d224255f75652259d23
StrayCat1 <christopher.esterlein@colorado.edu>
2020-09-23 14:35:05 -0600

    add repository annotation

--- user/src/main/java/org/cedar/onestop/user/repository/OnestopPrivilegeRepository.java
+++ user/src/main/java/org/cedar/onestop/user/repository/OnestopPrivilegeRepository.java
@@ -2,7 +2,9 @@ package org.cedar.onestop.user.repository;
 
 import org.cedar.onestop.user.domain.OnestopPrivilege;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OnestopPrivilegeRepository extends JpaRepository<OnestopPrivilege, String> {
     OnestopPrivilege findByName(String name);
 }
