dd5891c0464d5cd85fb853150a29223c6db50f3b
StrayCat1 <christopher.esterlein@colorado.edu>
2020-11-17 15:31:17 -0700

    update helm stable chart repo url

--- helm/infraInstall.sh
+++ helm/infraInstall.sh
@@ -4,7 +4,7 @@
 kubectl apply -f https://download.elastic.co/downloads/eck/1.1.0/all-in-one.yaml
 
 # add helm repos
-helm repo add stable https://kubernetes-charts.storage.googleapis.com/
+helm repo add stable https://charts.helm.sh/stable
 helm repo add confluent https://confluentinc.github.io/cp-helm-charts/
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo add bitnami https://charts.bitnami.com/bitnami
