d7de9c31421ae7ba4f27d6f2482a934f826c0cf3
brandon <brandon.shelton@colorado.edu>
2021-03-04 10:19:18 -0700

    initial implementation of JSON output

---
+++ sitemap/generatorJSON.js
@@ -0,0 +1,104 @@
+const yargs = require('yargs')
+const axios = require('axios')
+const fs = require('fs')
+const path = require('path')
+
+const argv = yargs
+  .option('api', {
+    alias: 'a',
+    description: 'Base URL for the catalog search api, e.g. http://localhost/onestop-search',
+    type: 'string',
+    default: 'http://localhost/onestop/api/search'
+  })
+  .option('website', {
+    alias: 'w',
+    description: 'Base URL for the catalog web page, e.g. http://localhost/onestop',
+    type: 'string',
+    default: 'http://localhost/onestop'
+  })
+  .option('page-size', {
+    alias: 'n',
+    description: 'Number of collections to request at a time',
+    type: 'number',
+    default: 10
+  })
+  .option('output-file', {
+    alias: 'o',
+    description: 'Name of the output JSON file',
+    type: 'string',
+    default: 'data'
+  })
+  .help()
+  .alias('help', 'h')
+  .argv
+
+
+const searchApiBase = argv.api
+const webBase = argv.website
+const collectionApiUrl = `${searchApiBase}/search/collection`
+const pageSize = argv['page-size']
+const fileName = argv['output-file'] + ".json"
+
+function processResponseData(response) {
+  return Array.from(response.data.data).map((d) => {
+    return { url: `${webBase}/collections/details/${d.id}` }
+  })
+}
+
+let axiosOptions = {
+  url: collectionApiUrl,
+  method: 'POST',
+  json: true,
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  },
+  data: {
+    "summary": false,
+    "sort": [{
+      "stagedDate": "asc"
+    }],
+    "search_after": [0],
+    "queries": [],
+    "page": {
+      "max": pageSize
+    }
+  }
+}
+
+async function pageApi (axiosOptions, collectionList) {
+  console.log("Requesting page for data after: " + axiosOptions.data.search_after[0])
+  await axios(axiosOptions)
+    .then((response) => {
+      console.log("Response status: " + response.status)
+      if (response.status === 200) {
+        let body = response.data
+        // If we got data back, process it and then keep going until we dont have anymore data
+        if (body && body.data.length > 0) {
+          // grab the last staged date, we will need it for the subsequent request
+          const lastStagedDate = body.data[body.data.length - 1].attributes.stagedDate
+          // update the axiosOptions
+          axiosOptions.data.search_after = [lastStagedDate]
+          // create the data structure we need for the sitemap tool
+          const bodyDataObjectList = processResponseData(response)
+          // add it to the list
+          collectionList = [...collectionList, ...bodyDataObjectList]
+          console.log("Received " + body.data.length + " items, continue paging...")
+          // get the next page
+          collectionList = pageApi(axiosOptions, collectionList)
+        } else {
+          console.log("No more data. Generating sitemap...")
+        }
+      }
+    })
+    //TODO - Future story more error handling
+    .catch(function (error) {
+      console.log("ERROR")
+      console.log(error)
+      //outStream.close()
+    })
+  return collectionList
+}
+
+//const outStream = fs.createWriteStream(path.resolve(fileName), { autoClose: false })
+pageApi(axiosOptions, []).then((list) => console.dir(list))
