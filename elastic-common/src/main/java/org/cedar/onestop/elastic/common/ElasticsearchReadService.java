97fb8f21b6c500364030f1d0461c8d60cf477fc8
Erin <erin.rowland@colorado.edu>
2022-03-07 13:44:10 -0700

    In elastic-common added error checking to the search class ElasticsearchReadService, including a method for constructing the error response to use by all search methods. Merged some code only used by 1 method back in for clarity of what was going on.

--- elastic-common/src/main/java/org/cedar/onestop/elastic/common/ElasticsearchReadService.java
+++ elastic-common/src/main/java/org/cedar/onestop/elastic/common/ElasticsearchReadService.java
@@ -35,6 +35,11 @@ public class ElasticsearchReadService extends ElasticsearchService {
     Request totalCountsRequest = assembleTotalCountRequest(endpoint, query);
     Map<String, Object> marshalledResponse = performRequest(totalCountsRequest);
 
+    var error = constructSearchErrorResponse(marshalledResponse);
+    if (error != null) {
+      return error;
+    }
+
     Map<String, Object> attributes = new HashMap<>();
     attributes.put("count", extractHitsValue(marshalledResponse));
     attributes.put("exactCount", isCountExact(marshalledResponse));
@@ -54,7 +59,6 @@ public class ElasticsearchReadService extends ElasticsearchService {
     resultMap.put("meta", meta);
 
     return resultMap;
-    // FIXME errors?
   }
 
   /**
@@ -74,6 +78,11 @@ public class ElasticsearchReadService extends ElasticsearchService {
     Request totalCountsRequest = assembleTotalCountRequest(endpoint, query);
     Map<String, Object> marshalledResponse = performRequest(totalCountsRequest);
 
+    var error = constructSearchErrorResponse(marshalledResponse);
+    if (error != null) {
+      return error;
+    }
+
     Map<String, Object> attributes = new HashMap<>();
     attributes.put("count", extractHitsValue(marshalledResponse));
     attributes.put("exactCount", isCountExact(marshalledResponse));
@@ -89,7 +98,6 @@ public class ElasticsearchReadService extends ElasticsearchService {
     resultMap.put("data", List.of(dataObject));
 
     return resultMap;
-    // FIXME errors?
   }
 
                ///////////////////////////////
@@ -152,27 +160,23 @@ public class ElasticsearchReadService extends ElasticsearchService {
    */
   public Map<String, Object> getById(String alias, String id)  {
     String endpoint = "/" + alias + "/_doc/" + id;
-    log.debug("Get by ID against endpoint: " + endpoint);
+    String type = config.typeFromAlias(alias);
+    log.debug("Get by ID (type="+type+") against endpoint: " + endpoint);
     Request idRequest = new Request("GET", endpoint);
     Map<String, Object> marshalledResponse = performRequest(idRequest);
-    String type = config.typeFromAlias(alias);
+
+    var error = constructSearchErrorResponse(marshalledResponse);
+    if (error != null) {
+      return error;
+    }
 
     Map<String, Object> response = new HashMap<>();
-    if ((Boolean) marshalledResponse.get("found")) {
-      var dataMap = new HashMap<>();
-      dataMap.put("id", marshalledResponse.get("_id"));
-      dataMap.put("type", config.typeFromAlias(alias));
-      dataMap.put("attributes", marshalledResponse.get("_source"));
-      response.put("data", List.of(dataMap));
-    }
-    else {
-      var error = new HashMap<String, Object>();
-      error.put("status", HttpStatus.SC_NOT_FOUND);
-      error.put("title", "No such document");
-      error.put("detail", "Record type [ " + type + " ] with ID [ " + id + " ] does not exist.");
-      response.put("errors", List.of(error));
-    }
+    var dataMap = new HashMap<>();
+    dataMap.put("id", marshalledResponse.get("_id"));
+    dataMap.put("type", config.typeFromAlias(alias));
+    dataMap.put("attributes", marshalledResponse.get("_source"));
 
+    response.put("data", List.of(dataMap));
     return response;
   }
 
@@ -193,33 +197,24 @@ public class ElasticsearchReadService extends ElasticsearchService {
     var request = new Request("GET", endpoint);
     var marshalledResponse = performRequest(request);
 
+    var error = constructSearchErrorResponse(marshalledResponse);
+    if (error != null) {
+      return error;
+    }
+
+    // Actual timestamped name is used in the response, not the alias, but need to drop the "statusCode"
+    List<String> keys = marshalledResponse.keySet().stream()
+        .filter(key -> !key.equals("statusCode"))
+        .collect(Collectors.toList());
+    String indexName = keys.get(0); // Only one key left in response
+
+    var data = new HashMap<String, Object>();
+    data.put("id", alias);
+    data.put("type", "index-map");
+    data.put("attributes", marshalledResponse.get(indexName));
+
     var response = new HashMap<String, Object>();
-
-    if(!marshalledResponse.containsKey("error")) {
-      // Actual timestamped name is used in the response, not the alias, but need to drop the "statusCode"
-      List<String> keys = marshalledResponse.keySet().stream()
-          .filter(key -> !key.equals("statusCode"))
-          .collect(Collectors.toList());
-      String indexName = keys.get(0); // Only one key left in response
-
-      var data = new HashMap<String, Object>();
-      data.put("id", alias);
-      data.put("type", "index-map");
-      data.put("attributes", marshalledResponse.get(indexName));
-
-      response.put("data", List.of(data));
-    }
-    else {
-      // FIXME granularity in error response -- could be a variety of errors here, but assuming always "not found" right now
-      var error = new HashMap<String, Object>();
-      error.put("status", HttpStatus.SC_NOT_FOUND);
-      error.put("title", "Index not found");
-      error.put("detail", "Index with alias [ " + alias + " ] does not exist.");
-
-      // FIXME -- put ES-specific info in a meta block?
-
-      response.put("errors", List.of(error));
-    }
+    response.put("data", List.of(data));
 
     return response;
   }
@@ -236,34 +231,24 @@ public class ElasticsearchReadService extends ElasticsearchService {
     var request = new Request("GET", endpoint);
     var marshalledResponse = performRequest(request);
 
+    var error = constructSearchErrorResponse(marshalledResponse);
+    if (error != null) {
+      return error;
+    }
+
+    // Actual timestamped name is used in the response, not the alias, but need to drop the "statusCode"
+    List<String> keys = marshalledResponse.keySet().stream()
+        .filter(key -> !key.equals("statusCode"))
+        .collect(Collectors.toList());
+    String indexName = keys.get(0); // Only one key left in response
+
+    var data = new HashMap<String, Object>();
+    data.put("id", alias);
+    data.put("type", "index-map");
+    data.put("attributes", marshalledResponse.get(indexName));
+
     var response = new HashMap<String, Object>();
-
-    if(!marshalledResponse.containsKey("error")) {
-      // Actual timestamped name is used in the response, not the alias, but need to drop the "statusCode"
-      List<String> keys = marshalledResponse.keySet().stream()
-          .filter(key -> !key.equals("statusCode"))
-          .collect(Collectors.toList());
-      String indexName = keys.get(0); // Only one key left in response
-
-      var data = new HashMap<String, Object>();
-      data.put("id", alias);
-      data.put("type", "index-map");
-      data.put("attributes", marshalledResponse.get(indexName));
-
-      response.put("data", List.of(data));
-    }
-    else {
-      // FIXME granularity in error response -- could be a variety of errors here, but assuming always "not found" right now
-      var error = new HashMap<String, Object>();
-      error.put("status", HttpStatus.SC_NOT_FOUND);
-      error.put("title", "Index not found");
-      error.put("detail", "Index with alias [ " + alias + " ] does not exist.");
-
-      // FIXME -- put ES-specific info in a meta block?
-
-      response.put("errors", List.of(error));
-    }
-
+    response.put("data", List.of(data));
     return response;
   }
 
@@ -312,7 +297,7 @@ public class ElasticsearchReadService extends ElasticsearchService {
       var error = new HashMap<String, Object>();
       error.put("status", HttpStatus.SC_BAD_REQUEST);
       error.put("title", "Could not parse Elasticsearch request body");
-      error.put("detail", "Search request body for Elasticsearch request did not parse as valid JSON.");
+      error.put("detail", "Search request body missing or did not parse as valid JSON.");
 
       response.put("errors", List.of(error));
       return response;
@@ -323,17 +308,27 @@ public class ElasticsearchReadService extends ElasticsearchService {
   }
 
   /**
-   *
-   * @param marshalledResponse
-   * @return
+   * Reorganize the search response from ES to how we want it.
+   * If an error occurred in ES and the response isn't what we expect capture the error and return that.
+   * @param marshalledResponse Map of the response with statusCode from ES.
+   * @return Map with attributes we expect from our search results or a list of errors.
    */
   public Map<String, Object> constructSearchResponse(Map<String, Object> marshalledResponse) {
-    // FIXME what happens in event of error response from ES?
+    var error = constructSearchErrorResponse(marshalledResponse);
+    if (error != null) {
+      return error;
+    }
 
     var dataList = new ArrayList<Map<String, Object>>();
-    var docs = getDocuments(marshalledResponse);
-    docs.forEach( m -> {
-      dataList.add(constructDataElement(m));
+    var hits = (Map) marshalledResponse.get("hits");
+    List<Map<String, Object>> documents = (List) hits.get("hits");
+    documents.forEach( m -> {
+      var dataElement = new HashMap<String, Object>();
+      dataElement.put("id", m.get("_id"));
+      dataElement.put("type", config.typeFromIndex((String) m.get("_index")));
+      dataElement.put("attributes", m.get("_source"));
+
+      dataList.add(dataElement);
     });
 
     var meta = new HashMap<String, Object>();
@@ -348,22 +343,29 @@ public class ElasticsearchReadService extends ElasticsearchService {
     return searchResponse;
   }
 
-                ///////////////////////////////
-                //       Search Helpers      //
-                ///////////////////////////////
+  /**
+   * If an error in ES search query happened the repsonse will reflect that.
+   * Capture that error and respond with a Map with `errors` attribute having a List of errors, most likely just one.
+   * @param marshalledResponse Map of the response with statusCode from ES.
+   * @return null if no errors or Map with `errors` attribute having a List of errors
+   */
+  public Map<String, Object> constructSearchErrorResponse(Map<String, Object> marshalledResponse) {
+    // If an error response from elasticsearch //
+    if (marshalledResponse.get("statusCode") != "200") {
+      log.error("Elasticsearch error response: " + marshalledResponse);
 
-  private List<Map<String, Object>> getDocuments(Map<String, Object> marshalledResponse) {
-    var hits = (Map) marshalledResponse.get("hits");
-    var documents = (List) hits.get("hits");
-    return documents;
-  }
+      Map<String, Object> esError = (Map<String, Object>) marshalledResponse.get("error");
+      var error = new HashMap<String, Object>();
+      error.put("status", marshalledResponse.get("statusCode"));
+      error.put("title", esError.get("type"));
+      error.put("detail", "Elasticsearch error: " + esError.get("reason"));
 
-  private Map<String, Object> constructDataElement(Map<String, Object> elasticDocument) {
-    var dataElement = new HashMap<String, Object>();
-    dataElement.put("id", elasticDocument.get("_id"));
-    dataElement.put("type", config.typeFromIndex((String) elasticDocument.get("_index")));
-    dataElement.put("attributes", elasticDocument.get("_source"));
-
-    return dataElement;
+      var response = new HashMap<String, Object>();
+      response.put("errors", List.of(error));
+      return response;
+    }
+    else {
+      return null;
+    }
   }
 }
