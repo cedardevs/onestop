52b84b207f15c7237af4b18bca73f9b88406d30d
Erin <erin.rowland@colorado.edu>
2022-06-01 09:58:24 -0600

    Fixed mistake of changing khttp:khttp to com.github.jkcclemens:khttp. Was due to SNAPSHOT stopped working as a version. Thought package had changed, didn't. https://github.com/ascclemens/khttp Some reason the old repo url of jitpack.io wasn't working a lot of the time, so followed doc page suggested url.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 
 repositories {
     mavenCentral()
-    maven { url = uri("https://jitpack.io") }
+    maven { url = uri("https://jcenter.bintray.com") }
 }
 
 dependencies {
     implementation("com.google.code.gson:gson:2.8.6")
-    implementation("com.github.jkcclemens:khttp:-SNAPSHOT")
+    implementation("khttp:khttp:1.0.0")
     implementation("org.jsonschema2pojo:jsonschema2pojo-core:1.0.2")
 }
